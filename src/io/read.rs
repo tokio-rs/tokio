67895279523686c78faa47b38b2ab349d62bcc52
Jack O'Connor <oconnor663@gmail.com>
2016-10-15 09:46:59 -0400

    rename ReadOnce back to Read, but keep it exposed

--- src/io/read.rs
+++ src/io/read.rs
@@ -1,4 +1,3 @@
-use std::io::{Error, Read};
 use std::mem;
 
 use futures::{Future, Poll};
@@ -16,32 +15,32 @@ enum State<R, T> {
 ///
 /// The returned future will resolve to both the I/O stream and the buffer
 /// as well as the number of bytes read once the read operation is completed.
-pub fn read<R, T>(rd: R, buf: T) -> ReadOnce<R, T>
-    where R: Read,
+pub fn read<R, T>(rd: R, buf: T) -> Read<R, T>
+    where R: ::std::io::Read,
           T: AsMut<[u8]>
 {
-    ReadOnce { state: State::Pending { rd: rd, buf: buf } }
+    Read { state: State::Pending { rd: rd, buf: buf } }
 }
 
 /// A future which can be used to easily read available number of bytes to fill
 /// a buffer.
 ///
 /// Created by the [`read`] function.
-pub struct ReadOnce<R, T> {
+pub struct Read<R, T> {
     state: State<R, T>,
 }
 
-impl<R, T> Future for ReadOnce<R, T>
-    where R: Read,
+impl<R, T> Future for Read<R, T>
+    where R: ::std::io::Read,
           T: AsMut<[u8]>
 {
     type Item = (R, T, usize);
-    type Error = Error;
+    type Error = ::std::io::Error;
 
-    fn poll(&mut self) -> Poll<(R, T, usize), Error> {
+    fn poll(&mut self) -> Poll<(R, T, usize), ::std::io::Error> {
         let nread = match self.state {
             State::Pending { ref mut rd, ref mut buf } => try_nb!(rd.read(&mut buf.as_mut()[..])),
-            State::Empty => panic!("poll a ReadOnce after it's done"),
+            State::Empty => panic!("poll a Read after it's done"),
         };
 
         match mem::replace(&mut self.state, State::Empty) {
