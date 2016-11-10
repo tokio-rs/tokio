17faf329e0959e87439656fbac6e3b3d7d292324
Aaron Turon <aturon@mozilla.com>
2016-11-10 09:46:23 -0800

    Add note about how to get a Framed

--- src/io/frame.rs
+++ src/io/frame.rs
@@ -395,6 +395,8 @@ impl<T: Io, E: Encode> Sink for FramedWrite<T, E> {
 
 /// A unified `Stream` and `Sink` interface to an underlying `Io` object, using
 /// the `Encode` and `Decode` traits to encode and decode frames.
+///
+/// You can acquire a `Framd` instance by using the `Io::framed` adapter.
 pub struct Framed<T, D, E> {
     upstream: T,
     read_state: ReadState,
