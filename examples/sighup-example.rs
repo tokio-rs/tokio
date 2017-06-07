be4fe9743975fe8829d1af4bd307e77517a581b4
Alex Crichton <alex@alexcrichton.com>
2017-06-07 14:53:04 -0700

    signal: Touch up the sighup-example slightly

--- examples/sighup-example.rs
+++ examples/sighup-example.rs
@@ -2,7 +2,7 @@ extern crate futures;
 extern crate tokio_core;
 extern crate tokio_signal;
 
-use futures::stream::Stream;
+use futures::{Stream, Future};
 use tokio_core::reactor::Core;
 use tokio_signal::unix::{Signal,SIGHUP};
 
@@ -11,18 +11,19 @@ fn main() {
     let mut core = Core::new().unwrap();
 
     // on Unix, we can listen to whatever signal we want, in this case: SIGHUP
-    let sighup = Signal::new(SIGHUP, &core.handle());
-    let stream = core.run(sighup).unwrap();
+    let stream = Signal::new(SIGHUP, &core.handle()).flatten_stream();
 
     println!("Waiting for SIGHUPS (Ctrl+C to quit)");
-    println!("  TIP: use `pkill -sighup sighup-example` from a second terminal to send a SIGHUP \
-                to all processes named 'sighup-example' (i.e. this binary)");
+    println!("  TIP: use `pkill -sighup sighup-example` from a second terminal \
+                to send a SIGHUP to all processes named 'sighup-example' \
+                (i.e. this binary)");
 
     // for_each is a powerful primitive provided by the Futures crate
     // it turns a Stream into a Future that completes after all stream-items
     // have been completed.
     let future = stream.for_each(|the_signal| {
-        println!("*Got a signal {}* I should probably reload my config or something", the_signal);
+        println!("*Got signal {:#x}* I should probably reload my config \
+                  or something", the_signal);
         Ok(())
     });
 
