c4e91718e6eafb42d33979077d1b8d3a1efc0a6a
Jules Kerssemakers <jules.kerssemakers+github@gmail.com>
2017-06-08 15:57:06 +0200

    Ctrl+C example: Defer stream initialisation (and explain how/why)

--- examples/ctrl-c.rs
+++ examples/ctrl-c.rs
@@ -2,7 +2,7 @@ extern crate futures;
 extern crate tokio_core;
 extern crate tokio_signal;
 
-use futures::stream::Stream;
+use futures::{Stream, Future};
 use tokio_core::reactor::Core;
 
 fn main() {
@@ -10,9 +10,13 @@ fn main() {
     let mut core = Core::new().unwrap();
 
     // tokio_signal provides a convenience builder for Ctrl+C
-    // this even works cross-platform, linux and windows!
-    let ctrlc = tokio_signal::ctrl_c(&core.handle());
-    let stream = core.run(ctrlc).unwrap();
+    // this even works cross-platform: linux and windows!
+    //
+    // `fn ctrl_c()` produces a `Future` of the actual stream-initialisation
+    // the `flatten_stream()` convenience method lazily defers that
+    // initialisation, allowing us to use it 'as if' it is already the
+    // stream we want, reducing boilerplate Future-handling.
+    let stream = tokio_signal::ctrl_c(&core.handle()).flatten_stream();
 
     println!("This program is now waiting for you to press Ctrl+C");
 
