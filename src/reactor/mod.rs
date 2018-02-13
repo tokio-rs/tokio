ab25943cd724387bf761e2f36e77624d1f32e587
Alan Somers <asomers@gmail.com>
2018-02-12 20:56:32 -0700

    Fix the build on FreeBSD
    See also https://github.com/tokio-rs/tokio-core/commit/9400ffb

--- src/reactor/mod.rs
+++ src/reactor/mod.rs
@@ -584,7 +584,7 @@ mod platform {
 
     #[cfg(any(target_os = "dragonfly", target_os = "freebsd"))]
     pub fn all() -> Ready {
-        hup() | UnixReady::aio().into()
+        hup() | UnixReady::aio()
     }
 
     #[cfg(not(any(target_os = "dragonfly", target_os = "freebsd")))]
@@ -602,7 +602,7 @@ mod platform {
 
     #[cfg(any(target_os = "dragonfly", target_os = "freebsd"))]
     fn is_aio(ready: &Ready) -> bool {
-        ready.is_aio()
+        UnixReady::from(*ready).is_aio()
     }
 
     #[cfg(not(any(target_os = "dragonfly", target_os = "freebsd")))]
