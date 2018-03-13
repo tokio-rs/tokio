2abeff01a53290aac0b9ce3228eb6ba6f8ce380c
Wesley Moore <wes@wezm.net>
2018-03-14 05:24:17 +1100

    Fix build on FreeBSD (#218)

--- src/reactor/poll_evented.rs
+++ src/reactor/poll_evented.rs
@@ -433,16 +433,6 @@ mod platform {
     use mio::Ready;
     use mio::unix::UnixReady;
 
-    #[cfg(target_os = "dragonfly")]
-    pub fn all() -> Ready {
-        hup() | UnixReady::aio()
-    }
-
-    #[cfg(target_os = "freebsd")]
-    pub fn all() -> Ready {
-        hup() | UnixReady::aio() | UnixReady::lio()
-    }
-
     const HUP: usize = 1 << 2;
     const ERROR: usize = 1 << 3;
     const AIO: usize = 1 << 4;
