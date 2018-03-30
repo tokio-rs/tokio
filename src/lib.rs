10cb9dd4687949fd56d3b8eb38aa28916f7c943a
Carl Lerche <me@carllerche.com>
2018-03-30 15:37:52 -0700

    Actually bump tokio to v0.1.5 (#273)

--- src/lib.rs
+++ src/lib.rs
@@ -63,7 +63,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/tokio/0.1.4")]
+#![doc(html_root_url = "https://docs.rs/tokio/0.1.5")]
 #![deny(missing_docs, warnings, missing_debug_implementations)]
 
 #[macro_use]
