c5c0383069a5e25c877f1e56ee4e7898a87c064e
Roman Proskuryakov <humbug@deeptown.org>
2018-04-30 09:39:59 +0300

    ^C^V docs to current_thread/mod.rs

--- src/runtime/current_thread/mod.rs
+++ src/runtime/current_thread/mod.rs
@@ -1,5 +1,22 @@
-//! TODO
-//!
+/// Single-threaded runtime provides a way to start reactor
+/// and executor on the same thread.
+///
+/// The single-threaded runtime is not `Send` and cannot be
+/// safely moved to other threads.
+///
+/// # Examples
+///
+/// Creating a new `Runtime` with default configuration values.
+///
+/// ```
+/// use tokio::runtime::current_thread::Runtime;
+/// use tokio::prelude::*;
+///
+/// let mut runtime = Runtime::new().unwrap();
+///
+/// // Use the runtime...
+/// // runtime.block_on(f); // where f is a future
+/// ```
 
 mod builder;
 mod runtime;
