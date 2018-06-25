234ba3c1a39fb55aee84646ffcc878705630857c
David Kellum <dek-oss@gravitext.com>
2018-06-25 15:17:58 -0700

    Fix typo/simplify util module rustdoc

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,8 +1,7 @@
 //! Utilities for working with Tokio.
 //!
 //! This module contains utilities that are useful for working with Tokio.
-//! Currently, this only includes [`FutureExt`]. However, this will
-//! include over time.
+//! Currently, this only includes [`FutureExt`], but this may grow over time.
 //!
 //! [`FutureExt`]: trait.FutureExt.html
 
