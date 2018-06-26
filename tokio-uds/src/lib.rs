dc90f4b63c19cfc885fc5ff7127857d576180896
Patrick Barrett <patrick.barrett@smartthings.com>
2018-06-26 10:41:08 -0500

    implement UnixDatagramFramed
    this implementation is basically UdpFramed with a couple of types changed

--- tokio-uds/src/lib.rs
+++ tokio-uds/src/lib.rs
@@ -11,13 +11,16 @@ extern crate bytes;
 extern crate futures;
 extern crate iovec;
 extern crate libc;
+#[macro_use]
 extern crate log;
 extern crate mio;
 extern crate mio_uds;
+extern crate tokio_codec;
 extern crate tokio_io;
 extern crate tokio_reactor;
 
 mod datagram;
+mod frame;
 mod incoming;
 mod listener;
 mod recv_dgram;
@@ -26,6 +29,7 @@ mod stream;
 mod ucred;
 
 pub use datagram::UnixDatagram;
+pub use frame::UnixDatagramFramed;
 pub use incoming::Incoming;
 pub use listener::UnixListener;
 pub use recv_dgram::RecvDgram;
