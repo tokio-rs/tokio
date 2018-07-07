1412e7038e05b8dc57e1c489bf903f3dfa956a8e
Laurentiu Nicola <lnicola@dend.ro>
2018-07-07 22:34:43 +0300

    Bump version

--- tokio-fs/src/lib.rs
+++ tokio-fs/src/lib.rs
@@ -13,7 +13,7 @@
 //! [blocking]: https://docs.rs/tokio-threadpool/0.1/tokio_threadpool/fn.blocking.html
 
 #![deny(missing_docs, missing_debug_implementations, warnings)]
-#![doc(html_root_url = "https://docs.rs/tokio-fs/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/tokio-fs/0.1.2")]
 
 #[macro_use]
 extern crate futures;
