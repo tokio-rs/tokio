1f9ee33cbeea2bf7369c58ca127817123fbff280
David Kellum <dek-oss@gravitext.com>
2018-08-03 14:17:19 -0700

    Fix a cut-paste error with -reactor rustdoc links

--- tokio-reactor/src/poll_evented.rs
+++ tokio-reactor/src/poll_evented.rs
@@ -85,7 +85,7 @@ use std::sync::atomic::Ordering::Relaxed;
 /// [`Registration`]: struct.Registration.html
 /// [`TcpListener`]: ../net/struct.TcpListener.html
 /// [`clear_read_ready`]: #method.clear_read_ready
-/// [`clear_read_ready`]: #method.clear_read_ready
+/// [`clear_write_ready`]: #method.clear_write_ready
 /// [`poll_read_ready`]: #method.poll_read_ready
 /// [`poll_write_ready`]: #method.poll_write_ready
 pub struct PollEvented<E: Evented> {
