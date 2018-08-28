3a4ccc32079601127181810c9a4a60221f95414e
Eliza Weisman <eliza@buoyant.io>
2018-08-28 13:42:30 -0700

    Add `set_decode_max_line_length` to `LinesCodec`
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-codec/src/lines_codec.rs
+++ tokio-codec/src/lines_codec.rs
@@ -12,12 +12,44 @@ pub struct LinesCodec {
     // The next time `decode` is called with `abcde\n`, the method will
     // only look at `de\n` before returning.
     next_index: usize,
+
+    /// The maximum length for a given line. If `None`, lines will be read
+    /// until a `\n` character is reached.
+    max_length: Option<usize>,
 }
 
 impl LinesCodec {
     /// Returns a `LinesCodec` for splitting up data into lines.
     pub fn new() -> LinesCodec {
-        LinesCodec { next_index: 0 }
+        LinesCodec {
+            next_index: 0,
+            max_length: None,
+        }
+    }
+
+    /// Sets a limit on the maximum line length when decoding.
+    ///
+    /// If this is set, lines will be ended when a `\n` character is read, _or_
+    /// when they reach the provided number of bytes. Otherwise, lines will
+    /// only be ended when a `\n` character is read.
+    pub fn set_decode_max_line_length(&mut self, limit: usize) -> &mut Self {
+        self.max_length = Some(limit);
+        self
+    }
+
+    /// Returns the current maximum line length when decoding, if one is set.
+    ///
+    /// ```
+    /// use tokio_codec::LinesCodec;
+    ///
+    /// let mut codec = LinesCodec::new();
+    /// assert_eq!(codec.decode_max_line_length(), None);
+    ///
+    /// codec.set_decode_max_line_length(256);
+    /// assert_eq!(codec.decode_max_line_length(), Some(256));
+    /// ```
+    pub fn decode_max_line_length(&self) -> Option<usize> {
+        self.max_length
     }
 }
 
@@ -41,12 +73,33 @@ impl Decoder for LinesCodec {
     type Error = io::Error;
 
     fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<String>, io::Error> {
-        if let Some(newline_offset) =
-            buf[self.next_index..].iter().position(|b| *b == b'\n')
-        {
-            let newline_index = newline_offset + self.next_index;
+        let mut trim_and_offset = None;
+        for (offset, b) in buf[self.next_index..].iter().enumerate() {
+           trim_and_offset = match (b, self.max_length) {
+                // The current character is a newline, split here.
+                (b'\n', _) => Some((1, offset)),
+                // There's a maximum line length set, and we've reached it.
+                (_, Some(max_len)) if offset == max_len =>
+                    // If we're at the line length limit, check if the next
+                    // character(s) is a newline --- if so, slice that off
+                    // as well, so that the next call to `decode` doesn't
+                    // return an empty line.
+                    match buf[offset + 1] {
+                        b'\r' if buf[offset + 2] == b'\n' =>
+                            Some((2, offset + 2)),
+                        b'\n' => Some((1, offset + 1)),
+                        _ => Some((0, offset)),
+                    },
+                // The current character isn't a newline, and we aren't at the
+                // length limit, so keep going.
+                _ => continue,
+            };
+            break;
+        };
+        if let Some((trim_amt, offset)) = trim_and_offset {
+            let newline_index = offset + self.next_index;
             let line = buf.split_to(newline_index + 1);
-            let line = &line[..line.len()-1];
+            let line = &line[..line.len() - trim_amt];
             let line = without_carriage_return(line);
             let line = utf8(line)?;
             self.next_index = 0;
