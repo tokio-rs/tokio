a6ed6151cdd38a09ca96293decb03a645c75f8fe
Eliza Weisman <eliza@buoyant.io>
2018-08-28 17:14:59 -0700

    Add spawn_out_of_executor_context for future::Executor trait

--- tokio-executor/tests/executor.rs
+++ tokio-executor/tests/executor.rs
@@ -9,3 +9,10 @@ fn spawn_out_of_executor_context() {
     let res = DefaultExecutor::current().spawn(Box::new(lazy(|| Ok(()))));
     assert!(res.is_err());
 }
+
+#[test]
+fn spawn_out_of_executor_context_futures_executor() {
+    use futures::future::Executor as FuturesExecutor;
+    let res = DefaultExecutor::current().execute(lazy(|| Ok(())));
+    assert!(res.is_err());
+}
