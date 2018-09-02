b660fe45f08fc9a88e6494b667bf86507d0915c4
Michal 'vorner' Vaner <vorner@vorner.cz>
2018-09-02 11:32:50 +0200

    signal: Fix tests after importing & linking
    * Don't use tokio-core any more for tests. That one brings tokio from
      crates.io instead of the current workspace and two versions of that
      don't want to cooperate.
    * Guard unix-specific examples on windows.
    * Leave CI setup to top-level directory.

--- tokio-signal/tests/drop_then_get_a_signal.rs
+++ tokio-signal/tests/drop_then_get_a_signal.rs
@@ -7,22 +7,17 @@ use support::*;
 
 #[test]
 fn drop_then_get_a_signal() {
-    let mut lp = Core::new().unwrap();
-    let handle = lp.handle();
-
-    let signal = run_core_with_timeout(&mut lp, Signal::with_handle(
-        libc::SIGUSR1,
-        &handle.new_tokio_handle(),
-    )).expect("failed to create first signal");
+    let mut rt = CurrentThreadRuntime::new().unwrap();
+    let signal = run_with_timeout(&mut rt, Signal::new(libc::SIGUSR1))
+        .expect("failed to create first signal");
     drop(signal);
 
     send_signal(libc::SIGUSR1);
-    let signal = lp.run(Signal::with_handle(libc::SIGUSR1, &handle.new_tokio_handle()))
+    let signal = run_with_timeout(&mut rt, Signal::new(libc::SIGUSR1))
         .expect("failed to create signal")
         .into_future()
         .map(|_| ())
         .map_err(|(e, _)| panic!("{}", e));
 
-    run_core_with_timeout(&mut lp, signal)
-        .expect("failed to get signal");
+    run_with_timeout(&mut rt, signal).expect("failed to get signal");
 }
--- tokio-signal/tests/support.rs
+++ tokio-signal/tests/support.rs
@@ -1,45 +1,33 @@
+#![cfg(unix)]
+
 extern crate libc;
 extern crate futures;
 extern crate tokio;
-extern crate tokio_core;
 extern crate tokio_signal;
 
 use self::libc::{c_int, getpid, kill};
-use std::time::{Duration, Instant};
-use self::tokio::timer::Deadline;
-use self::tokio_core::reactor::Timeout;
+use std::time::Duration;
+use self::tokio::timer::Timeout;
 
 pub use self::futures::{Future, Stream};
-pub use self::tokio_core::reactor::Core;
-pub use self::tokio::runtime::current_thread::Runtime as CurrentThreadRuntime;
+pub use self::tokio::runtime::current_thread::{self, Runtime as CurrentThreadRuntime};
 pub use self::tokio_signal::unix::Signal;
 
-pub fn run_core_with_timeout<F>(lp: &mut Core, future: F) -> Result<F::Item, F::Error>
-    where F: Future
-{
-    let timeout = Timeout::new(Duration::from_secs(1), &lp.handle())
-        .expect("failed to register timeout")
-        .map(|()| panic!("timeout exceeded"))
-        .map_err(|e| panic!("timeout error: {}", e));
-
-    lp.run(future.select(timeout))
-        .map(|(r, _)| r)
-        .map_err(|(e, _)| e)
-}
-
-pub fn run_with_timeout<F>(rt: &mut CurrentThreadRuntime, future: F) -> Result<F::Item, F::Error>
-    where F: Future
-{
-    let deadline = Deadline::new(future, Instant::now() + Duration::from_secs(1))
+pub fn with_timeout<F: Future>(future: F) -> impl Future<Item = F::Item, Error = F::Error> {
+    Timeout::new(future, Duration::from_secs(1))
         .map_err(|e| if e.is_timer() {
             panic!("failed to register timer");
         } else if e.is_elapsed() {
             panic!("timed out")
         } else {
             e.into_inner().expect("missing inner error")
-        });
+        })
+}
 
-    rt.block_on(deadline)
+pub fn run_with_timeout<F>(rt: &mut CurrentThreadRuntime, future: F) -> Result<F::Item, F::Error>
+    where F: Future
+{
+    rt.block_on(with_timeout(future))
 }
 
 #[cfg(unix)]
