603d39fce07506ee7d7dbd9fd8aecf2078f9d3cf
Eliza Weisman <eliza@buoyant.io>
2019-01-04 10:29:24 -0800

    Clean up weird code in benchmarks
    I think this was originally intended to avoid the benchmarks getting
    optimized away, but that doesn't seem to be a problem.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-trace/benches/no_subscriber.rs
+++ tokio-trace/benches/no_subscriber.rs
@@ -8,51 +8,37 @@ use test::Bencher;
 
 #[bench]
 fn bench_span_no_subscriber(b: &mut Bencher) {
-    let n = test::black_box(1);
     b.iter(|| {
-        (0..n).fold(0, |old, new| {
-            span!("span");
-            old ^ new
-        })
+        span!("span");
     });
 }
 
 #[bench]
 fn bench_log_no_logger(b: &mut Bencher) {
-    let n = test::black_box(1);
     b.iter(|| {
-        (0..n).fold(0, |old, new| {
-            log!(log::Level::Info, "log");
-            old ^ new
-        })
+        log!(log::Level::Info, "log");
     });
 }
 
 #[bench]
 fn bench_costly_field_no_subscriber(b: &mut Bencher) {
-    let n = test::black_box(1);
     b.iter(|| {
-        (0..n).fold(0, |old, new| {
-            span!(
-                "span",
-                foo = tokio_trace::field::display(format!("bar {:?}", 2))
-            );
-            old ^ new
-        })
+        span!(
+            "span",
+            foo = tokio_trace::field::display(format!("bar {:?}", 2))
+        );
     });
 }
 
 #[bench]
 fn bench_no_span_no_subscriber(b: &mut Bencher) {
-    let n = test::black_box(1);
-    b.iter(|| (0..n).fold(0, |new, old| old ^ new));
+    b.iter(|| { });
 }
 
 #[bench]
 fn bench_1_atomic_load(b: &mut Bencher) {
     // This is just included as a baseline.
-    let n = test::black_box(1);
     use std::sync::atomic::{AtomicUsize, Ordering};
     let foo = AtomicUsize::new(1);
-    b.iter(|| (0..n).fold(0, |new, _old| foo.load(Ordering::Relaxed) ^ new));
+    b.iter(|| foo.load(Ordering::Relaxed));
 }
