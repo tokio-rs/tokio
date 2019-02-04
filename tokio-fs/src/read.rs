07d8b9207f2c30220501cd2a53fdbd5594b6b3ca
Linus Färnstrand <faern@faern.net>
2019-02-04 15:03:08 +0100

    tokio-fs: Add read, write helper functions

---
+++ tokio-fs/src/read.rs
@@ -0,0 +1,53 @@
+use {file, File};
+use futures::{Async, Future, Poll};
+use std::{io, mem, path::Path};
+use tokio_io;
+
+/// Creates a future which will open a file for reading and read the entire
+/// contents into a buffer and return said buffer.
+///
+/// This is the async equivalent of `std::fs::read`.
+pub fn read<P>(path: P) -> ReadFile<P>
+where
+    P: AsRef<Path> + Send + 'static,
+{
+    ReadFile {
+        state: State::Open(File::open(path)),
+    }
+}
+
+/// A future used to open a file and read its entire contents into a buffer.
+#[derive(Debug)]
+pub struct ReadFile<P: AsRef<Path> + Send + 'static> {
+    state: State<P>,
+}
+
+#[derive(Debug)]
+enum State<P: AsRef<Path> + Send + 'static> {
+    Open(file::OpenFuture<P>),
+    Read(tokio_io::io::ReadToEnd<File>),
+}
+
+impl<P: AsRef<Path> + Send + 'static> Future for ReadFile<P> {
+    type Item = Vec<u8>;
+    type Error = io::Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        let new_state = match &mut self.state {
+            State::Open(ref mut open_file) => {
+                let file = try_ready!(open_file.poll());
+                let buf = Vec::new();
+                let read = tokio_io::io::read_to_end(file, buf);
+                State::Read(read)
+            }
+            State::Read(ref mut read) => {
+                let (_, buf) = try_ready!(read.poll());
+                return Ok(Async::Ready(buf));
+            }
+        };
+
+        mem::replace(&mut self.state, new_state);
+        // We just entered the Read state, need to poll it before returning.
+        self.poll()
+    }
+}
