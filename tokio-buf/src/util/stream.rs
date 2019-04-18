7e51ab05e95cef00204ab18523de45f37d9687d3
Lucio Franco <luciofranco14@gmail.com>
2019-04-18 11:42:34 -0400

    buf: Add IntoStream (#1048)
    * buf: Add IntoStream
    
    * Add debug implementation for IntoStream
    
    * Add get_ref, get_mut and into_inner

--- tokio-buf/src/util/stream.rs
+++ tokio-buf/src/util/stream.rs
@@ -1,10 +1,10 @@
 use bytes::Buf;
-use futures::{Poll, Stream};
+use futures::{Async, Poll, Stream};
 use BufStream;
 
 /// Converts a `Stream` of `Buf` types into a `BufStream`.
 ///
-/// While `Stream` and `BufSream` are very similar, they are not identical. The
+/// While `Stream` and `BufStream` are very similar, they are not identical. The
 /// `stream` function returns a `BufStream` that is backed by the provided
 /// `Stream` type.
 pub fn stream<T>(stream: T) -> FromStream<T>
@@ -31,5 +31,46 @@ where
 
     fn poll_buf(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
         self.stream.poll()
+    }
+}
+
+/// Converts a `BufStream` into a `Stream`.
+#[derive(Debug)]
+pub struct IntoStream<T> {
+    buf: T,
+}
+
+impl<T> IntoStream<T> {
+    /// Create a new `Stream` from the provided `BufStream`.
+    pub fn new(buf: T) -> Self {
+        IntoStream { buf }
+    }
+
+    /// Get a reference to the inner `BufStream`.
+    pub fn get_ref(&self) -> &T {
+        &self.buf
+    }
+
+    /// Get a mutable reference to the inner `BufStream`
+    pub fn get_mut(&mut self) -> &mut T {
+        &mut self.buf
+    }
+
+    /// Get the inner `BufStream`.
+    pub fn into_inner(self) -> T {
+        self.buf
+    }
+}
+
+impl<T: BufStream> Stream for IntoStream<T> {
+    type Item = T::Item;
+    type Error = T::Error;
+
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        match self.buf.poll_buf()? {
+            Async::Ready(Some(buf)) => Ok(Async::Ready(Some(buf))),
+            Async::Ready(None) => Ok(Async::Ready(None)),
+            Async::NotReady => Ok(Async::NotReady),
+        }
     }
 }
