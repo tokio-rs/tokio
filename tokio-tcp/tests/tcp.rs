3b729ec9cd617d0f16eea20036fb23c1ada51764
Carl Lerche <me@carllerche.com>
2019-05-24 14:08:07 -0700

    Fix TCP poll_hup test (#1106)
    This updates tests to track a fix applied in Mio. Previously, Mio
    incorrectly fired HUP events. This was due to Mio mapping `RDHUP` to
    HUP. The test is updated to correctly generate a HUP event.
    
    Additionally, HUP events will be removed from all platforms except for
    Linux. This is caused by the inability to reliably map kqueue events to
    the epoll HUP behavior.

--- tokio-tcp/tests/tcp.rs
+++ tokio-tcp/tests/tcp.rs
@@ -80,13 +80,14 @@ fn accept2() {
     t.join().unwrap();
 }
 
-#[cfg(unix)]
-mod unix {
+#[cfg(target_os = "linux")]
+mod linux {
     use tokio_tcp::TcpStream;
 
     use env_logger;
     use futures::{future, Future};
     use mio::unix::UnixReady;
+    use net2::TcpStreamExt;
     use tokio_io::AsyncRead;
 
     use std::io::Write;
@@ -101,6 +102,7 @@ mod unix {
         let addr = t!(srv.local_addr());
         let t = thread::spawn(move || {
             let mut client = t!(srv.accept()).0;
+            client.set_linger(Some(Duration::from_millis(0))).unwrap();
             client.write(b"hello world").unwrap();
             thread::sleep(Duration::from_millis(200));
         });
