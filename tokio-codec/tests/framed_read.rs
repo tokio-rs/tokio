6068cfaf0987880840882304801e83d57a2706b6
jesskfulwood <jesskfulwood@gmail.com>
2019-06-27 06:44:13 +0100

    codec: re-enable FramedRead tests

--- tokio-codec/tests/framed_read.rs
+++ tokio-codec/tests/framed_read.rs
@@ -29,6 +29,12 @@ macro_rules! assert_read {
     }};
 }
 
+macro_rules! pin {
+    ($id:ident) => {
+        Pin::new(&mut $id)
+    };
+}
+
 struct U32Decoder;
 
 impl Decoder for U32Decoder {
@@ -48,161 +54,206 @@ impl Decoder for U32Decoder {
 #[test]
 fn read_multi_frame_in_packet() {
     let mut task = MockTask::new();
-
     let mock = mock! {
         Ok(b"\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x02".to_vec()),
     };
-
     let mut framed = FramedRead::new(mock, U32Decoder);
+
     task.enter(|cx| {
-        assert_read!(Pin::new(&mut framed).poll_next(cx), 0);
-        assert_read!(Pin::new(&mut framed).poll_next(cx), 1);
-        assert_read!(Pin::new(&mut framed).poll_next(cx), 2);
-        assert!(assert_ready!(Pin::new(&mut framed).poll_next(cx)).is_none());
+        assert_read!(pin!(framed).poll_next(cx), 0);
+        assert_read!(pin!(framed).poll_next(cx), 1);
+        assert_read!(pin!(framed).poll_next(cx), 2);
+        assert!(assert_ready!(pin!(framed).poll_next(cx)).is_none());
     });
 }
 
-// #[test]
-// fn read_multi_frame_across_packets() {
-//     let mock = mock! {
-//         Ok(b"\x00\x00\x00\x00".to_vec()),
-//         Ok(b"\x00\x00\x00\x01".to_vec()),
-//         Ok(b"\x00\x00\x00\x02".to_vec()),
-//     };
+#[test]
+fn read_multi_frame_across_packets() {
+    let mut task = MockTask::new();
+    let mock = mock! {
+        Ok(b"\x00\x00\x00\x00".to_vec()),
+        Ok(b"\x00\x00\x00\x01".to_vec()),
+        Ok(b"\x00\x00\x00\x02".to_vec()),
+    };
+    let mut framed = FramedRead::new(mock, U32Decoder);
 
-//     let mut framed = FramedRead::new(mock, U32Decoder);
-//     assert_eq!(Ready(Some(0)), framed.poll().unwrap());
-//     assert_eq!(Ready(Some(1)), framed.poll().unwrap());
-//     assert_eq!(Ready(Some(2)), framed.poll().unwrap());
-//     assert_eq!(Ready(None), framed.poll().unwrap());
-// }
+    task.enter(|cx| {
+        assert_read!(pin!(framed).poll_next(cx), 0);
+        assert_read!(pin!(framed).poll_next(cx), 1);
+        assert_read!(pin!(framed).poll_next(cx), 2);
+        assert!(assert_ready!(pin!(framed).poll_next(cx)).is_none());
+    });
+}
 
-// #[test]
-// fn read_not_ready() {
-//     let mock = mock! {
-//         Err(io::Error::new(io::ErrorKind::WouldBlock, "")),
-//         Ok(b"\x00\x00\x00\x00".to_vec()),
-//         Ok(b"\x00\x00\x00\x01".to_vec()),
-//     };
+#[test]
+fn read_not_ready() {
+    let mut task = MockTask::new();
+    let mock = mock! {
+        Err(io::Error::new(io::ErrorKind::WouldBlock, "")),
+        Ok(b"\x00\x00\x00\x00".to_vec()),
+        Ok(b"\x00\x00\x00\x01".to_vec()),
+    };
+    let mut framed = FramedRead::new(mock, U32Decoder);
 
-//     let mut framed = FramedRead::new(mock, U32Decoder);
-//     assert_eq!(NotReady, framed.poll().unwrap());
-//     assert_eq!(Ready(Some(0)), framed.poll().unwrap());
-//     assert_eq!(Ready(Some(1)), framed.poll().unwrap());
-//     assert_eq!(Ready(None), framed.poll().unwrap());
-// }
+    task.enter(|cx| {
+        assert!(pin!(framed).poll_next(cx).is_pending());
+        assert_read!(pin!(framed).poll_next(cx), 0);
+        assert_read!(pin!(framed).poll_next(cx), 1);
+        assert!(assert_ready!(pin!(framed).poll_next(cx)).is_none());
+    });
+}
 
-// #[test]
-// fn read_partial_then_not_ready() {
-//     let mock = mock! {
-//         Ok(b"\x00\x00".to_vec()),
-//         Err(io::Error::new(io::ErrorKind::WouldBlock, "")),
-//         Ok(b"\x00\x00\x00\x00\x00\x01\x00\x00\x00\x02".to_vec()),
-//     };
+#[test]
+fn read_partial_then_not_ready() {
+    let mut task = MockTask::new();
+    let mock = mock! {
+        Ok(b"\x00\x00".to_vec()),
+        Err(io::Error::new(io::ErrorKind::WouldBlock, "")),
+        Ok(b"\x00\x00\x00\x00\x00\x01\x00\x00\x00\x02".to_vec()),
+    };
+    let mut framed = FramedRead::new(mock, U32Decoder);
 
-//     let mut framed = FramedRead::new(mock, U32Decoder);
-//     assert_eq!(NotReady, framed.poll().unwrap());
-//     assert_eq!(Ready(Some(0)), framed.poll().unwrap());
-//     assert_eq!(Ready(Some(1)), framed.poll().unwrap());
-//     assert_eq!(Ready(Some(2)), framed.poll().unwrap());
-//     assert_eq!(Ready(None), framed.poll().unwrap());
-// }
+    task.enter(|cx| {
+        assert!(pin!(framed).poll_next(cx).is_pending());
+        assert_read!(pin!(framed).poll_next(cx), 0);
+        assert_read!(pin!(framed).poll_next(cx), 1);
+        assert_read!(pin!(framed).poll_next(cx), 2);
+        assert!(assert_ready!(pin!(framed).poll_next(cx)).is_none());
+    });
+}
 
-// #[test]
-// fn read_err() {
-//     let mock = mock! {
-//         Err(io::Error::new(io::ErrorKind::Other, "")),
-//     };
+#[test]
+fn read_err() {
+    let mut task = MockTask::new();
+    let mock = mock! {
+        Err(io::Error::new(io::ErrorKind::Other, "")),
+    };
+    let mut framed = FramedRead::new(mock, U32Decoder);
 
-//     let mut framed = FramedRead::new(mock, U32Decoder);
-//     assert_eq!(io::ErrorKind::Other, framed.poll().unwrap_err().kind());
-// }
+    task.enter(|cx| {
+        assert_eq!(
+            io::ErrorKind::Other,
+            assert_ready!(pin!(framed).poll_next(cx))
+                .unwrap()
+                .unwrap_err()
+                .kind()
+        )
+    });
+}
 
-// #[test]
-// fn read_partial_then_err() {
-//     let mock = mock! {
-//         Ok(b"\x00\x00".to_vec()),
-//         Err(io::Error::new(io::ErrorKind::Other, "")),
-//     };
+#[test]
+fn read_partial_then_err() {
+    let mut task = MockTask::new();
+    let mock = mock! {
+        Ok(b"\x00\x00".to_vec()),
+        Err(io::Error::new(io::ErrorKind::Other, "")),
+    };
+    let mut framed = FramedRead::new(mock, U32Decoder);
 
-//     let mut framed = FramedRead::new(mock, U32Decoder);
-//     assert_eq!(io::ErrorKind::Other, framed.poll().unwrap_err().kind());
-// }
+    task.enter(|cx| {
+        assert_eq!(
+            io::ErrorKind::Other,
+            assert_ready!(pin!(framed).poll_next(cx))
+                .unwrap()
+                .unwrap_err()
+                .kind()
+        )
+    });
+}
 
-// #[test]
-// fn read_partial_would_block_then_err() {
-//     let mock = mock! {
-//         Ok(b"\x00\x00".to_vec()),
-//         Err(io::Error::new(io::ErrorKind::WouldBlock, "")),
-//         Err(io::Error::new(io::ErrorKind::Other, "")),
-//     };
+#[test]
+fn read_partial_would_block_then_err() {
+    let mut task = MockTask::new();
+    let mock = mock! {
+        Ok(b"\x00\x00".to_vec()),
+        Err(io::Error::new(io::ErrorKind::WouldBlock, "")),
+        Err(io::Error::new(io::ErrorKind::Other, "")),
+    };
+    let mut framed = FramedRead::new(mock, U32Decoder);
 
-//     let mut framed = FramedRead::new(mock, U32Decoder);
-//     assert_eq!(NotReady, framed.poll().unwrap());
-//     assert_eq!(io::ErrorKind::Other, framed.poll().unwrap_err().kind());
-// }
+    task.enter(|cx| {
+        assert!(pin!(framed).poll_next(cx).is_pending());
+        assert_eq!(
+            io::ErrorKind::Other,
+            assert_ready!(pin!(framed).poll_next(cx))
+                .unwrap()
+                .unwrap_err()
+                .kind()
+        )
+    });
+}
 
-// #[test]
-// fn huge_size() {
-//     let data = [0; 32 * 1024];
+#[test]
+fn huge_size() {
+    let mut task = MockTask::new();
+    let data = [0; 32 * 1024];
+    let mut framed = FramedRead::new(Slice(&data[..]), BigDecoder);
 
-//     let mut framed = FramedRead::new(&data[..], BigDecoder);
-//     assert_eq!(Ready(Some(0)), framed.poll().unwrap());
-//     assert_eq!(Ready(None), framed.poll().unwrap());
+    task.enter(|cx| {
+        assert_read!(pin!(framed).poll_next(cx), 0);
+        assert!(assert_ready!(pin!(framed).poll_next(cx)).is_none());
+    });
 
-//     struct BigDecoder;
+    struct BigDecoder;
 
-//     impl Decoder for BigDecoder {
-//         type Item = u32;
-//         type Error = io::Error;
+    impl Decoder for BigDecoder {
+        type Item = u32;
+        type Error = io::Error;
 
-//         fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<u32>> {
-//             if buf.len() < 32 * 1024 {
-//                 return Ok(None);
-//             }
-//             buf.split_to(32 * 1024);
-//             Ok(Some(0))
-//         }
-//     }
-// }
+        fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<u32>> {
+            if buf.len() < 32 * 1024 {
+                return Ok(None);
+            }
+            buf.split_to(32 * 1024);
+            Ok(Some(0))
+        }
+    }
+}
 
-// #[test]
-// fn data_remaining_is_error() {
-//     let data = [0; 5];
+#[test]
+fn data_remaining_is_error() {
+    let mut task = MockTask::new();
+    let slice = Slice(&[0; 5]);
+    let mut framed = FramedRead::new(slice, U32Decoder);
 
-//     let mut framed = FramedRead::new(&data[..], U32Decoder);
-//     assert_eq!(Ready(Some(0)), framed.poll().unwrap());
-//     assert!(framed.poll().is_err());
-// }
+    task.enter(|cx| {
+        assert_read!(pin!(framed).poll_next(cx), 0);
+        assert!(assert_ready!(pin!(framed).poll_next(cx)).unwrap().is_err());
+    });
+}
 
-// #[test]
-// fn multi_frames_on_eof() {
-//     struct MyDecoder(Vec<u32>);
+#[test]
+fn multi_frames_on_eof() {
+    let mut task = MockTask::new();
+    struct MyDecoder(Vec<u32>);
 
-//     impl Decoder for MyDecoder {
-//         type Item = u32;
-//         type Error = io::Error;
+    impl Decoder for MyDecoder {
+        type Item = u32;
+        type Error = io::Error;
 
-//         fn decode(&mut self, _buf: &mut BytesMut) -> io::Result<Option<u32>> {
-//             unreachable!();
-//         }
+        fn decode(&mut self, _buf: &mut BytesMut) -> io::Result<Option<u32>> {
+            unreachable!();
+        }
 
-//         fn decode_eof(&mut self, _buf: &mut BytesMut) -> io::Result<Option<u32>> {
-//             if self.0.is_empty() {
-//                 return Ok(None);
-//             }
+        fn decode_eof(&mut self, _buf: &mut BytesMut) -> io::Result<Option<u32>> {
+            if self.0.is_empty() {
+                return Ok(None);
+            }
 
-//             Ok(Some(self.0.remove(0)))
-//         }
-//     }
+            Ok(Some(self.0.remove(0)))
+        }
+    }
 
-//     let mut framed = FramedRead::new(mock!(), MyDecoder(vec![0, 1, 2, 3]));
-//     assert_eq!(Ready(Some(0)), framed.poll().unwrap());
-//     assert_eq!(Ready(Some(1)), framed.poll().unwrap());
-//     assert_eq!(Ready(Some(2)), framed.poll().unwrap());
-//     assert_eq!(Ready(Some(3)), framed.poll().unwrap());
-//     assert_eq!(Ready(None), framed.poll().unwrap());
-// }
+    let mut framed = FramedRead::new(mock!(), MyDecoder(vec![0, 1, 2, 3]));
+
+    task.enter(|cx| {
+        assert_read!(pin!(framed).poll_next(cx), 0);
+        assert_read!(pin!(framed).poll_next(cx), 1);
+        assert_read!(pin!(framed).poll_next(cx), 2);
+        assert_read!(pin!(framed).poll_next(cx), 3);
+        assert!(assert_ready!(pin!(framed).poll_next(cx)).is_none());
+    });
+}
 
 // ===== Mock ======
 
@@ -230,6 +281,22 @@ impl AsyncRead for Mock {
         _cx: &mut Context<'_>,
         buf: &mut [u8],
     ) -> Poll<io::Result<usize>> {
-        Ready(Pin::get_mut(self).read(buf))
+        match Pin::get_mut(self).read(buf) {
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => Pending,
+            other => Ready(other),
+        }
+    }
+}
+
+// TODO this newtype is necessary because `&[u8]` does not implement `AsyncRead`
+struct Slice<'a>(&'a [u8]);
+
+impl<'a> AsyncRead for Slice<'a> {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<io::Result<usize>> {
+        Ready(Pin::get_mut(self).0.read(buf))
     }
 }
