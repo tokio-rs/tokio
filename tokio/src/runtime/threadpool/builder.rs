4446eb4db86a7cf1fa130762403f72313d0cd79c
Eliza Weisman <eliza@buoyant.io>
2019-07-01 16:18:12 -0700

    chore: remove `tokio-trace`, add "Related Projects" to README (v0.1) (#1223)
    * chore: remove `tokio-trace`, add "Related Projects" to README (#1221)
    
    The `tokio-trace` and `tokio-trace-core` crates have been renamed to
    `tracing` and `tracing-core`, and moved to their own repository
    (`tokio-rs/tracing`).
    
    This branch removes `tokio-trace` and `tokio-trace-core` from the
    `tokio` repository. In addition, I've added a "Related Projects" section
    to the root README, which lists `tracing` (as well as  `mio`, and
    `bytes`) as other libraries maintained by the Tokio project. I thought
    that this would help folks looking for `tokio-trace` here find it in its
    new home.
    
    In addition, it changes `tokio` to depend on `tracing-core` rather than
    `tokio-trace-core`.
    
    Closes #1159
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>
    
    * Remove erroneous add of `tokio-macros`
    
    * Some more tokio-trace remnants
    
    * Remove tokio-trace remnants from Cirrus CI
    
    * disable tracing-core feature by default
    
    It can't build on Rust 1.26.0
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>
    
    * fix feature flagging
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio/src/runtime/threadpool/builder.rs
+++ tokio/src/runtime/threadpool/builder.rs
@@ -12,7 +12,9 @@ use tokio_reactor;
 use tokio_threadpool::Builder as ThreadPoolBuilder;
 use tokio_timer::clock::{self, Clock};
 use tokio_timer::timer::{self, Timer};
-use tokio_trace_core as trace;
+
+#[cfg(feature = "experimental-tracing")]
+use tracing_core as trace;
 
 /// Builds Tokio Runtime with custom configuration values.
 ///
@@ -364,9 +366,10 @@ impl Builder {
         let clock = self.clock.clone();
 
         // Get the current trace dispatcher.
-        // TODO(eliza): when `tokio-trace-core` is stable enough to take a
+        // TODO(eliza): when `tracing-core` is stable enough to take a
         // public API dependency, we should allow users to set a custom
         // subscriber for the runtime.
+        #[cfg(feature = "experimental-tracing")]
         let dispatch = trace::dispatcher::get_default(trace::Dispatch::clone);
 
         let pool = self
@@ -377,9 +380,14 @@ impl Builder {
                 tokio_reactor::with_default(&reactor_handles[index], enter, |enter| {
                     clock::with_default(&clock, enter, |enter| {
                         timer::with_default(&timer_handles[index], enter, |_| {
+
+                            #[cfg(feature = "experimental-tracing")]
                             trace::dispatcher::with_default(&dispatch, || {
                                 w.run();
-                            })
+                            });
+
+                            #[cfg(not(feature = "experimental-tracing"))]
+                            w.run();
                         });
                     })
                 });
