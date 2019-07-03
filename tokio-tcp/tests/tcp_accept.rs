56936f02f120b3e6e91fb6a330227e22551ddd15
Carl Lerche <me@carllerche.com>
2019-07-03 09:06:54 -0700

    add test

--- tokio-tcp/tests/tcp_accept.rs
+++ tokio-tcp/tests/tcp_accept.rs
@@ -2,50 +2,24 @@
 #![feature(async_await)]
 
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::mpsc;
+use tokio::sync::oneshot;
 use tokio_test::assert_ok;
 
 #[tokio::test]
 async fn accept() {
     let addr = "127.0.0.1:0".parse().unwrap();
     let mut listener = assert_ok!(TcpListener::bind(&addr));
+    let addr = listener.local_addr().unwrap();
 
-    let (mut tx, rx) = mpsc::unbounded_channel();
+    let (tx, rx) = oneshot::channel();
 
     tokio::spawn(async move {
-        loop {
-            let (socket, _) = assert_ok!(listener.accept().await);
-            assert_ok!(tx.try_send(socket));
-        }
+        let (socket, _) = assert_ok!(listener.accept().await);
+        assert_ok!(tx.send(socket));
     });
 
     let cli = assert_ok!(TcpStream::connect(&addr).await);
-    let srv = rx.next().await;
+    let srv = assert_ok!(rx.await);
 
-    assert_eq!(cli.local_addr(), srv.peer_addr());
-
-
-    /*
-    let srv = t!(TcpListener::bind(&t!("127.0.0.1:0".parse())));
-    let addr = t!(srv.local_addr());
-
-    let (tx, rx) = channel();
-    let client = srv
-        .incoming()
-        .map(move |t| {
-            tx.send(()).unwrap();
-            t
-        })
-        .into_future()
-        .map_err(|e| e.0);
-    assert!(rx.try_recv().is_err());
-    let t = thread::spawn(move || net::TcpStream::connect(&addr).unwrap());
-
-    let (mine, _remaining) = t!(client.wait());
-    let mine = mine.unwrap();
-    let theirs = t.join().unwrap();
-
-    assert_eq!(t!(mine.local_addr()), t!(theirs.peer_addr()));
-    assert_eq!(t!(theirs.local_addr()), t!(mine.peer_addr()));
-    */
+    assert_eq!(cli.local_addr().unwrap(), srv.peer_addr().unwrap());
 }
