0cfa120ba8c93822d6a84f2e7392008d84622010
Taiki Endo <te316e89@gmail.com>
2019-07-15 15:21:09 +0900

    tokio: add AsyncBufReadExt::read_until

--- tokio/src/io/async_buf_read_ext.rs
+++ tokio/src/io/async_buf_read_ext.rs
@@ -1,6 +1,34 @@
+use crate::io::read_until::{read_until, ReadUntil};
+
 use tokio_io::AsyncBufRead;
 
 /// An extension trait which adds utility methods to `AsyncBufRead` types.
-pub trait AsyncBufReadExt: AsyncBufRead {}
+pub trait AsyncBufReadExt: AsyncBufRead {
+    /// Creates a future which will read all the bytes associated with this I/O
+    /// object into `buf` until the delimiter `byte` or EOF is reached.
+    /// This method is the async equivalent to [`BufRead::read_until`](std::io::BufRead::read_until).
+    ///
+    /// This function will read bytes from the underlying stream until the
+    /// delimiter or EOF is found. Once found, all bytes up to, and including,
+    /// the delimiter (if found) will be appended to `buf`.
+    ///
+    /// The returned future will resolve to the number of bytes read once the read
+    /// operation is completed.
+    ///
+    /// In the case of an error the buffer and the object will be discarded, with
+    /// the error yielded.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// unimplemented!();
+    /// ```
+    fn read_until<'a>(&'a mut self, byte: u8, buf: &'a mut Vec<u8>) -> ReadUntil<'a, Self>
+    where
+        Self: Unpin,
+    {
+        read_until(self, byte, buf)
+    }
+}
 
 impl<R: AsyncBufRead + ?Sized> AsyncBufReadExt for R {}
