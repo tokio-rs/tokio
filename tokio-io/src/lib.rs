7f7f74985e18a6bbe6e05e49ca2407506a000889
Sean McArthur <sean@seanmonstar.com>
2019-07-15 14:53:16 -0700

    io: Minor adjustments to tokio-test IO (#1306)
    This also re-exports `bytes::{Buf, BufMut}` from `tokio-io`.

--- tokio-io/src/lib.rs
+++ tokio-io/src/lib.rs
@@ -2,7 +2,6 @@
 #![deny(missing_debug_implementations, missing_docs, rust_2018_idioms)]
 #![cfg_attr(test, deny(warnings))]
 #![doc(test(no_crate_inject, attr(deny(rust_2018_idioms))))]
-//#![feature(async_await)]
 
 //! Core I/O traits and combinators when working with Tokio.
 //!
@@ -19,3 +18,6 @@ mod async_write;
 pub use self::async_buf_read::AsyncBufRead;
 pub use self::async_read::AsyncRead;
 pub use self::async_write::AsyncWrite;
+
+// Re-export `Buf` and `BufMut` since they are part of the API
+pub use bytes::{Buf, BufMut};
