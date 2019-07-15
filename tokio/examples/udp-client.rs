86d578eb7b3ae6ac9225a91c38396c728feaca04
John C. Doneth <doneth7@gmail.com>
2019-07-15 13:31:45 -0400

    update udp-client

--- tokio/examples/udp-client.rs
+++ tokio/examples/udp-client.rs
@@ -32,21 +32,21 @@
 use std::env;
 use std::io::{stdin, Read};
 use std::net::SocketAddr;
+use std::error::Error;
 use tokio::net::UdpSocket;
 
-fn get_stdin_data() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+fn get_stdin_data() -> Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>> {
     let mut buf = Vec::new();
     stdin().read_to_end(&mut buf)?;
     Ok(buf)
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let remote_addr: SocketAddr = env::args()
         .nth(1)
         .unwrap_or("127.0.0.1:8080".into())
-        .parse()
-        .unwrap();
+        .parse()?;
 
     // We use port 0 to let the operating system allocate an available port for us.
     let local_addr: SocketAddr = if remote_addr.is_ipv4() {
@@ -54,19 +54,20 @@ async fn main() {
     } else {
         "[::]:0"
     }
-    .parse()
-    .unwrap();
+    .parse()?;
 
-    let mut socket = UdpSocket::bind(&local_addr).unwrap();
+    let mut socket = UdpSocket::bind(&local_addr)?;
     const MAX_DATAGRAM_SIZE: usize = 65_507;
-    socket.connect(&remote_addr).unwrap();
-    let data = get_stdin_data().unwrap();
-    socket.send(&data).await.unwrap();
+    socket.connect(&remote_addr)?;
+    let data = get_stdin_data()?;
+    socket.send(&data).await?;
     let mut data = vec![0u8; MAX_DATAGRAM_SIZE];
-    let len = socket.recv(&mut data).await.unwrap();
+    let len = socket.recv(&mut data).await?;
     println!(
         "Received {} bytes:\n{}",
         len,
         String::from_utf8_lossy(&data[..len])
     );
+
+    Ok(())
 }
