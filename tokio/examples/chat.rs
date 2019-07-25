947ad5859fe94c03d4b43a6550840b071be95ad4
John C. Doneth <doneth7@gmail.com>
2019-07-25 14:39:02 -0400

    use tokio::sync::lock::Lock instead of futures::lock::Mutex

--- tokio/examples/chat.rs
+++ tokio/examples/chat.rs
@@ -27,10 +27,10 @@
 #![feature(async_await)]
 #![deny(warnings, rust_2018_idioms)]
 
-use futures::{lock::Mutex, Poll, SinkExt, Stream, StreamExt};
+use futures::{Poll, SinkExt, Stream, StreamExt};
 
 use std::{
-    collections::HashMap, env, error::Error, io, net::SocketAddr, pin::Pin, sync::Arc,
+    collections::HashMap, env, error::Error, io, net::SocketAddr, pin::Pin,
     task::Context,
 };
 
@@ -38,7 +38,7 @@ use tokio::{
     self,
     codec::{Framed, LinesCodec, LinesCodecError},
     net::{TcpListener, TcpStream},
-    sync::mpsc,
+    sync::{mpsc, lock::Lock},
 };
 
 /// Shorthand for the transmit half of the message channel.
@@ -101,7 +101,7 @@ impl Shared {
 impl Peer {
     /// Create a new instance of `Peer`.
     async fn new(
-        state: Arc<Mutex<Shared>>,
+        mut state: Lock<Shared>,
         lines: Framed<TcpStream, LinesCodec>,
     ) -> io::Result<Peer> {
         // Get the client socket address
@@ -156,7 +156,7 @@ impl Stream for Peer {
 
 /// Process an individual chat client
 async fn process(
-    state: Arc<Mutex<Shared>>,
+    mut state: Lock<Shared>,
     stream: TcpStream,
     addr: SocketAddr,
 ) -> Result<(), Box<dyn Error>> {
@@ -234,7 +234,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // The server task will hold a handle to this. For every new client, the
     // `state` handle is cloned and passed into the task that processes the
     // client connection.
-    let state = Arc::new(Mutex::new(Shared::new()));
+    let state = Lock::new(Shared::new());
 
     let addr = env::args().nth(1).unwrap_or("127.0.0.1:6142".to_string());
     let addr = addr.parse::<SocketAddr>()?;
