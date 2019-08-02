d7426f9e126bacb12dacf50f8ba3e84f34f0f609
Lucio Franco <luciofranco14@gmail.com>
2019-08-02 12:36:50 -0400

    tokio: Update connect to async/await

--- tokio/examples/connect.rs
+++ tokio/examples/connect.rs
@@ -14,17 +14,30 @@
 //! this repository! Many of them recommend running this as a simple "hook up
 //! stdin/stdout to a server" to get up and running.
 
-// #![deny(warnings, rust_2018_idioms)]
+#![deny(warnings, rust_2018_idioms)]
 #![feature(async_await)]
 
+use futures::{SinkExt, Stream};
 use std::{env, error::Error, net::SocketAddr};
 use tokio::{
     codec::{FramedRead, FramedWrite},
     io,
+    sync::{mpsc, oneshot},
 };
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let (tx, rx) = oneshot::channel();
+    tokio::spawn(async move {
+        run().await.unwrap();
+        tx.send(()).unwrap();
+    });
+
+    rx.await.map_err(Into::into)
+}
+
+// Currently, we need to spawn the initial future due to https://github.com/tokio-rs/tokio/issues/1356
+async fn run() -> Result<(), Box<dyn Error>> {
     // Determine if we're going to run in TCP or UDP mode
     let mut args = env::args().skip(1).collect::<Vec<_>>();
     let tcp = match args.iter().position(|a| a == "--udp") {
@@ -42,21 +55,34 @@ async fn main() -> Result<(), Box<dyn Error>> {
     };
     let addr = addr.parse::<SocketAddr>()?;
 
-    let stdin = FramedRead::new(io::stdin(), codec::Bytes);
+    let stdin = stdin();
     let stdout = FramedWrite::new(io::stdout(), codec::Bytes);
 
     if tcp {
         tcp::connect(&addr, stdin, stdout).await?;
     } else {
-        unimplemented!()
+        udp::connect(&addr, stdin, stdout).await?;
     }
 
     Ok(())
 }
 
+// Temporary work around for stdin blocking the stream
+fn stdin() -> impl Stream<Item = Result<Vec<u8>, io::Error>> + Unpin {
+    let mut stdin = FramedRead::new(io::stdin(), codec::Bytes);
+
+    let (mut tx, rx) = mpsc::unbounded_channel();
+
+    tokio::spawn(async move {
+        tx.send_all(&mut stdin).await.unwrap();
+    });
+
+    rx
+}
+
 mod tcp {
     use super::codec;
-    use futures::{future::join, Sink, SinkExt, Stream, StreamExt};
+    use futures::{future, Sink, SinkExt, Stream, StreamExt};
     use std::{error::Error, io, net::SocketAddr};
     use tokio::{
         codec::{FramedRead, FramedWrite},
@@ -70,11 +96,74 @@ mod tcp {
     ) -> Result<(), Box<dyn Error>> {
         let (r, w) = TcpStream::connect(addr).await?.split();
         let sink = FramedWrite::new(w, codec::Bytes);
-        let mut stream = FramedRead::new(r, codec::Bytes).map(|i| i.unwrap());
+        let mut stream = FramedRead::new(r, codec::Bytes).filter_map(|i| match i {
+            Ok(i) => future::ready(Some(i)),
+            Err(e) => {
+                println!("failed to read from socket; error={}", e);
+                future::ready(None)
+            }
+        });
 
-        match join(stdin.forward(sink), stdout.send_all(&mut stream)).await {
+        match future::join(stdin.forward(sink), stdout.send_all(&mut stream)).await {
             (Err(e), _) | (_, Err(e)) => Err(e.into()),
             _ => Ok(()),
+        }
+    }
+}
+
+mod udp {
+    use futures::{future, Sink, SinkExt, Stream, StreamExt};
+    use std::{error::Error, io, net::SocketAddr};
+    use tokio::net::udp::{
+        split::{UdpSocketRecvHalf, UdpSocketSendHalf},
+        UdpSocket,
+    };
+
+    pub async fn connect(
+        addr: &SocketAddr,
+        stdin: impl Stream<Item = Result<Vec<u8>, io::Error>> + Unpin,
+        stdout: impl Sink<Vec<u8>, Error = io::Error> + Unpin,
+    ) -> Result<(), Box<dyn Error>> {
+        // We'll bind our UDP socket to a local IP/port, but for now we
+        // basically let the OS pick both of those.
+        let bind_addr = if addr.ip().is_ipv4() {
+            "0.0.0.0:0".parse()?
+        } else {
+            "[::]:0".parse()?
+        };
+
+        let socket = UdpSocket::bind(&bind_addr)?;
+        socket.connect(addr)?;
+        let (mut r, mut w) = socket.split();
+
+        future::try_join(send(stdin, &mut w), recv(stdout, &mut r)).await?;
+
+        Ok(())
+    }
+
+    async fn send(
+        mut stdin: impl Stream<Item = Result<Vec<u8>, io::Error>> + Unpin,
+        writer: &mut UdpSocketSendHalf,
+    ) -> Result<(), io::Error> {
+        while let Some(item) = stdin.next().await {
+            let buf = item?;
+            writer.send(&buf[..]).await?;
+        }
+
+        Ok(())
+    }
+
+    async fn recv(
+        mut stdout: impl Sink<Vec<u8>, Error = io::Error> + Unpin,
+        reader: &mut UdpSocketRecvHalf,
+    ) -> Result<(), io::Error> {
+        loop {
+            let mut buf = vec![0; 1024];
+            let n = reader.recv(&mut buf[..]).await?;
+
+            if n > 0 {
+                stdout.send(buf).await?;
+            }
         }
     }
 }
