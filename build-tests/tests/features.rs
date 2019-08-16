90427f1a6e19b24abdb1b21e26bb3e72c035ca07
Carl Lerche <me@carllerche.com>
2019-08-16 07:59:00 -0700

    chore: rename ui-tests -> build-tests

--- build-tests/tests/features.rs
+++ build-tests/tests/features.rs
@@ -3,43 +3,43 @@
 #[test]
 #[cfg(feature = "tokio-net")]
 fn net_default() {
-    use ui_tests::tokio_net::driver::{set_default, Handle, Reactor, Registration};
-    use ui_tests::tokio_net::util::PollEvented;
+    use build_tests::tokio_net::driver::{set_default, Handle, Reactor, Registration};
+    use build_tests::tokio_net::util::PollEvented;
 }
 
 #[test]
 #[cfg(feature = "net-with-tcp")]
 fn net_with_tcp() {
-    use ui_tests::tokio_net::tcp;
+    use build_tests::tokio_net::tcp;
 }
 
 #[test]
 #[cfg(feature = "net-with-udp")]
 fn net_with_udp() {
-    use ui_tests::tokio_net::udp;
+    use build_tests::tokio_net::udp;
 }
 
 #[test]
 #[cfg(feature = "tokio-with-net")]
 fn tokio_with_net() {
     // net is present
-    use ui_tests::tokio::net;
+    use build_tests::tokio::net;
 }
 #[test]
 fn compile_fail() {
     let t = trybuild::TestCases::new();
 
     #[cfg(feature = "executor-without-current-thread")]
-    t.compile_fail("tests/ui/executor_without_current_thread.rs");
+    t.compile_fail("tests/fail/executor_without_current_thread.rs");
 
     #[cfg(feature = "net-no-features")]
     {
-        t.compile_fail("tests/ui/net_without_tcp_missing_tcp.rs");
-        t.compile_fail("tests/ui/net_without_udp_missing_udp.rs");
+        t.compile_fail("tests/fail/net_without_tcp_missing_tcp.rs");
+        t.compile_fail("tests/fail/net_without_udp_missing_udp.rs");
     }
 
     #[cfg(feature = "tokio-no-features")]
-    t.compile_fail("tests/ui/tokio_without_net_missing_net.rs");
+    t.compile_fail("tests/fail/tokio_without_net_missing_net.rs");
 
     drop(t);
 }
