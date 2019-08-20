ff96dcde0fb501ffb8077b6923523d0e86cec881
Eliza Weisman <eliza@buoyant.io>
2019-08-20 10:15:38 -0700

    fix unused macros
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-net/src/tracing.rs
+++ tokio-net/src/tracing.rs
@@ -4,10 +4,6 @@
 //!
 //! This means we don't have to put a `#[cfg(feature = "tracing")]` on every
 //! individual use of a `tracing` macro.
-#[cfg(not(feature = "tracing"))]
-#[derive(Clone, Debug)]
-pub(crate) struct Span {}
-
 #[cfg(feature = "tracing")]
 macro_rules! trace {
     ($($arg:tt)+) => {
@@ -30,53 +26,4 @@ macro_rules! debug {
 #[cfg(not(feature = "tracing"))]
 macro_rules! debug {
     ($($arg:tt)+) => {};
-}
-
-#[cfg(feature = "tracing")]
-macro_rules! warn {
-    ($($arg:tt)+) => {
-        tracing::warn!($($arg)+)
-    };
-}
-
-#[cfg(not(feature = "tracing"))]
-macro_rules! warn {
-    ($($arg:tt)+) => {};
-}
-
-#[cfg(feature = "tracing")]
-macro_rules! error {
-    ($($arg:tt)+) => {
-        tracing::error!($($arg)+)
-    };
-}
-
-#[cfg(not(feature = "tracing"))]
-macro_rules! error {
-    ($($arg:tt)+) => {};
-}
-
-#[cfg(feature = "tracing")]
-macro_rules! trace_span {
-    ($($arg:tt)+) => {
-        tracing::trace_span!($($arg)+)
-    };
-}
-
-#[cfg(not(feature = "tracing"))]
-macro_rules! trace_span {
-    ($($arg:tt)+) => {
-        crate::tracing::Span::new()
-    };
-}
-
-#[cfg(not(feature = "tracing"))]
-impl Span {
-    pub(crate) fn new() -> Self {
-        Span {}
-    }
-
-    pub(crate) fn enter(&self) -> Span {
-        Span {}
-    }
 }
