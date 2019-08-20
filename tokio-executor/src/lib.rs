e8760064ecedd217cf6995058b44a84d0f36b62c
Eliza Weisman <eliza@buoyant.io>
2019-08-20 10:22:08 -0700

    tracing is only used when impls are enabled
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-executor/src/lib.rs
+++ tokio-executor/src/lib.rs
@@ -56,8 +56,9 @@
 //! [`DefaultExecutor`]: struct.DefaultExecutor.html
 //! [`Park`]: park/index.html
 //! [`Future::poll`]: https://doc.rust-lang.org/std/future/trait.Future.html#tymethod.poll
+#[cfg(any(feature = "current-thread", feature = "threadpool"))]
 #[macro_use]
-pub(crate) mod tracing;
+mod tracing;
 
 mod enter;
 mod error;
