dc8f645ad7b9f549da491bb2e68dad3ea6aa6814
Taiki Endo <te316e89@gmail.com>
2019-08-21 10:53:18 +0900

    fix test

--- tokio-io/tests/take.rs
+++ tokio-io/tests/take.rs
@@ -9,8 +9,8 @@ async fn take() {
     let mut buf = [0; 6];
     let rd: &[u8] = b"hello world";
 
-    let mut rd = rd.take(5);
+    let mut rd = rd.take(4);
     let n = assert_ok!(rd.read(&mut buf).await);
-    assert_eq!(n, 5);
-    assert_eq!(buf, b"hello");
+    assert_eq!(n, 4);
+    assert_eq!(&buf, &b"hell\0\0"[..]);
 }
