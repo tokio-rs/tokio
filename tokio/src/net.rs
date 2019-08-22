654f9d703f8ad55e6d5651ba1c47ca23fa641fd3
Danny Browning <bdbrowning2@gmail.com>
2019-08-22 10:12:00 -0600

    tokio: expose signal feature (#1491)
    Expose tokio_net::signal::ctrl_c via tokio::net::signal::ctrl_c as feature signal.

--- tokio/src/net.rs
+++ tokio/src/net.rs
@@ -57,6 +57,14 @@ pub mod udp {
 #[cfg(feature = "udp")]
 pub use self::udp::{UdpFramed, UdpSocket};
 
+#[cfg(feature = "signal")]
+pub mod signal {
+    //! Asynchronous signal handling for `tokio`. ctrl-C notifications are
+    //! supported on both unix and windows systems. For finer grained signal
+    //! handling support on unix systems, see `tokio_net::signal::unix::Signal`.
+    pub use tokio_net::signal::ctrl_c;
+}
+
 #[cfg(all(unix, feature = "uds"))]
 pub mod unix {
     //! Unix domain socket bindings for `tokio` (only available on unix systems).
