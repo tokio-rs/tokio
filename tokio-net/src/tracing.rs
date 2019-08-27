ba6e5cb6d1581c7c1cf8b550e4bbcf42471dcd82
Eliza Weisman <eliza@buoyant.io>
2019-08-27 15:37:10 -0700

    fix missing macro
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-net/src/tracing.rs
+++ tokio-net/src/tracing.rs
@@ -4,6 +4,10 @@
 //!
 //! This means we don't have to put a `#[cfg(feature = "tracing")]` on every
 //! individual use of a `tracing` macro.
+#[cfg(not(feature = "tracing"))]
+#[derive(Clone, Debug)]
+pub(crate) struct Span {}
+
 #[cfg(feature = "tracing")]
 macro_rules! trace {
     ($($arg:tt)+) => {
@@ -38,4 +42,29 @@ macro_rules! error {
 #[cfg(all(unix, feature = "process", not(feature = "tracing")))]
 macro_rules! error {
     ($($arg:tt)+) => {};
+}
+
+#[cfg(feature = "tracing")]
+macro_rules! trace_span {
+    ($($arg:tt)+) => {
+        tracing::trace_span!($($arg)+)
+    };
+}
+
+#[cfg(not(feature = "tracing"))]
+macro_rules! trace_span {
+    ($($arg:tt)+) => {
+        crate::tracing::Span::new()
+    };
+}
+
+#[cfg(not(feature = "tracing"))]
+impl Span {
+    pub(crate) fn new() -> Self {
+        Span {}
+    }
+
+    pub(crate) fn enter(&self) -> Span {
+        Span {}
+    }
 }
