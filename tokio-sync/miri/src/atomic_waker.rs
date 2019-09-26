e40005d9205ad1381a2a2aee6019840e045b74c9
Jon Gjengset <jon@thesquareplanet.com>
2019-09-26 15:18:35 -0400

    Add atomic waker miri

---
+++ tokio-sync/miri/src/atomic_waker.rs
@@ -0,0 +1,37 @@
+#![warn(rust_2018_idioms)]
+
+use std::task::Waker;
+use tokio_sync::AtomicWaker;
+use tokio_test::task::MockTask;
+
+trait AssertSend: Send {}
+trait AssertSync: Send {}
+
+impl AssertSend for AtomicWaker {}
+impl AssertSync for AtomicWaker {}
+
+impl AssertSend for Waker {}
+impl AssertSync for Waker {}
+
+fn main() {
+    {
+        let waker = AtomicWaker::new();
+        let mut task = MockTask::new();
+
+        task.enter(|cx| waker.register_by_ref(cx.waker()));
+        waker.wake();
+
+        assert!(task.is_woken());
+    }
+
+    {
+        let waker = AtomicWaker::new();
+        waker.wake();
+
+        // Registering should not result in a notification
+        let mut task = MockTask::new();
+        task.enter(|cx| waker.register_by_ref(cx.waker()));
+
+        assert!(!task.is_woken());
+    }
+}
