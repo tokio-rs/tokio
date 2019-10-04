53e9632abc7a15a7d3e46bbf99700f4a6db36731
Eliza Weisman <eliza@buoyant.io>
2019-10-04 14:48:58 -0700

    io: add missing utility functions
    The standard library's `io` module has small utilities such as `repeat`,
    `empty`, and `sink`, which return `Read` and `Write` implementations.
    These can come in handy in some circiumstances. `tokio::io` has no
    equivalents that implement `AsyncRead`/`AsyncWrite`.
    
    This commit adds `repeat`, `empty`, and `sink` helpers to `tokio::io`.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

---
+++ tokio-io/src/io/empty.rs
@@ -0,0 +1,56 @@
+use crate::AsyncWrite;
+use std::fmt;
+use std::io;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+// An async reader which is always at EOF.
+///
+/// This struct is generally created by calling [`empty`]. Please see
+/// the documentation of [`empty()`][`empty`] for more details.
+///
+/// This is an asynchronous version of `std::io::empty`.
+///
+/// [`empty`]: fn.empty.html
+pub struct Empty {
+    _p: (),
+}
+
+/// Creates a new empty async reader.
+///
+/// All reads from the returned reader will return `Poll::Ready(Ok(0))`.
+///
+/// This is an asynchronous version of `std::io::empty`.
+///
+/// # Examples
+///
+/// A slightly sad example of not reading anything into a buffer:
+///
+/// ```rust
+/// # use tokio_io::{self as io, AsyncRead};
+/// # async fn dox() {
+/// let mut buffer = String::new();
+/// io::empty().read_to_string(&mut buffer).await.unwrap();
+/// assert!(buffer.is_empty());
+/// # }
+/// ```
+pub fn empty() -> Empty {
+    Empty { _p: () }
+}
+
+impl AsyncRead for Empty {
+    #[inline]
+    fn poll_read(
+        self: Pin<&mut Self>,
+        _: &mut Context<'_>,
+        _: &mut [u8],
+    ) -> Poll<io::Result<usize>> {
+        Poll::Ready(Ok(0))
+    }
+}
+
+impl fmt::Debug for Empty {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.pad("Empty { .. }")
+    }
+}
