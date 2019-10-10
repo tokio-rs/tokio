d659a8556c83988d7c50c267b9956bfd58205b7d
Eliza Weisman <eliza@buoyant.io>
2019-10-10 13:23:49 -0700

    fix concurrent immutable/mutable access
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-net/src/driver/sharded_slab/tests.rs
+++ tokio-net/src/driver/sharded_slab/tests.rs
@@ -39,7 +39,7 @@ mod tiny_slab {
     use tid::Tid;
 
     // Overridden for tests
-    const INITIAL_PAGE_SIZE: usize = 4;
+    const INITIAL_PAGE_SIZE: usize = 2;
 
     // Constants not overridden
     #[cfg(target_pointer_width = "64")]
@@ -70,33 +70,34 @@ mod tiny_slab {
 
     #[test]
     fn remove_remote_and_reuse() {
-        loom::model(|| {
+        let mut model = loom::model::Builder::new();
+        model.max_branches = 100000;
+        model.check(|| {
+            println!("\n --- iteration ---\n");
             let slab = Arc::new(Slab::new());
 
             let idx1 = slab.insert(1).expect("insert");
             let idx2 = slab.insert(2).expect("insert");
-            let idx3 = slab.insert(3).expect("insert");
-            let idx4 = slab.insert(4).expect("insert");
 
             assert_eq!(slab.get(idx1), Some(&1), "slab: {:#?}", slab);
             assert_eq!(slab.get(idx2), Some(&2), "slab: {:#?}", slab);
-            assert_eq!(slab.get(idx3), Some(&3), "slab: {:#?}", slab);
-            assert_eq!(slab.get(idx4), Some(&4), "slab: {:#?}", slab);
 
             let s = slab.clone();
             let t1 = thread::spawn(move || {
                 assert_eq!(s.remove(idx1), Some(1), "slab: {:#?}", s);
-                assert_eq!(s.get(idx1), None);
+                let value = s.get(idx1);
+
+                // We may or may not see the new value yet, depending on when
+                // this occurs, but we must either  see the new value or `None`;
+                // the old value has been removed!
+                assert!(value == None || value == Some(&3));
             });
 
-            let idx5 = slab.insert(5).expect("insert");
+            let idx3 = slab.insert(3).expect("insert");
             t1.join().expect("thread 1 should not panic");
 
-            assert_eq!(slab.get(idx5), Some(&5), "slab: {:#?}", slab);
-            assert_eq!(slab.get(idx2), Some(&2), "slab: {:#?}", slab);
             assert_eq!(slab.get(idx3), Some(&3), "slab: {:#?}", slab);
-            assert_eq!(slab.get(idx4), Some(&4), "slab: {:#?}", slab);
-            assert_eq!(slab.get(idx1), None, "slab: {:#?}", slab);
+            assert_eq!(slab.get(idx2), Some(&2), "slab: {:#?}", slab);
         });
     }
 
