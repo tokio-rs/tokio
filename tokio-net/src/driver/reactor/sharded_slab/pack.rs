84f66d771e71dac4e9b0c43d6529f1bd9a054204
Eliza Weisman <eliza@buoyant.io>
2019-10-11 12:26:15 -0700

    add comments to `Pack`
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-net/src/driver/reactor/sharded_slab/pack.rs
+++ tokio-net/src/driver/reactor/sharded_slab/pack.rs
@@ -1,16 +1,49 @@
 pub(super) const WIDTH: usize = std::mem::size_of::<usize>() * 8;
 
+/// Trait encapsulating the calculations required for bit-packing slab indices.
+///
+/// This allows us to avoid manually repeating some calculations when packing
+/// and unpacking indices.
 pub(super) trait Pack: Sized {
-    const LEN: usize;
+    // ====== provided by each implementation =================================
 
+    /// The number of bits occupied by this type when packed into a usize.
+    ///
+    /// This must be provided to determine the number of bits into which to pack
+    /// the type.
+    const LEN: usize;
+    /// The type packed on the less significant side of this type.
+    ///
+    /// If this type is packed into the least significant bit of a usize, this
+    /// should be `()`, which occupies no bytes.
+    ///
+    /// This is used to calculate the shift amount for packing this value.
+    type Prev: Pack;
+
+    // ====== calculated automatically ========================================
+
+    /// A number consisting of `Self::LEN` 1 bits, starting at the least
+    /// significant bit.
+    ///
+    /// This is the higest value this type can represent. This number is shifted
+    /// left by `Self::SHIFT` bits to calculate this type's `MASK`.
+    ///
+    /// This is computed automatically based on `Self::LEN`.
     const BITS: usize = {
         let shift = 1 << (Self::LEN - 1);
         shift | (shift - 1)
     };
+    /// The number of bits to shift a number to pack it into a usize with other
+    /// values.
+    ///
+    /// This is caculated automatically based on the `LEN` and `SHIFT` constants
+    /// of the previous value.
     const SHIFT: usize = Self::Prev::SHIFT + Self::Prev::LEN;
-    const MASK: usize = Self::BITS << Self::SHIFT;
 
-    type Prev: Pack;
+    /// The mask to extract only this type from a packed `usize`.
+    ///
+    /// This is calculated by shifting `Self::BITS` left by `Self::SHIFT`.
+    const MASK: usize = Self::BITS << Self::SHIFT;
 
     fn as_usize(&self) -> usize;
     fn from_usize(val: usize) -> Self;
