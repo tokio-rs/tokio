a01853ba99cda9284490c42735158289e3c50577
Carl Lerche <me@carllerche.com>
2019-10-20 21:06:49 -0700

    fs: move into `tokio`
    A step towards collapsing Tokio sub crates into a single `tokio`
    crate (#1318).
    
    The `fs` implementation is now provided by the main `tokio` crate. The
    `fs` functionality may still be excluded from the build by skipping the
    `fs` feature flag.

---
+++ tokio/src/fs/create_dir.rs
@@ -0,0 +1,14 @@
+use crate::fs::asyncify;
+
+use std::io;
+use std::path::Path;
+
+/// Creates a new, empty directory at the provided path
+///
+/// This is an async version of [`std::fs::create_dir`][std]
+///
+/// [std]: https://doc.rust-lang.org/std/fs/fn.create_dir.html
+pub async fn create_dir<P: AsRef<Path>>(path: P) -> io::Result<()> {
+    let path = path.as_ref().to_owned();
+    asyncify(move || std::fs::create_dir(path)).await
+}
---
+++ tokio/src/fs/create_dir_all.rs
@@ -0,0 +1,15 @@
+use crate::fs::asyncify;
+
+use std::io;
+use std::path::Path;
+
+/// Recursively create a directory and all of its parent components if they
+/// are missing.
+///
+/// This is an async version of [`std::fs::create_dir_all`][std]
+///
+/// [std]: https://doc.rust-lang.org/std/fs/fn.create_dir_all.html
+pub async fn create_dir_all<P: AsRef<Path>>(path: P) -> io::Result<()> {
+    let path = path.as_ref().to_owned();
+    asyncify(move || std::fs::create_dir_all(path)).await
+}
