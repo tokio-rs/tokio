84c270153d8238da6db64ae4ed4bb6a1b9e53c8d
Eliza Weisman <eliza@buoyant.io>
2019-10-26 13:19:10 -0700

    document confusing methods
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio/src/net/driver/reactor/dispatch/page/scheduled_io.rs
+++ tokio/src/net/driver/reactor/dispatch/page/scheduled_io.rs
@@ -102,6 +102,15 @@ impl ScheduledIo {
         })
     }
 
+    /// Returns the current readiness value of this `ScheduledIo`, if the
+    /// provided `token` is still a valid access.
+    ///
+    /// # Returns
+    ///
+    /// If the given token's generation no longer matches the `ScheduledIo`'s
+    /// generation, then the corresponding IO resource has been removed and
+    /// replaced with a new resource. In that case, this method returns `None`.
+    /// Otherwise, this returns the current readiness.
     pub(in crate::net::driver) fn get_readiness(&self, token: usize) -> Option<usize> {
         let gen = token & Generation::MASK;
         let ready = self.readiness.load(Ordering::Acquire);
@@ -111,6 +120,20 @@ impl ScheduledIo {
         Some(ready & (!Generation::MASK))
     }
 
+    /// Sets the readiness on this `ScheduledIo` by invoking the given closure on
+    /// the current value, returning the previous readiness value.
+    ///
+    /// # Arguments
+    /// - `token`: the token for this `ScheduledIo`.
+    /// - `f`: a closure returning a new readiness value given the previous
+    ///   readiness.
+    ///
+    /// # Returns
+    ///
+    /// If the given token's generation no longer matches the `ScheduledIo`'s
+    /// generation, then the corresponding IO resource has been removed and
+    /// replaced with a new resource. In that case, this method returns `Err`.
+    /// Otherwise, this returns the previous readiness.
     pub(in crate::net::driver) fn set_readiness(
         &self,
         token: usize,
@@ -119,21 +142,28 @@ impl ScheduledIo {
         let gen = token & Generation::MASK;
         let mut current = self.readiness.load(Ordering::Acquire);
         loop {
+            // Check that the generation for this access is still the current
+            // one.
             if current & Generation::MASK != gen {
                 return Err(());
             }
-            let new = f(current & mio::Ready::all().as_usize());
-            debug_assert!(new < Generation::ONE);
+            // Mask out the generation bits so that the modifying function
+            // doesn't see them.
+            let current_readiness = current & mio::Ready::all().as_usize();
+            let new = f(current_readiness);
+            debug_assert!(
+                new < Generation::ONE,
+                "new readiness value would overwrite generation bits!"
+            );
+
             match self.readiness.compare_exchange(
                 current,
                 new | gen,
                 Ordering::AcqRel,
                 Ordering::Acquire,
             ) {
-                Ok(actual) => {
-                    debug_assert_eq!(actual, current);
-                    return Ok(actual);
-                }
+                Ok(_) => return Ok(current),
+                // we lost the race, retry!
                 Err(actual) => current = actual,
             }
         }
