2005ad9e27565689de03a91399c71664be7a2adb
Markus Westerlind <markus.westerlind@imperva.com>
2019-10-29 10:23:19 +0100

    test: Verify that read_into_buf can read more bytes without consuming first

--- tokio/tests/io_read_into_buf.rs
+++ tokio/tests/io_read_into_buf.rs
@@ -2,7 +2,7 @@
 
 use std::pin::Pin;
 
-use tokio::io::{AsyncBufRead, AsyncBufReadExt, BufReader};
+use tokio::io::{AsyncBufRead, AsyncBufReadExt, AsyncReadExt, BufReader};
 use tokio_test::assert_ok;
 
 #[tokio::test]
@@ -30,4 +30,36 @@ async fn read_into_buf() {
     let read = assert_ok!(rd.read_into_buf().await);
     assert_eq!(read, 1);
     assert_eq!(Pin::new(&mut rd).get_buf(), b"5");
+}
+
+#[tokio::test]
+async fn read_into_buf_read_again() {
+    let rd = b"1".chain(&b"23"[..]);
+    let mut rd = BufReader::with_capacity(2, rd);
+
+    let read = assert_ok!(rd.read_into_buf().await);
+    assert_eq!(read, 1);
+    assert_eq!(Pin::new(&mut rd).get_buf(), b"1");
+
+    let read = assert_ok!(rd.read_into_buf().await);
+    assert_eq!(read, 1);
+    assert_eq!(Pin::new(&mut rd).get_buf(), b"12");
+
+    Pin::new(&mut rd).consume(1);
+
+    assert_eq!(Pin::new(&mut rd).get_buf(), b"2");
+
+    Pin::new(&mut rd).consume(1);
+
+    assert_eq!(Pin::new(&mut rd).get_buf(), b"");
+
+    let read = assert_ok!(rd.read_into_buf().await);
+    assert_eq!(read, 1);
+    assert_eq!(Pin::new(&mut rd).get_buf(), b"3");
+
+    Pin::new(&mut rd).consume(1);
+
+    let read = assert_ok!(rd.read_into_buf().await);
+    assert_eq!(read, 0);
+    assert_eq!(Pin::new(&mut rd).get_buf(), b"");
 }
