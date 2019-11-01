7bde9ad191c490b3cb0b16078e974c4af442a149
Carl Lerche <me@carllerche.com>
2019-11-01 12:04:12 -0700

    compat: extract crate to a dedicated git repo
    The compat crate is moved to https://github.com/tokio-rs/tokio-compat.
    This allows pinning it to specific revisions of the Tokio git
    repository. The master branch is intended to go through significant
    churn and it will be easier to update the compat layer in batches.

--- tokio-compat/src/prelude.rs
+++
@@ -1,4 +0,0 @@
-//! A prelude for `tokio` 0.1/0.2 compatibility.
-#[cfg(feature = "sink")]
-pub use futures_util::compat::Sink01CompatExt as _;
-pub use futures_util::compat::{Future01CompatExt as _, Stream01CompatExt as _};
