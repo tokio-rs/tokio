cbe369a3ed5c252ca7581e37986dc912d88e58c6
Artem Vorotnikov <artem@vorotnikov.me>
2019-12-04 10:51:23 -0800

    Make JoinError Sync (#1888)
    * Make JoinError Sync
    
    * Move Mutex inside JoinError internals, hide its constructors
    
    * Deprecate JoinError constructors, fix internal usages

--- tokio/src/task/error.rs
+++ tokio/src/task/error.rs
@@ -1,6 +1,7 @@
 use std::any::Any;
 use std::fmt;
 use std::io;
+use std::sync::Mutex;
 
 doc_rt_core! {
     /// Task failed to execute to completion.
@@ -11,21 +12,31 @@ doc_rt_core! {
 
 enum Repr {
     Cancelled,
-    Panic(Box<dyn Any + Send + 'static>),
+    Panic(Mutex<Box<dyn Any + Send + 'static>>),
 }
 
 impl JoinError {
-    /// Create a new `cancelled` error
+    #[doc(hidden)]
+    #[deprecated]
     pub fn cancelled() -> JoinError {
+        Self::cancelled2()
+    }
+
+    pub(crate) fn cancelled2() -> JoinError {
         JoinError {
             repr: Repr::Cancelled,
         }
     }
 
-    /// Create a new `panic` error
+    #[doc(hidden)]
+    #[deprecated]
     pub fn panic(err: Box<dyn Any + Send + 'static>) -> JoinError {
+        Self::panic2(err)
+    }
+
+    pub(crate) fn panic2(err: Box<dyn Any + Send + 'static>) -> JoinError {
         JoinError {
-            repr: Repr::Panic(err),
+            repr: Repr::Panic(Mutex::new(err)),
         }
     }
 }
