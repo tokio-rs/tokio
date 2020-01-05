aa0fdf002b1ac2a9f2c0ebe59a4168f4ecd532db
John VanEnk <vanenkj@gmail.com>
2020-01-04 23:58:26 -0800

    Clarify that Sender and Receiver are only Send and Sync if T is Send or Sync.

--- tokio/src/sync/broadcast.rs
+++ tokio/src/sync/broadcast.rs
@@ -3,7 +3,8 @@
 //!
 //! A [`Sender`] is used to broadcast values to **all** connected [`Receiver`]
 //! values. [`Sender`] handles are clone-able, allowing concurrent send and
-//! receive actions. [`Sender`] and [`Receiver`] are both `Send` and `Sync`.
+//! receive actions. [`Sender`] and [`Receiver`] are both `Send` and `Sync` as
+//! long as `T` is also `Send` or `Sync` respectively.
 //!
 //! When a value is sent, **all** [`Receiver`] handles are notified and will
 //! receive the value. The value is stored once inside the channel and cloned on
