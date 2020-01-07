b652306eb43d3def938c146990d03ecfa5ad579a
Carl Lerche <me@carllerche.com>
2020-01-06 22:40:57 -0800

    fix conditional comnpilation

--- tokio/src/runtime/context.rs
+++ tokio/src/runtime/context.rs
@@ -1,5 +1,6 @@
 //! Thread local runtime context
-use crate::runtime::{self, Handle, Spawner};
+use crate::runtime::Handle;
+
 use std::cell::RefCell;
 
 thread_local! {
@@ -10,32 +11,40 @@ pub(crate) fn current() -> Option<Handle> {
     CONTEXT.with(|ctx| ctx.borrow().clone())
 }
 
-pub(crate) fn io_handle() -> runtime::io::Handle {
-    CONTEXT.with(|ctx| match *ctx.borrow() {
-        Some(ref ctx) => ctx.io_handle.clone(),
-        None => Default::default(),
-    })
+cfg_io_driver! {
+    pub(crate) fn io_handle() -> crate::runtime::io::Handle {
+        CONTEXT.with(|ctx| match *ctx.borrow() {
+            Some(ref ctx) => ctx.io_handle.clone(),
+            None => Default::default(),
+        })
+    }
 }
 
-pub(crate) fn time_handle() -> crate::runtime::time::Handle {
-    CONTEXT.with(|ctx| match *ctx.borrow() {
-        Some(ref ctx) => ctx.time_handle.clone(),
-        None => Default::default(),
-    })
+cfg_time! {
+    pub(crate) fn time_handle() -> crate::runtime::time::Handle {
+        CONTEXT.with(|ctx| match *ctx.borrow() {
+            Some(ref ctx) => ctx.time_handle.clone(),
+            None => Default::default(),
+        })
+    }
+
+    cfg_test_util! {
+        pub(crate) fn clock() -> Option<crate::runtime::time::Clock> {
+            CONTEXT.with(|ctx| match *ctx.borrow() {
+                Some(ref ctx) => Some(ctx.clock.clone()),
+                None => None,
+            })
+        }
+    }
 }
 
-pub(crate) fn spawn_handle() -> Option<Spawner> {
-    CONTEXT.with(|ctx| match *ctx.borrow() {
-        Some(ref ctx) => Some(ctx.spawner.clone()),
-        None => None,
-    })
-}
-
-pub(crate) fn clock() -> Option<crate::runtime::time::Clock> {
-    CONTEXT.with(|ctx| match *ctx.borrow() {
-        Some(ref ctx) => Some(ctx.clock.clone()),
-        None => None,
-    })
+cfg_rt_core! {
+    pub(crate) fn spawn_handle() -> Option<crate::runtime::Spawner> {
+        CONTEXT.with(|ctx| match *ctx.borrow() {
+            Some(ref ctx) => Some(ctx.spawner.clone()),
+            None => None,
+        })
+    }
 }
 
 /// Set this [`ThreadContext`] as the current active [`ThreadContext`].
