780d6f91a0b56c9cbfca01a9a4694f21eb670f96
Alice Ryhl <alice@ryhl.io>
2020-01-07 18:17:01 +0100

    docs: improve tokio::io API documentation (#2060)
    * Links are added where missing and examples are improved.
    * Improve `stdin`, `stdout`, and `stderr` documentation by going
      into more details regarding what can go wrong in concurrent
      situations and provide examples for `stdout` and `stderr`.

--- tokio/src/io/split.rs
+++ tokio/src/io/split.rs
@@ -17,12 +17,12 @@ use std::sync::Arc;
 use std::task::{Context, Poll};
 
 cfg_io_util! {
-    /// The readable half of a value returned from `split`.
+    /// The readable half of a value returned from [`split`](split()).
     pub struct ReadHalf<T> {
         inner: Arc<Inner<T>>,
     }
 
-    /// The writable half of a value returned from `split`.
+    /// The writable half of a value returned from [`split`](split()).
     pub struct WriteHalf<T> {
         inner: Arc<Inner<T>>,
     }
@@ -30,8 +30,8 @@ cfg_io_util! {
     /// Split a single value implementing `AsyncRead + AsyncWrite` into separate
     /// `AsyncRead` and `AsyncWrite` handles.
     ///
-    /// To restore this read/write object from its `split::ReadHalf` and
-    /// `split::WriteHalf` use `unsplit`.
+    /// To restore this read/write object from its `ReadHalf` and
+    /// `WriteHalf` use [`unsplit`](ReadHalf::unsplit()).
     pub fn split<T>(stream: T) -> (ReadHalf<T>, WriteHalf<T>)
     where
         T: AsyncRead + AsyncWrite,
