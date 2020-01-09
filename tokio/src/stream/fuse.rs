f58317574d491a8a6768c7438112f69233c0d241
Carl Lerche <me@carllerche.com>
2020-01-09 15:07:35 -0800

    stream: add `StreamExt::fuse`
    Provides an async equivalent to `Iterator::fuse`.

---
+++ tokio/src/stream/fuse.rs
@@ -0,0 +1,54 @@
+use crate::stream::Stream;
+
+use pin_project_lite::pin_project;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+pin_project! {
+    /// Stream returned by [`fuse()`][super::StreamExt::fuse].
+    #[derive(Debug)]
+    pub struct Fuse<T> {
+        #[pin]
+        stream: Option<T>,
+    }
+}
+
+impl<T> Fuse<T>
+where
+    T: Stream,
+{
+    pub(crate) fn new(stream: T) -> Fuse<T> {
+        Fuse { stream: Some(stream) }
+    }
+}
+
+impl<T> Stream for Fuse<T>
+where
+    T: Stream,
+{
+    type Item = T::Item;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<T::Item>> {
+        let res = unsafe {
+            match self.as_mut().project().stream.get_unchecked_mut() {
+                Some(stream) => ready!(Pin::new_unchecked(stream).poll_next(cx)),
+                None => return Poll::Ready(None),
+            }
+        };
+
+        if res.is_none() {
+            // Do not poll the stream anymore
+            self.as_mut().project().stream.set(None);
+        }
+
+        Poll::Ready(res)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        if self.stream.is_none() {
+            (0, Some(0))
+        } else {
+            unimplemented!();
+        }
+    }
+}
