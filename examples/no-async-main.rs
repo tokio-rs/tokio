ada08d4bf09cef7c35cd27d0c9ffaea65c46951f
Sarah Allen <sarah@ultrasaurus.com>
2020-02-19 08:34:57 -0800

    rustfmt applied

--- examples/no-async-main.rs
+++ examples/no-async-main.rs
@@ -5,19 +5,19 @@
 //!
 //!     cargo run --example no-async-main
 
-use tokio::sync::{oneshot, mpsc};
 use tokio::runtime::{Handle, Runtime};
+use tokio::sync::{mpsc, oneshot};
 use Command::Increment;
 
 #[derive(Debug)]
 enum Command {
-    Increment,
-    // Other commands can be added here
+  Increment,
+  // Other commands can be added here
 }
 
 #[derive(Debug)]
 enum Response {
-  IncrementCompleted(u64)
+  IncrementCompleted(u64),
 }
 
 struct Commander {
@@ -27,7 +27,7 @@ struct Commander {
 #[derive(Clone)]
 struct Sender {
   handle: Handle,
-  cmd_tx: mpsc::Sender<(Command, oneshot::Sender<Response>)>
+  cmd_tx: mpsc::Sender<(Command, oneshot::Sender<Response>)>,
 }
 
 impl Commander {
@@ -43,33 +43,28 @@ impl Commander {
       let mut counter: u64 = 0;
       while let Some((cmd, response)) = cmd_rx.recv().await {
         match cmd {
-            Increment => {
-                counter += 1;
-                response.send(Response::IncrementCompleted(counter)).unwrap();
-            }
+          Increment => {
+            counter += 1;
+            response
+              .send(Response::IncrementCompleted(counter))
+              .unwrap();
+          }
         }
       }
     });
     ready_callback(Sender::new(cmd_tx, self.runtime.handle().clone()));
   }
-
 }
 
 impl Sender {
-  pub fn new(cmd_tx: mpsc::Sender<(Command, oneshot::Sender<Response>)>,
-             handle: Handle) -> Self {
-    Sender {
-      handle,
-      cmd_tx
-    }
+  pub fn new(cmd_tx: mpsc::Sender<(Command, oneshot::Sender<Response>)>, handle: Handle) -> Self {
+    Sender { handle, cmd_tx }
   }
 
-  pub fn send_command(&mut self, cmd: Command, f: impl Fn(Response) -> () + Send + 'static)
-  {
+  pub fn send_command(&mut self, cmd: Command, f: impl Fn(Response) -> () + Send + 'static) {
     let (resp_tx, resp_rx) = oneshot::channel::<Response>();
     let mut cmd_tx = self.cmd_tx.clone();
     self.handle.spawn(async move {
-
       cmd_tx.send((cmd, resp_tx)).await.ok().unwrap();
       let res: Response = resp_rx.await.unwrap();
 
@@ -79,18 +74,18 @@ impl Sender {
   }
 } // impl Sender
 
-
 fn main() {
   let c = Commander::new();
   c.connect(move |mut sender| {
-      println!("Yay!");
-      sender.send_command(Increment, |response| {
-        println!("  ==> received response {:?}", response);
-      })
+    println!("Yay!");
+    sender.send_command(Increment, |response| {
+      println!("  ==> received response {:?}", response);
+    })
   });
 
   let mut input = String::new();
   println!("press return to quit");
-  std::io::stdin().read_line(&mut input).expect("stdio read_line");
-
+  std::io::stdin()
+    .read_line(&mut input)
+    .expect("stdio read_line");
 }
