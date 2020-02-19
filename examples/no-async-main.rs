0ecdcb1c3045f1da692ec0ba8036c3e6a5492320
Sarah Allen <sarah@ultrasaurus.com>
2020-02-18 19:42:29 -0800

    WIP -- example does not compile

---
+++ examples/no-async-main.rs
@@ -0,0 +1,102 @@
+//! A simple client that does some work and triggers a callback asynchronously
+//! with an API can be called from no-async code and uses async tokio inside
+//!
+//!  THIS DOES NOT WORK YET 
+//! -- it does not compile 
+//! -- may not be using the best idioms for what I'm attempting to illustrate
+//!
+//!
+//! And then in another terminal run:
+//!
+//!     cargo run --example no-async-main
+
+use std::net::SocketAddr;
+use tokio::sync::{oneshot, mpsc};
+use tokio::runtime::Runtime;
+use Command::Increment;
+
+#[derive(Debug)]
+enum Command {
+    Increment,
+    // Other commands can be added here
+}
+
+#[derive(Debug)]
+enum Response {
+  IncrementCompleted(u64)
+}
+
+struct Commander {
+  runtime: Runtime,
+  cmd_tx: Option<mpsc::Sender<(Command, oneshot::Sender<Response>)>>
+  // join_handles: Vec<>  = vec![]
+}
+
+impl Commander {
+  pub fn new() -> Commander {
+    Commander {
+      runtime: Runtime::new().unwrap(),
+      cmd_tx: None
+    }
+  }
+
+  pub fn send_command(&mut self, cmd: Command, f: impl Fn(Response) -> () + Send + 'static)
+  {
+    if let Some(self_cmd_tx) = &self.cmd_tx {
+      let (resp_tx, resp_rx) = oneshot::channel::<Response>();
+
+      let mut cmd_tx = self_cmd_tx.clone();
+      self.runtime.spawn(async move {
+
+            cmd_tx.send((cmd, resp_tx)).await.ok().unwrap();
+            let res: Response = resp_rx.await.unwrap();
+
+            println!("=> {:?}", res);
+            f(res);
+        });
+    } else {
+      panic!("need to connect before calling send_command")
+    }
+  }
+
+  pub fn connect(&mut self, _addr: SocketAddr,
+                            f: impl FnMut(bool) -> () + Send + 'static) {
+    let (cmd_tx, mut cmd_rx) = mpsc::channel::<(Command, oneshot::Sender<Response>)>(100);
+    self.cmd_tx = Some(cmd_tx);
+
+    // Spawn a task to manage the counter
+    self.runtime.spawn(async move {
+      // imagine we connect TCPsocket or something other slow thing here
+      f(true);
+      let mut counter: u64 = 0;
+      while let Some((cmd, response)) = cmd_rx.recv().await {
+        match cmd {
+            Increment => {
+                counter += 1;
+                response.send(Response::IncrementCompleted(counter)).unwrap();
+            }
+        }
+      }
+    });
+  }
+} // impl Commander
+
+fn main() {
+  let addr: SocketAddr = "127.0.0.1:1234".parse().unwrap();
+  let mut c = Commander::new();
+  c.connect(addr, move |connected| {
+    if connected {
+      println!("Yay!");
+      c.send_command(Increment, |response| {
+        println!("received response {:?}", response);
+      })
+    } else {
+      println!("sorry.. try again later");
+    }
+  });
+
+  let mut input = String::new();
+  println!("press return to quit");
+  std::io::stdin().read_line(&mut input).expect("stdio read_line");
+
+}
