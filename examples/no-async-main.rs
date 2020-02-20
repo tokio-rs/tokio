9b5c86d27e26a8ceb2a87c84edb6267891ecc91d
Sarah Allen <sarah@ultrasaurus.com>
2020-02-19 16:18:54 -0800

    it seems tokio wants tab = 4 spaces

--- examples/no-async-main.rs
+++ examples/no-async-main.rs
@@ -4,88 +4,87 @@
 //! To run the example:
 //!
 //!     cargo run --example no-async-main
-
 use tokio::runtime::{Handle, Runtime};
 use tokio::sync::{mpsc, oneshot};
 use Command::Increment;
 
 #[derive(Debug)]
 enum Command {
-  Increment,
-  // Other commands can be added here
+    Increment,
+    // Other commands can be added here
 }
 
 #[derive(Debug)]
 enum Response {
-  IncrementCompleted(u64),
+    IncrementCompleted(u64),
 }
 
 struct Commander {
-  runtime: Runtime,
+    runtime: Runtime,
 }
 
 #[derive(Clone)]
 struct Sender {
-  handle: Handle,
-  cmd_tx: mpsc::Sender<(Command, oneshot::Sender<Response>)>,
+    handle: Handle,
+    cmd_tx: mpsc::Sender<(Command, oneshot::Sender<Response>)>,
 }
 
 impl Commander {
-  pub fn new() -> Commander {
-    Commander {
-      runtime: Runtime::new().unwrap(),
-    }
-  }
-  pub fn connect(&self, ready_callback: impl Fn(Sender) -> () + Send + 'static) {
-    let (cmd_tx, mut cmd_rx) = mpsc::channel::<(Command, oneshot::Sender<Response>)>(100);
-    // Spawn a task to manage the counter
-    self.runtime.spawn(async move {
-      let mut counter: u64 = 0;
-      while let Some((cmd, response)) = cmd_rx.recv().await {
-        match cmd {
-          Increment => {
-            counter += 1;
-            response
-              .send(Response::IncrementCompleted(counter))
-              .unwrap();
-          }
+    pub fn new() -> Commander {
+        Commander {
+            runtime: Runtime::new().unwrap(),
         }
-      }
-    });
-    ready_callback(Sender::new(cmd_tx, self.runtime.handle().clone()));
-  }
+    }
+    pub fn connect(&self, ready_callback: impl Fn(Sender) -> () + Send + 'static) {
+        let (cmd_tx, mut cmd_rx) = mpsc::channel::<(Command, oneshot::Sender<Response>)>(100);
+        // Spawn a task to manage the counter
+        self.runtime.spawn(async move {
+            let mut counter: u64 = 0;
+            while let Some((cmd, response)) = cmd_rx.recv().await {
+                match cmd {
+                    Increment => {
+                        counter += 1;
+                        response
+                            .send(Response::IncrementCompleted(counter))
+                            .unwrap();
+                    }
+                }
+            }
+        });
+        ready_callback(Sender::new(cmd_tx, self.runtime.handle().clone()));
+    }
 }
 
 impl Sender {
-  pub fn new(cmd_tx: mpsc::Sender<(Command, oneshot::Sender<Response>)>, handle: Handle) -> Self {
-    Sender { handle, cmd_tx }
-  }
+    pub fn new(cmd_tx: mpsc::Sender<(Command, oneshot::Sender<Response>)>, handle: Handle) -> Self {
+        Sender { handle, cmd_tx }
+    }
 
-  pub fn send_command(&mut self, cmd: Command, f: impl Fn(Response) -> () + Send + 'static) {
-    let (resp_tx, resp_rx) = oneshot::channel::<Response>();
-    let mut cmd_tx = self.cmd_tx.clone();
-    self.handle.spawn(async move {
-      cmd_tx.send((cmd, resp_tx)).await.ok().unwrap();
-      let res: Response = resp_rx.await.unwrap();
+    pub fn send_command(&mut self, cmd: Command, f: impl Fn(Response) -> () + Send + 'static) {
+        let (resp_tx, resp_rx) = oneshot::channel::<Response>();
+        let mut cmd_tx = self.cmd_tx.clone();
+        self.handle.spawn(async move {
+            cmd_tx.send((cmd, resp_tx)).await.ok().unwrap();
+            let res: Response = resp_rx.await.unwrap();
 
-      println!("  => {:?}", res);
-      f(res);
-    });
-  }
+            println!("  => {:?}", res);
+            f(res);
+        });
+    }
 } // impl Sender
 
 fn main() {
-  let c = Commander::new();
-  c.connect(move |mut sender| {
-    println!("Yay!");
-    sender.send_command(Increment, |response| {
-      println!("  ==> received response {:?}", response);
-    })
-  });
+    let c = Commander::new();
+    c.connect(move |mut sender| {
+        println!("Yay!");
+        sender.send_command(Increment, |response| {
+            println!("  ==> received response {:?}", response);
+        })
+    });
 
-  let mut input = String::new();
-  println!("press return to quit");
-  std::io::stdin()
-    .read_line(&mut input)
-    .expect("stdio read_line");
+    let mut input = String::new();
+    println!("press return to quit");
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("stdio read_line");
 }
