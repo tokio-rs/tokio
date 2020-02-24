357c0ac22b0bbe400cc0da5f2189fae280cd1929
Kevin Leimkuhler <kevin@kleimkuhler.com>
2020-02-23 18:05:38 -0800

    Do not run issue_2174 test on freebsd
    Signed-off-by: Kevin Leimkuhler <kevin@kleimkuhler.com>

--- tokio/tests/process_issue_2174.rs
+++ tokio/tests/process_issue_2174.rs
@@ -1,5 +1,13 @@
-#![cfg(all(unix, feature = "process"))]
+#![cfg(feature = "process")]
 #![warn(rust_2018_idioms)]
+// This test reveals a difference in behavior of kqueue on FreeBSD. When the
+// reader disconnects, there does not seem to be an `EVFILT_WRITE` filter that
+// is returned.
+//
+// It is expected that `EVFILT_WRITE` would be returned with either the
+// `EV_EOF` or `EV_ERROR` flag set. If either flag is set a write would be
+// attempted, but that does not seem to occur.
+#![cfg(all(unix, not(target_os = "freebsd")))]
 
 use std::process::Stdio;
 use std::time::Duration;
