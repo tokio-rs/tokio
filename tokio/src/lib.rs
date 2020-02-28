ecc23c084a64188e272e2c1e9e370189358a088b
Carl Lerche <me@carllerche.com>
2020-02-28 13:51:24 -0800

    chore: prepare v0.2.13 release (#2282)
    Includes a quick bug fix

--- tokio/src/lib.rs
+++ tokio/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tokio/0.2.12")]
+#![doc(html_root_url = "https://docs.rs/tokio/0.2.13")]
 #![allow(
     clippy::cognitive_complexity,
     clippy::large_enum_variant,
