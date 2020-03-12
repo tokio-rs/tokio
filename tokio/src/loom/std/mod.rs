9c7126bb0ceb8a6e7f29ef9474fb4ba9471f35af
Carl Lerche <me@carllerche.com>
2020-03-12 13:58:02 -0700

    fix warning

--- tokio/src/loom/std/mod.rs
+++ tokio/src/loom/std/mod.rs
@@ -64,7 +64,7 @@ pub(crate) mod sync {
         pub(crate) use crate::loom::std::atomic_u64::AtomicU64;
         pub(crate) use crate::loom::std::atomic_usize::AtomicUsize;
 
-        pub(crate) use std::sync::atomic::{AtomicU16, fence};
+        pub(crate) use std::sync::atomic::AtomicU16;
         pub(crate) use std::sync::atomic::{spin_loop_hint, AtomicBool};
     }
 }
