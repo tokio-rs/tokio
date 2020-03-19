5fd1b8f67c4a9976ed76f304484a4213283a3d6b
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-03-18 20:05:55 -0400

    util: Prepare `0.3.1` release (#2330)

--- tokio-util/src/lib.rs
+++ tokio-util/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tokio-util/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/tokio-util/0.3.1")]
 #![allow(clippy::needless_doctest_main)]
 #![warn(
     missing_debug_implementations,
