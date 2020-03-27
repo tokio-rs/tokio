0f9d3fe17e1b8200c01fe3d94a1ebe87f2c8a5fc
Carl Lerche <me@carllerche.com>
2020-03-27 15:53:14 -0700

    fix build

--- tokio/src/coop.rs
+++ tokio/src/coop.rs
@@ -98,8 +98,10 @@ where
     })
 }
 
-pub(crate) fn has_budget_remaining() -> bool {
-    HITS.with(|hits| hits.get() > 0)
+cfg_rt_threaded! {
+    pub(crate) fn has_budget_remaining() -> bool {
+        HITS.with(|hits| hits.get() > 0)
+    }
 }
 
 cfg_blocking_impl! {
