a922c04eeacff3c661bc54d095eeea4a50fab909
Kevin Leimkuhler <kevin@kleimkuhler.com>
2020-04-01 10:09:14 -0700

    Use asyncify
    Signed-off-by: Kevin Leimkuhler <kevin@kleimkuhler.com>

--- tokio/src/fs/copy.rs
+++ tokio/src/fs/copy.rs
@@ -1,5 +1,4 @@
-use crate::fs::File;
-use crate::io;
+use crate::fs::asyncify;
 use std::path::Path;
 
 /// Copies the contents of one file to another. This function will also copy the permission bits of the original file to the destination file.
@@ -19,16 +18,7 @@ use std::path::Path;
 /// ```
 
 pub async fn copy<P: AsRef<Path>, Q: AsRef<Path>>(from: P, to: Q) -> Result<u64, std::io::Error> {
-    let from = File::open(from).await?;
-    let to = File::create(to).await?;
-
-    // Do not set permissions on already existing non-files like pipes and
-    // fifos.
-    if to.metadata().await?.is_file() {
-        let from_permissions = from.metadata().await?.permissions();
-        to.set_permissions(from_permissions).await?;
-    }
-
-    let (mut from, mut to) = (io::BufReader::new(from), io::BufWriter::new(to));
-    io::copy(&mut from, &mut to).await
+    let from = from.as_ref().to_owned();
+    let to = to.as_ref().to_owned();
+    asyncify(|| std::fs::copy(from, to)).await
 }
