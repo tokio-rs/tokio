72dfb19f4dc911de33a855e865afe4db6bdef185
Alice Ryhl <alice@ryhl.io>
2020-04-04 15:39:17 +0200

    test: improve send/sync test macro

--- tokio/tests/async_send_sync.rs
+++ tokio/tests/async_send_sync.rs
@@ -42,78 +42,38 @@ macro_rules! into_todo {
     }};
 }
 macro_rules! async_assert_fn {
-    ($($f:ident)::+($($arg:ty),*): Send & Sync) => {
+    ($($f:ident $(< $($generic:ty),* > )? )::+($($arg:ty),*): Send & Sync) => {
         #[allow(unreachable_code)]
         #[allow(unused_variables)]
         const _: fn() = || {
-            let f = $($f)::+( $( into_todo!($arg) ),* );
+            let f = $($f $(::<$($generic),*>)? )::+( $( into_todo!($arg) ),* );
             require_send(&f);
             require_sync(&f);
         };
     };
-    ($($f:ident)::+($($arg:ty),*): Send & !Sync) => {
+    ($($f:ident $(< $($generic:ty),* > )? )::+($($arg:ty),*): Send & !Sync) => {
         #[allow(unreachable_code)]
         #[allow(unused_variables)]
         const _: fn() = || {
-            let f = $($f)::+( $( into_todo!($arg) ),* );
-            require_send(&f);
-            AmbiguousIfSync::some_item(&f);
-        };
-    };
-    ($($f:ident)::+($($arg:ty),*): !Send & Sync) => {
-        #[allow(unreachable_code)]
-        #[allow(unused_variables)]
-        const _: fn() = || {
-            let f = $($f)::+( $( into_todo!($arg) ),* );
-            AmbiguousIfSend::some_item(&f);
-            require_sync(&f);
-        };
-    };
-    ($($f:ident)::+($($arg:ty),*): !Send & !Sync) => {
-        #[allow(unreachable_code)]
-        #[allow(unused_variables)]
-        const _: fn() = || {
-            let f = $($f)::+( $( into_todo!($arg) ),* );
-            AmbiguousIfSend::some_item(&f);
-            AmbiguousIfSync::some_item(&f);
-        };
-    };
-    ($t:ty : $method:ident($($arg:ty),*): Send & Sync) => {
-        #[allow(unreachable_code)]
-        #[allow(unused_variables)]
-        const _: fn() = || {
-            let call_on: $t = todo!();
-            let f = call_on.$method( $( into_todo!($arg) ),* );
-            require_send(&f);
-            require_sync(&f);
-        };
-    };
-    ($t:ty : $method:ident($($arg:ty),*): Send & !Sync) => {
-        #[allow(unreachable_code)]
-        #[allow(unused_variables)]
-        const _: fn() = || {
-            let call_on: $t = todo!();
-            let f = call_on.$method( $( into_todo!($arg) ),* );
+            let f = $($f $(::<$($generic),*>)? )::+( $( into_todo!($arg) ),* );
             require_send(&f);
             AmbiguousIfSync::some_item(&f);
         };
     };
-    ($t:ty : $method:ident($($arg:ty),*): !Send & Sync) => {
+    ($($f:ident $(< $($generic:ty),* > )? )::+($($arg:ty),*): !Send & Sync) => {
         #[allow(unreachable_code)]
         #[allow(unused_variables)]
         const _: fn() = || {
-            let call_on: $t = todo!();
-            let f = call_on.$method( $( into_todo!($arg) ),* );
+            let f = $($f $(::<$($generic),*>)? )::+( $( into_todo!($arg) ),* );
             AmbiguousIfSend::some_item(&f);
             require_sync(&f);
         };
     };
-    ($t:ty : $method:ident($($arg:ty),*): !Send & !Sync) => {
+    ($($f:ident $(< $($generic:ty),* > )? )::+($($arg:ty),*): !Send & !Sync) => {
         #[allow(unreachable_code)]
         #[allow(unused_variables)]
         const _: fn() = || {
-            let call_on: $t = todo!();
-            let f = call_on.$method( $( into_todo!($arg) ),* );
+            let f = $($f $(::<$($generic),*>)? )::+( $( into_todo!($arg) ),* );
             AmbiguousIfSend::some_item(&f);
             AmbiguousIfSync::some_item(&f);
         };
@@ -128,8 +88,7 @@ async_assert_fn!(tokio::io::split(TcpStream): Send & Sync);
 async_assert_fn!(tokio::io::stderr(): Send & Sync);
 async_assert_fn!(tokio::io::stdin(): Send & Sync);
 async_assert_fn!(tokio::io::stdout(): Send & Sync);
-async_assert_fn!(tokio::io::Split<Cursor<Vec<u8>>>:next_segment(): Send & Sync);
-
+async_assert_fn!(tokio::io::Split<Cursor<Vec<u8>>>::next_segment(_): Send & Sync);
 
 async_assert_fn!(tokio::fs::canonicalize(&str): Send & Sync);
 async_assert_fn!(tokio::fs::copy(&str, &str): Send & Sync);
@@ -148,112 +107,104 @@ async_assert_fn!(tokio::fs::rename(&str, &str): Send & Sync);
 async_assert_fn!(tokio::fs::set_permissions(&str, std::fs::Permissions): Send & Sync);
 async_assert_fn!(tokio::fs::symlink_metadata(&str): Send & Sync);
 async_assert_fn!(tokio::fs::write(&str, Vec<u8>): Send & Sync);
-async_assert_fn!(tokio::fs::ReadDir:next_entry(): Send & Sync);
-async_assert_fn!(tokio::fs::OpenOptions:open(&str): Send & Sync);
-async_assert_fn!(tokio::fs::DirEntry:metadata(): Send & Sync);
-async_assert_fn!(tokio::fs::DirEntry:file_type(): Send & Sync);
+async_assert_fn!(tokio::fs::ReadDir::next_entry(_): Send & Sync);
+async_assert_fn!(tokio::fs::OpenOptions::open(_, &str): Send & Sync);
+async_assert_fn!(tokio::fs::DirEntry::metadata(_): Send & Sync);
+async_assert_fn!(tokio::fs::DirEntry::file_type(_): Send & Sync);
 
 async_assert_fn!(tokio::fs::File::open(&str): Send & Sync);
 async_assert_fn!(tokio::fs::File::create(&str): Send & Sync);
-async_assert_fn!(tokio::fs::File:seek(std::io::SeekFrom): Send & Sync);
-async_assert_fn!(tokio::fs::File:sync_all(): Send & Sync);
-async_assert_fn!(tokio::fs::File:sync_data(): Send & Sync);
-async_assert_fn!(tokio::fs::File:set_len(u64): Send & Sync);
-async_assert_fn!(tokio::fs::File:metadata(): Send & Sync);
-async_assert_fn!(tokio::fs::File:try_clone(): Send & Sync);
-async_assert_fn!(tokio::fs::File:into_std(): Send & Sync);
-async_assert_fn!(tokio::fs::File:set_permissions(std::fs::Permissions): Send & Sync);
+async_assert_fn!(tokio::fs::File::seek(_, std::io::SeekFrom): Send & Sync);
+async_assert_fn!(tokio::fs::File::sync_all(_): Send & Sync);
+async_assert_fn!(tokio::fs::File::sync_data(_): Send & Sync);
+async_assert_fn!(tokio::fs::File::set_len(_, u64): Send & Sync);
+async_assert_fn!(tokio::fs::File::metadata(_): Send & Sync);
+async_assert_fn!(tokio::fs::File::try_clone(_): Send & Sync);
+async_assert_fn!(tokio::fs::File::into_std(_): Send & Sync);
+async_assert_fn!(tokio::fs::File::set_permissions(_, std::fs::Permissions): Send & Sync);
 
 
 async_assert_fn!(tokio::net::lookup_host(SocketAddr): Send & Sync);
 async_assert_fn!(tokio::net::TcpListener::bind(SocketAddr): Send & Sync);
-async_assert_fn!(tokio::net::TcpListener:accept(): Send & Sync);
+async_assert_fn!(tokio::net::TcpListener::accept(_): Send & Sync);
 async_assert_fn!(tokio::net::TcpStream::connect(SocketAddr): Send & Sync);
-async_assert_fn!(tokio::net::TcpStream:peek(&mut [u8]): Send & Sync);
-async_assert_fn!(tokio::net::tcp::ReadHalf<'_>:peek(&mut [u8]): Send & Sync);
+async_assert_fn!(tokio::net::TcpStream::peek(_, &mut [u8]): Send & Sync);
+async_assert_fn!(tokio::net::tcp::ReadHalf::peek(_, &mut [u8]): Send & Sync);
 async_assert_fn!(tokio::net::UdpSocket::bind(SocketAddr): Send & Sync);
-async_assert_fn!(tokio::net::UdpSocket:connect(SocketAddr): Send & Sync);
-async_assert_fn!(tokio::net::UdpSocket:send(&[u8]): Send & Sync);
-async_assert_fn!(tokio::net::UdpSocket:recv(&mut [u8]): Send & Sync);
-async_assert_fn!(tokio::net::UdpSocket:send_to(&[u8], SocketAddr): Send & Sync);
-async_assert_fn!(tokio::net::UdpSocket:recv_from(&mut [u8]): Send & Sync);
-async_assert_fn!(tokio::net::udp::RecvHalf:recv(&mut [u8]): Send & Sync);
-async_assert_fn!(tokio::net::udp::RecvHalf:recv_from(&mut [u8]): Send & Sync);
-async_assert_fn!(tokio::net::udp::SendHalf:send(&[u8]): Send & Sync);
-async_assert_fn!(tokio::net::udp::SendHalf:send_to(&[u8], &SocketAddr): Send & Sync);
+async_assert_fn!(tokio::net::UdpSocket::connect(_, SocketAddr): Send & Sync);
+async_assert_fn!(tokio::net::UdpSocket::send(_, &[u8]): Send & Sync);
+async_assert_fn!(tokio::net::UdpSocket::recv(_, &mut [u8]): Send & Sync);
+async_assert_fn!(tokio::net::UdpSocket::send_to(_, &[u8], SocketAddr): Send & Sync);
+async_assert_fn!(tokio::net::UdpSocket::recv_from(_, &mut [u8]): Send & Sync);
+async_assert_fn!(tokio::net::udp::RecvHalf::recv(_, &mut [u8]): Send & Sync);
+async_assert_fn!(tokio::net::udp::RecvHalf::recv_from(_, &mut [u8]): Send & Sync);
+async_assert_fn!(tokio::net::udp::SendHalf::send(_, &[u8]): Send & Sync);
+async_assert_fn!(tokio::net::udp::SendHalf::send_to(_, &[u8], &SocketAddr): Send & Sync);
 async_assert_fn!(tokio::net::UnixListener::bind(&str): Send & Sync);
-async_assert_fn!(tokio::net::UnixListener:accept(): Send & Sync);
-async_assert_fn!(tokio::net::UnixDatagram:send(&[u8]): Send & Sync);
-async_assert_fn!(tokio::net::UnixDatagram:recv(&mut [u8]): Send & Sync);
-async_assert_fn!(tokio::net::UnixDatagram:send_to(&[u8], &str): Send & Sync);
-async_assert_fn!(tokio::net::UnixDatagram:recv_from(&mut [u8]): Send & Sync);
+async_assert_fn!(tokio::net::UnixListener::accept(_): Send & Sync);
+async_assert_fn!(tokio::net::UnixDatagram::send(_, &[u8]): Send & Sync);
+async_assert_fn!(tokio::net::UnixDatagram::recv(_, &mut [u8]): Send & Sync);
+async_assert_fn!(tokio::net::UnixDatagram::send_to(_, &[u8], &str): Send & Sync);
+async_assert_fn!(tokio::net::UnixDatagram::recv_from(_, &mut [u8]): Send & Sync);
 async_assert_fn!(tokio::net::UnixStream::connect(&str): Send & Sync);
 
 
-async_assert_fn!(tokio::process::Child:wait_with_output(): Send & Sync);
+async_assert_fn!(tokio::process::Child::wait_with_output(_): Send & Sync);
 async_assert_fn!(tokio::signal::ctrl_c(): Send & Sync);
 #[cfg(unix)]
-async_assert_fn!(tokio::signal::unix::Signal:recv(): Send & Sync);
+async_assert_fn!(tokio::signal::unix::Signal::recv(_): Send & Sync);
 
-const _: fn() = || {
-    let f = tokio::stream::empty::<Rc<u8>>();
-    require_send(&f);
-    require_sync(&f);
-};
-const _: fn() = || {
-    let f = tokio::stream::pending::<Rc<u8>>();
-    require_send(&f);
-    require_sync(&f);
-};
+async_assert_fn!(tokio::stream::empty<Rc<u8>>(): Send & Sync);
+async_assert_fn!(tokio::stream::pending<Rc<u8>>(): Send & Sync);
 async_assert_fn!(tokio::stream::iter(std::vec::IntoIter<u8>): Send & Sync);
 
 
-async_assert_fn!(tokio::sync::Barrier:wait(): Send & Sync);
-async_assert_fn!(tokio::sync::Mutex<u8>:lock(): Send & Sync);
-async_assert_fn!(tokio::sync::Mutex<Cell<u8>>:lock(): Send & Sync);
-async_assert_fn!(tokio::sync::Mutex<Rc<u8>>:lock(): !Send & !Sync);
-async_assert_fn!(tokio::sync::Notify:notified(): Send & !Sync);
-async_assert_fn!(tokio::sync::RwLock<u8>:read(): Send & Sync);
-async_assert_fn!(tokio::sync::RwLock<u8>:write(): Send & Sync);
-async_assert_fn!(tokio::sync::RwLock<Cell<u8>>:read(): !Send & !Sync);
-async_assert_fn!(tokio::sync::RwLock<Cell<u8>>:write(): !Send & !Sync);
-async_assert_fn!(tokio::sync::RwLock<Rc<u8>>:read(): !Send & !Sync);
-async_assert_fn!(tokio::sync::RwLock<Rc<u8>>:write(): !Send & !Sync);
-async_assert_fn!(tokio::sync::Semaphore:acquire(): Send & Sync);
+async_assert_fn!(tokio::sync::Barrier::wait(_): Send & Sync);
+async_assert_fn!(tokio::sync::Mutex<u8>::lock(_): Send & Sync);
+async_assert_fn!(tokio::sync::Mutex<Cell<u8>>::lock(_): Send & Sync);
+async_assert_fn!(tokio::sync::Mutex<Rc<u8>>::lock(_): !Send & !Sync);
+async_assert_fn!(tokio::sync::Notify::notified(_): Send & !Sync);
+async_assert_fn!(tokio::sync::RwLock<u8>::read(_): Send & Sync);
+async_assert_fn!(tokio::sync::RwLock<u8>::write(_): Send & Sync);
+async_assert_fn!(tokio::sync::RwLock<Cell<u8>>::read(_): !Send & !Sync);
+async_assert_fn!(tokio::sync::RwLock<Cell<u8>>::write(_): !Send & !Sync);
+async_assert_fn!(tokio::sync::RwLock<Rc<u8>>::read(_): !Send & !Sync);
+async_assert_fn!(tokio::sync::RwLock<Rc<u8>>::write(_): !Send & !Sync);
+async_assert_fn!(tokio::sync::Semaphore::acquire(_): Send & Sync);
 
-async_assert_fn!(tokio::sync::broadcast::Receiver<u8>:recv(): Send & Sync);
-async_assert_fn!(tokio::sync::broadcast::Receiver<Cell<u8>>:recv(): Send & Sync);
-async_assert_fn!(tokio::sync::broadcast::Receiver<Rc<u8>>:recv(): !Send & !Sync);
+async_assert_fn!(tokio::sync::broadcast::Receiver<u8>::recv(_): Send & Sync);
+async_assert_fn!(tokio::sync::broadcast::Receiver<Cell<u8>>::recv(_): Send & Sync);
+async_assert_fn!(tokio::sync::broadcast::Receiver<Rc<u8>>::recv(_): !Send & !Sync);
 
-async_assert_fn!(tokio::sync::mpsc::Receiver<u8>:recv(): Send & Sync);
-async_assert_fn!(tokio::sync::mpsc::Receiver<Cell<u8>>:recv(): Send & Sync);
-async_assert_fn!(tokio::sync::mpsc::Receiver<Rc<u8>>:recv(): !Send & !Sync);
-async_assert_fn!(tokio::sync::mpsc::Sender<u8>:send(u8): Send & Sync);
-async_assert_fn!(tokio::sync::mpsc::Sender<Cell<u8>>:send(Cell<u8>): Send & !Sync);
-async_assert_fn!(tokio::sync::mpsc::Sender<Rc<u8>>:send(Rc<u8>): !Send & !Sync);
+async_assert_fn!(tokio::sync::mpsc::Receiver<u8>::recv(_): Send & Sync);
+async_assert_fn!(tokio::sync::mpsc::Receiver<Cell<u8>>::recv(_): Send & Sync);
+async_assert_fn!(tokio::sync::mpsc::Receiver<Rc<u8>>::recv(_): !Send & !Sync);
+async_assert_fn!(tokio::sync::mpsc::Sender<u8>::send(_, u8): Send & Sync);
+async_assert_fn!(tokio::sync::mpsc::Sender<Cell<u8>>::send(_, Cell<u8>): Send & !Sync);
+async_assert_fn!(tokio::sync::mpsc::Sender<Rc<u8>>::send(_, Rc<u8>): !Send & !Sync);
 
-async_assert_fn!(tokio::sync::mpsc::UnboundedReceiver<u8>:recv(): Send & Sync);
-async_assert_fn!(tokio::sync::mpsc::UnboundedReceiver<Cell<u8>>:recv(): Send & Sync);
-async_assert_fn!(tokio::sync::mpsc::UnboundedReceiver<Rc<u8>>:recv(): !Send & !Sync);
+async_assert_fn!(tokio::sync::mpsc::UnboundedReceiver<u8>::recv(_): Send & Sync);
+async_assert_fn!(tokio::sync::mpsc::UnboundedReceiver<Cell<u8>>::recv(_): Send & Sync);
+async_assert_fn!(tokio::sync::mpsc::UnboundedReceiver<Rc<u8>>::recv(_): !Send & !Sync);
 
-async_assert_fn!(tokio::sync::watch::Receiver<u8>:recv(): Send & Sync);
-async_assert_fn!(tokio::sync::watch::Receiver<Cell<u8>>:recv(): !Send & !Sync);
-async_assert_fn!(tokio::sync::watch::Receiver<Rc<u8>>:recv(): !Send & !Sync);
-async_assert_fn!(tokio::sync::watch::Sender<u8>:closed(): Send & Sync);
-async_assert_fn!(tokio::sync::watch::Sender<Cell<u8>>:closed(): !Send & !Sync);
-async_assert_fn!(tokio::sync::watch::Sender<Rc<u8>>:closed(): !Send & !Sync);
+async_assert_fn!(tokio::sync::watch::Receiver<u8>::recv(_): Send & Sync);
+async_assert_fn!(tokio::sync::watch::Receiver<Cell<u8>>::recv(_): !Send & !Sync);
+async_assert_fn!(tokio::sync::watch::Receiver<Rc<u8>>::recv(_): !Send & !Sync);
+async_assert_fn!(tokio::sync::watch::Sender<u8>::closed(_): Send & Sync);
+async_assert_fn!(tokio::sync::watch::Sender<Cell<u8>>::closed(_): !Send & !Sync);
+async_assert_fn!(tokio::sync::watch::Sender<Rc<u8>>::closed(_): !Send & !Sync);
 
 
-async_assert_fn!(tokio::task::LocalKey<u32>:scope(u32, BoxFutureSync<()>): Send & Sync);
-async_assert_fn!(tokio::task::LocalKey<u32>:scope(u32, BoxFutureSend<()>): Send & !Sync);
-async_assert_fn!(tokio::task::LocalKey<u32>:scope(u32, BoxFuture<()>): !Send & !Sync);
-async_assert_fn!(tokio::task::LocalKey<Cell<u32>>:scope(Cell<u32>, BoxFutureSync<()>): Send & !Sync);
-async_assert_fn!(tokio::task::LocalKey<Cell<u32>>:scope(Cell<u32>, BoxFutureSend<()>): Send & !Sync);
-async_assert_fn!(tokio::task::LocalKey<Cell<u32>>:scope(Cell<u32>, BoxFuture<()>): !Send & !Sync);
-async_assert_fn!(tokio::task::LocalKey<Rc<u32>>:scope(Rc<u32>, BoxFutureSync<()>): !Send & !Sync);
-async_assert_fn!(tokio::task::LocalKey<Rc<u32>>:scope(Rc<u32>, BoxFutureSend<()>): !Send & !Sync);
-async_assert_fn!(tokio::task::LocalKey<Rc<u32>>:scope(Rc<u32>, BoxFuture<()>): !Send & !Sync);
-async_assert_fn!(tokio::task::LocalSet:run_until(BoxFutureSync<()>): !Send & !Sync);
+async_assert_fn!(tokio::task::LocalKey<u32>::scope(_, u32, BoxFutureSync<()>): Send & Sync);
+async_assert_fn!(tokio::task::LocalKey<u32>::scope(_, u32, BoxFutureSend<()>): Send & !Sync);
+async_assert_fn!(tokio::task::LocalKey<u32>::scope(_, u32, BoxFuture<()>): !Send & !Sync);
+async_assert_fn!(tokio::task::LocalKey<Cell<u32>>::scope(_, Cell<u32>, BoxFutureSync<()>): Send & !Sync);
+async_assert_fn!(tokio::task::LocalKey<Cell<u32>>::scope(_, Cell<u32>, BoxFutureSend<()>): Send & !Sync);
+async_assert_fn!(tokio::task::LocalKey<Cell<u32>>::scope(_, Cell<u32>, BoxFuture<()>): !Send & !Sync);
+async_assert_fn!(tokio::task::LocalKey<Rc<u32>>::scope(_, Rc<u32>, BoxFutureSync<()>): !Send & !Sync);
+async_assert_fn!(tokio::task::LocalKey<Rc<u32>>::scope(_, Rc<u32>, BoxFutureSend<()>): !Send & !Sync);
+async_assert_fn!(tokio::task::LocalKey<Rc<u32>>::scope(_, Rc<u32>, BoxFuture<()>): !Send & !Sync);
+async_assert_fn!(tokio::task::LocalSet::run_until(_, BoxFutureSync<()>): !Send & !Sync);
 
 
 async_assert_fn!(tokio::time::advance(Duration): Send & Sync);
@@ -265,4 +216,4 @@ async_assert_fn!(tokio::time::timeout(Duration, BoxFuture<()>): !Send & !Sync);
 async_assert_fn!(tokio::time::timeout_at(Instant, BoxFutureSync<()>): Send & Sync);
 async_assert_fn!(tokio::time::timeout_at(Instant, BoxFutureSend<()>): Send & !Sync);
 async_assert_fn!(tokio::time::timeout_at(Instant, BoxFuture<()>): !Send & !Sync);
-async_assert_fn!(tokio::time::Interval:tick(): Send & Sync);
+async_assert_fn!(tokio::time::Interval::tick(_): Send & Sync);
