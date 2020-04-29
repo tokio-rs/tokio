22545c89c72ec201474026df94875b2f1e505215
Boqin Qin <Bobbqqin@gmail.com>
2020-04-30 00:26:51 +0800

    io: update doc for poll_read_ready and poll_write_ready
    Co-Authored-By: Alice Ryhl <alice@ryhl.io>

--- tokio/src/io/poll_evented.rs
+++ tokio/src/io/poll_evented.rs
@@ -236,8 +236,8 @@ where
     ///
     /// # Warning
     ///
-    /// &self is to allow concurrently calling poll_read_ready and poll_write_ready.
-    /// But calling this function concurrently will mess up mio's internal state.
+    /// This method may not be called concurrently. It takes `&self` to allow
+    /// calling it concurrently with `poll_write_ready`.
     pub fn poll_read_ready(
         &self,
         cx: &mut Context<'_>,
