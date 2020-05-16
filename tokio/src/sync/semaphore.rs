5f6c32e7c680698b1e36439de3c382de1c9bb326
NOVA-ME <nova-me@whatsoo.org>
2020-05-16 17:44:27 +0800

    delete blank quote

--- tokio/src/sync/semaphore.rs
+++ tokio/src/sync/semaphore.rs
@@ -78,7 +78,7 @@ impl Semaphore {
     pub fn available_permits(&self) -> usize {
         self.ll_sem.available_permits()
     }
-    
+
     /// Adds `n` new permits to the semaphore.
     ///
     /// The maximum number of permits is `usize::MAX >> 3`, and this function will panic if the limit is exceeded.
