a256072c753d769774e352d79924399c4019503d
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-05-19 18:13:01 +0300

    attempt=4

--- tokio/src/io/mod.rs
+++ tokio/src/io/mod.rs
@@ -162,7 +162,7 @@ mod async_buf_read;
 pub use self::async_buf_read::AsyncBufRead;
 
 mod async_read;
-#[cfg(any(feature = "io-std", feature = "fs"))]
+#[cfg(any(feature = "io-std", feature = "fs", test))]
 pub(crate) use self::async_read::prepare_uninitialized_buffer_std_read;
 pub use self::async_read::AsyncRead;
 
