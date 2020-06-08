612c4a37e731ea32ff52990553c56719e844867d
Carl Lerche <me@carllerche.com>
2020-06-08 15:43:22 -0700

    fix warnings

--- tokio/tests/tcp_into_split.rs
+++ tokio/tests/tcp_into_split.rs
@@ -3,7 +3,6 @@
 
 use std::io::{Error, ErrorKind, Result};
 use std::io::{Read, Write};
-use std::sync::{Arc, Barrier};
 use std::{net, thread};
 
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
@@ -88,9 +87,6 @@ async fn drop_write() -> Result<()> {
 
     let listener = net::TcpListener::bind("127.0.0.1:0")?;
     let addr = listener.local_addr()?;
-
-    let barrier = Arc::new(Barrier::new(2));
-    let barrier2 = barrier.clone();
 
     let handle = thread::spawn(move || {
         let (mut stream, _) = listener.accept().unwrap();
