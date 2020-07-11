d4e4aef71c22540e8fc3b3829a050edda0573287
Eliza Weisman <eliza@buoyant.io>
2020-07-11 11:36:38 -0700

    examples: don't need enter/exit,
    The context shows the currently active task, so we _really_ only need
    `new` and `close` events.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- examples/chat.rs
+++ examples/chat.rs
@@ -45,7 +45,7 @@ use std::task::{Context, Poll};
 async fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::FULL)
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::ACTIVE)
         .with_target(false)
         .with_timer(())
         .init();
