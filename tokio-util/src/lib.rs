2d32bfba912fb99b387b444a2b5638e89296d780
Carl Lerche <me@carllerche.com>
2020-08-07 13:32:49 -0700

    chore: prepare for v0.3 breaking changes
    Bug fixes will be applied to the v0.2.x branch.

--- tokio-util/src/lib.rs
+++ tokio-util/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tokio-util/0.3.1")]
+#![doc(html_root_url = "https://docs.rs/tokio-util/0.4.0")]
 #![allow(clippy::needless_doctest_main)]
 #![warn(
     missing_debug_implementations,
