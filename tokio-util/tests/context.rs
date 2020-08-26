e4a0085cbd730291acf8e3fc9218d9f36dce7d20
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2020-08-26 19:41:34 +0200

    two runtime tests (WIP) - failing

--- tokio-util/tests/context.rs
+++ tokio-util/tests/context.rs
@@ -1,77 +1,34 @@
 #![warn(rust_2018_idioms)]
 
-use tokio::{net::TcpListener, sync::oneshot};
-use tokio_util::context::{HandleExt, TokioContext};
-
-use std::future::Future;
+use tokio_util::context::HandleExt;
 
 use tokio::runtime::Builder;
 
-struct ThreadPool {
-    inner: futures::executor::ThreadPool,
-    rt: tokio::runtime::Runtime,
-}
-
-impl ThreadPool {
-    fn spawn(&self, f: impl Future<Output = ()> + Send + 'static) {
-        let handle = self.rt.handle().clone();
-        let h: TokioContext<_> = handle.wrap(f);
-        self.inner.spawn_ok(h);
-    }
-}
-
+use tokio::time::*;
 #[test]
 fn tokio_context_with_another_runtime() {
-    let (tx, rx) = oneshot::channel();
-    let custom_executor: ThreadPool = {
-        // Spawn tokio runtime on a single background thread
-        // enabling IO and timers.
-        let rt = Builder::new()
-            .basic_scheduler()
-            .enable_all()
-            .core_threads(1)
-            .build()
-            .unwrap();
+    let mut rt1 = Builder::new()
+        .basic_scheduler()
+        .core_threads(1)
+        .build()
+        .unwrap();
+    let rt2 = Builder::new()
+        .basic_scheduler()
+        .core_threads(1)
+        .enable_all()
+        .build()
+        .unwrap();
 
-        let inner = futures::executor::ThreadPool::builder().create().unwrap();
-
-        ThreadPool { inner, rt }
-    };
-
-    custom_executor.spawn(async move {
-        let listener = TcpListener::bind("0.0.0.0:0").await.unwrap();
-        println!("addr: {:?}", listener.local_addr());
-        tx.send(()).unwrap();
+    #[allow(unused_attributes)]
+    #[should_panic]
+    let _ = rt1.block_on(async move {
+        delay_for(Duration::from_micros(10)).await;
     });
 
-    futures::executor::block_on(rx).unwrap();
-}
-
-#[test]
-#[should_panic]
-fn tokio_context_with_another_runtime_no_timer_io() {
-    let (tx, rx) = oneshot::channel();
-    let custom_executor: ThreadPool = {
-        // Spawn tokio runtime on a single background thread
-        // enabling IO and timers.
-        let rt = Builder::new()
-            .basic_scheduler()
-            .core_threads(1)
-            .build()
-            .unwrap();
-
-        let inner = futures::executor::ThreadPool::builder().create().unwrap();
-
-        ThreadPool { inner, rt }
-    };
-
-    custom_executor.spawn(async move {
-        let listener = TcpListener::bind("0.0.0.0:0").await.unwrap();
-        println!("addr: {:?}", listener.local_addr());
-        tx.send(()).unwrap();
+    let _ = rt1.block_on(async move {
+        rt2.handle()
+            .clone()
+            .wrap(tokio::time::delay_for(Duration::from_micros(10)))
+            .await
     });
-
-    // panics: "there is no reactor running, must be called from the context
-    // of Tokio runtime"
-    futures::executor::block_on(rx).unwrap();
 }
