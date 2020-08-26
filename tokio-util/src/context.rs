c50a48dfbd8586db8a973cdfce9a7a5f57e5c1f4
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2020-08-26 20:38:34 +0200

    enrich the docs

--- tokio-util/src/context.rs
+++ tokio-util/src/context.rs
@@ -11,6 +11,9 @@ use tokio::runtime::Handle;
 
 pin_project! {
     /// TokioContext allows connecting a custom executor with the tokio runtime.
+    ///
+    /// It contains a `Handle` to the runtime. A handle to the runtime can be
+    /// obtain by calling the `Runtime::handle()` method.
     pub struct TokioContext<F> {
         #[pin]
         inner: F,
@@ -31,20 +34,41 @@ impl<F: Future> Future for TokioContext<F> {
 /// Trait extension that simplifies bundling a `Handle` with a `Future`.
 pub trait HandleExt: Into<Handle> + Clone {
     /// Convenience method that takes a Future and returns a `TokioContext`.
-    /// # Example
     ///
-    /// use std::futures::Future;
-    /// use tokio-utils::context::{HandleExt};
-    /// use tokio::runtime::Handle;
+    /// # Example: calling Tokio Runtime from a custom ThreadPool
     ///
-    /// impl ThreadPool {
-    /// fn spawn(&self, f: impl Future<Output = ()> + Send + 'static) {
-    ///     let handle = self.rt.handle().clone();
-    ///     // create a TokioContext from the handle and future.
-    ///     let h = handle.wrap(f);
-    ///     self.inner.spawn_ok(h);
+    /// use std::futures::{self, Future};
+    /// use tokio-utils::context::HandleExt;
+    /// use tokio::runtime::{Builder, Handle, Runtime};
+    /// use tokio::time::{delay_for, Duration};
+    ///
+    /// struct ThreadPool {
+    ///     pub inner: futures::executor::ThreadPool,
+    ///     pub rt: Runtime,
     /// }
     ///
+    /// impl ThreadPool {
+    ///     fn spawn(&self, f: impl Future<Output = ()> + Send + 'static) {
+    ///         let handle = self.rt.handle().clone();
+    ///         // create a TokioContext from the handle and future.
+    ///         let h = handle.wrap(f);
+    ///         self.inner.spawn_ok(h);
+    ///     }
+    /// }
+    ///
+    /// let rt = tokio::runtime::Builder::new()
+    ///     .threaded_scheduler()
+    ///     .enable_all()
+    ///     .build.unwrap();
+    /// let inner = futures::executor::ThreadPool::builder().create().unwrap();
+    /// let executor: ThreadPool {
+    ///     inner, rt
+    /// }
+    ///
+    /// executor.spawn(async {
+    ///     delay_for(Duration::from_millis(2)).await
+    /// });
+
     fn wrap<F>(&self, fut: F) -> TokioContext<F>
     where
         F: Future,
--- tokio-util/tests/context.rs
+++ tokio-util/tests/context.rs
@@ -1,34 +1,27 @@
 #![warn(rust_2018_idioms)]
 
-use tokio_util::context::HandleExt;
-
 use tokio::runtime::Builder;
-
 use tokio::time::*;
+use tokio_util::context::HandleExt;
 #[test]
 fn tokio_context_with_another_runtime() {
     let mut rt1 = Builder::new()
-        .basic_scheduler()
+        .threaded_scheduler()
         .core_threads(1)
+        // no timer!
         .build()
         .unwrap();
     let rt2 = Builder::new()
-        .basic_scheduler()
+        .threaded_scheduler()
         .core_threads(1)
         .enable_all()
         .build()
         .unwrap();
 
-    #[allow(unused_attributes)]
-    #[should_panic]
-    let _ = rt1.block_on(async move {
-        delay_for(Duration::from_micros(10)).await;
-    });
-
-    let _ = rt1.block_on(async move {
+    // Without the `HandleExt.wrap()` there would be a panic because there is
+    // no timer running, since it would be referencing runtime r1.
+    let _ = rt1.block_on(
         rt2.handle()
-            .clone()
-            .wrap(tokio::time::delay_for(Duration::from_micros(10)))
-            .await
-    });
+            .wrap(async move { delay_for(Duration::from_millis(2)).await }),
+    );
 }
