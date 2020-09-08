6fc04c51a2637ee4a30e4dd9f7f5f901b067acb3
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-09-08 18:56:28 +0300

    fmt

--- tokio-util/src/sync/mod.rs
+++ tokio-util/src/sync/mod.rs
@@ -1,6 +1,6 @@
 //! Synchronization primitives
 
 mod cancellation_token;
-pub use cancellation_token::{CancellationToken, WaitForCancellationFuture, Aborted, Wrapped};
+pub use cancellation_token::{Aborted, CancellationToken, WaitForCancellationFuture, Wrapped};
 
 mod intrusive_double_linked_list;
