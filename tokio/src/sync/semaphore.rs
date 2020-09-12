e487fa61ba64473d145faed0a271cfbad6946a2d
Frank Steffahn <frank.steffahn@stu.uni-kiel.de>
2020-09-12 13:30:59 +0200

    Add const constructors to `RwLock`, `Notify`, and `Semaphore`.
    Referring to the types in `tokio::sync`.
    Also add `const` to `new` for the remaining atomic integers in `src/loom` and `UnsafeCell`.
    
    Builds upon previous work in #2790.
    Closes #2756.

--- tokio/src/sync/semaphore.rs
+++ tokio/src/sync/semaphore.rs
@@ -74,6 +74,14 @@ impl Semaphore {
         }
     }
 
+    /// Creates a new semaphore with the initial number of permits.
+    #[cfg(all(feature = "parking_lot", not(all(loom, test))))]
+    pub const fn const_new(permits: usize) -> Self {
+        Self {
+            ll_sem: ll::Semaphore::const_new(permits),
+        }
+    }
+
     /// Returns the current number of available permits.
     pub fn available_permits(&self) -> usize {
         self.ll_sem.available_permits()
