c3e7a90fba74781533ff8105abcca9063dad2362
Evan Cameron <cameron.evan@gmail.com>
2020-10-06 10:33:00 -0400

    Update tokio/src/net/udp/socket.rs
    Co-authored-by: Taiki Endo <te316e89@gmail.com>

--- tokio/src/net/udp/socket.rs
+++ tokio/src/net/udp/socket.rs
@@ -78,30 +78,30 @@ cfg_udp! {
     /// a similar "echo" example but that supports concurrent sending/receiving:
     ///
     /// ```no_run
-    ///     use tokio::{net::UdpSocket, sync::mpsc};
-    ///     use std::{io, net::SocketAddr, sync::Arc};
+    /// use tokio::{net::UdpSocket, sync::mpsc};
+    /// use std::{io, net::SocketAddr, sync::Arc};
     ///
-    ///     #[tokio::main]
-    ///     async fn main() -> io::Result<()> {
-    ///         let sock = UdpSocket::bind("0.0.0.0:8080".parse::<SocketAddr>().unwrap()).await?;
-    ///         let r = Arc::new(sock);
-    ///         let s = r.clone();
-    ///         let (tx, mut rx) = mpsc::channel::<(Vec<u8>, SocketAddr)>(1_000);
+    /// #[tokio::main]
+    /// async fn main() -> io::Result<()> {
+    ///     let sock = UdpSocket::bind("0.0.0.0:8080".parse::<SocketAddr>().unwrap()).await?;
+    ///     let r = Arc::new(sock);
+    ///     let s = r.clone();
+    ///     let (tx, mut rx) = mpsc::channel::<(Vec<u8>, SocketAddr)>(1_000);
     ///
-    ///         tokio::spawn(async move {
-    ///             while let Some((bytes, addr)) = rx.recv().await {
-    ///                 let len = s.send_to(&bytes, &addr).await.unwrap();
-    ///                 println!("{:?} bytes sent", len);
-    ///             }
-    ///         });
-    ///
-    ///         let mut buf = [0; 1024];
-    ///         loop {
-    ///             let (len, addr) = r.recv_from(&mut buf).await?;
-    ///             println!("{:?} bytes received from {:?}", len, addr);
-    ///             tx.send((buf[..len].to_vec(), addr)).await.unwrap();
+    ///     tokio::spawn(async move {
+    ///         while let Some((bytes, addr)) = rx.recv().await {
+    ///             let len = s.send_to(&bytes, &addr).await.unwrap();
+    ///             println!("{:?} bytes sent", len);
     ///         }
+    ///     });
+    ///
+    ///     let mut buf = [0; 1024];
+    ///     loop {
+    ///         let (len, addr) = r.recv_from(&mut buf).await?;
+    ///         println!("{:?} bytes received from {:?}", len, addr);
+    ///         tx.send((buf[..len].to_vec(), addr)).await.unwrap();
     ///     }
+    /// }
     /// ```
     ///
     pub struct UdpSocket {
