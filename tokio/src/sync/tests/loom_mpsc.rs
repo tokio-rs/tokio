730082fac55d27a58f7a2a48ee76068406671168
Zahari Dichev <zaharidichev@gmail.com>
2020-10-10 19:13:27 +0300

    add loom test

--- tokio/src/sync/tests/loom_mpsc.rs
+++ tokio/src/sync/tests/loom_mpsc.rs
@@ -2,7 +2,9 @@ use crate::sync::mpsc;
 
 use futures::future::poll_fn;
 use loom::future::block_on;
+use loom::sync::Arc;
 use loom::thread;
+use tokio_test::assert_ok;
 
 #[test]
 fn closing_tx() {
@@ -51,6 +53,33 @@ fn closing_bounded_rx() {
 
         block_on(tx1.closed());
         block_on(tx2.closed());
+    });
+}
+
+#[test]
+fn closing_and_sending() {
+    loom::model(|| {
+        let (tx1, mut rx) = mpsc::channel::<()>(16);
+        let tx1 = Arc::new(tx1);
+        let tx2 = tx1.clone();
+
+        let th1 = thread::spawn(move || {
+            tx1.try_send(()).unwrap();
+        });
+
+        let th2 = thread::spawn(move || {
+            block_on(tx2.closed());
+        });
+
+        let th3 = thread::spawn(move || {
+            let v = block_on(rx.recv());
+            assert!(v.is_some());
+            drop(rx);
+        });
+
+        assert_ok!(th1.join());
+        assert_ok!(th2.join());
+        assert_ok!(th3.join());
     });
 }
 
