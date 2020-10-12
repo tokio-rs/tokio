5c5f2052d6cd37045831d555df77b79137f59348
Carl Lerche <me@carllerche.com>
2020-10-11 22:09:30 -0700

    try to fix CI

--- tests-build/tests/macros.rs
+++ tests-build/tests/macros.rs
@@ -5,7 +5,7 @@ fn compile_fail_full() {
     #[cfg(feature = "full")]
     t.compile_fail("tests/fail/macros_invalid_input.rs");
 
-    #[cfg(feature = "rt-core")]
+    #[cfg(all(feature = "rt-core", not(feature = "full")))]
     t.compile_fail("tests/fail/macros_core_no_default.rs");
 
     drop(t);
