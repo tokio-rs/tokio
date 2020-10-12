044d918fa19f3d0881440b50d5ded31398d78d73
Lucio Franco <luciofranco14@gmail.com>
2020-10-12 14:40:33 -0400

    Update tokio/src/runtime/builder.rs
    Co-authored-by: Alice Ryhl <alice@ryhl.io>

--- tokio/src/runtime/builder.rs
+++ tokio/src/runtime/builder.rs
@@ -203,7 +203,7 @@ impl Builder {
     ///
     /// This will panic if `val` is not larger than `0`.
     pub fn worker_threads(&mut self, val: usize) -> &mut Self {
-        assert!(val > 0, "Worker threads can not be set to 0");
+        assert!(val > 0, "Worker threads cannot be set to 0");
         self.worker_threads = Some(val);
         self
     }
