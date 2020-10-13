5e8f3f85db7058e89f0c738bd52e7bca7e13eaeb
Carl Lerche <me@carllerche.com>
2020-10-13 13:51:44 -0700

    fix loom

--- tokio/src/runtime/tests/loom_blocking.rs
+++ tokio/src/runtime/tests/loom_blocking.rs
@@ -8,14 +8,15 @@ fn blocking_shutdown() {
         let v = Arc::new(());
 
         let rt = mk_runtime(1);
-        rt.enter(|| {
+        {
+            let _enter = rt.enter();
             for _ in 0..2 {
                 let v = v.clone();
                 crate::task::spawn_blocking(move || {
                     assert!(1 < Arc::strong_count(&v));
                 });
             }
-        });
+        }
 
         drop(rt);
         assert_eq!(1, Arc::strong_count(&v));
