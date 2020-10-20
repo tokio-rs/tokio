b71b20e7e02e1632ab6c1801b1f9aa2dc581a4ad
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2020-10-20 14:30:28 +0200

    update docs

--- tokio/src/stream/extend.rs
+++ tokio/src/stream/extend.rs
@@ -5,11 +5,11 @@ use std::marker::Unpin;
 /// `rust,no_run`
 /// use tokio::stream;
 ///
-/// let mut b = vec![0, 0, 0, 0];
+/// let mut b = vec![-2];
 /// let stream = stream::iter(vec![0, 2, 4, 6]);
 ///
 /// stream::extend(&mut buff, s).await;
-/// assert_eq!(vec![0, 2, 4, 6], buff);
+/// assert_eq!(vec![-2, 0, 2, 4, 6], buff);
 pub async fn extend<T, S>(buff: &mut Vec<T>, mut b: S)
 where
     S: Stream<Item = T> + Unpin,
