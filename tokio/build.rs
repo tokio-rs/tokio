47017ecb863eb91108538c5a973c690548fefd17
Eliza Weisman <eliza@buoyant.io>
2020-10-30 10:54:11 -0700

    use autocfg
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio/build.rs
+++ tokio/build.rs
@@ -1,41 +1,22 @@
-use std::{env, ffi::OsString, process::Command};
+use autocfg::AutoCfg;
 
 fn main() {
-    match rustc_minor_version() {
-        // If rustc >= 1.47.0, we can enable `track_caller`.
-        Ok(minor) if minor >= 47 => println!("cargo:rustc-cfg=tokio_track_caller"),
-        Err(e) => println!("cargo:warning=could not parse rustc version: {}", e),
-        _ => {}
-    }
-}
+    match AutoCfg::new() {
+        Ok(ac) => {
+            // The #[track_caller] attribute was stabilized in rustc 1.46.0.
+            if ac.probe_rustc_version(1, 46) {
+                autocfg::emit("tokio_track_caller")
+            }
+        }
 
-fn rustc_minor_version() -> Result<usize, Box<dyn std::error::Error>> {
-    let rustc = env::var_os("RUSTC").unwrap_or_else(|| OsString::from("rustc"));
-    let out = Command::new(rustc).arg("-V").output()?;
-    let version_str = std::str::from_utf8(&out.stdout)?;
-    let mut parts = version_str.split(' ');
-    if parts.next() != Some("rustc") {
-        return Err(format!("weird rustc version: {:?} (missing 'rustc') ", version_str).into());
-    }
-    if let Some(part) = parts.next() {
-        let mut parts = part.split('.');
-        if parts.next() != Some("1") {
-            return Err(format!(
-                "weird rustc version: {:?} (does not start with 1)",
-                version_str
-            )
-            .into());
+        Err(e) => {
+            // If we couldn't detect the compiler version and features, just
+            // print a warning. This isn't a fatal error: we can still build
+            // Tokio, we just can't enable cfgs automatically.
+            println!(
+                "cargo:warning=tokio: failed to detect compiler features: {}",
+                e
+            );
         }
-        if let Some(middle) = parts.next() {
-            Ok(middle.parse()?)
-        } else {
-            Err(format!("weird rustc version: {:?} (no minor version)", version_str).into())
-        }
-    } else {
-        Err(format!(
-            "weird rustc version: {:?} (missing version entirely?) ",
-            version_str,
-        )
-        .into())
     }
 }
