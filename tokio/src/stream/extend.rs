a87f1d00463f44c04d2146669824995641873e4d
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2020-11-03 19:37:40 +0100

    fix warnings

--- tokio/src/stream/extend.rs
+++ tokio/src/stream/extend.rs
@@ -1,16 +1,16 @@
 use crate::stream::{Stream, StreamExt};
-use std::{iter::Extend, marker::Unpin};
+use std::iter::Extend;
 /// Extends a slice from a Stream
 /// # Example
 /// `rust,no_run`
 /// use tokio::stream;
 ///
-/// let mut b = vec![-2];
-/// let stream = stream::iter(vec![0, 2, 4, 6]);
+/// let buff = vec![-2];
+/// let s = stream::iter(vec![0, 2, 4, 6]);
 ///
 /// stream::extend(&mut buff, s).await;
 /// assert_eq!(vec![-2, 0, 2, 4, 6], buff);
-pub async fn extend<E, S>(buff: &mut E, mut b: S)
+pub async fn extend<E, S>(buff: &mut E, b: S)
 where
     S: Stream,
     E: Extend<S::Item>,
