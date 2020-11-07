cbbfaf047fc4c7049750829eeae57c2ad8c5e1cb
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-11-07 18:00:08 +0300

    no need to invent block_on

--- tokio-util/src/sync/tests/mpsc.rs
+++ tokio-util/src/sync/tests/mpsc.rs
@@ -3,19 +3,9 @@ use crate::sync::Sender;
 use futures::future::poll_fn;
 use tokio::sync::mpsc::channel;
 
-fn block_on<F: std::future::Future>(fut: F) -> F::Output {
-    tokio::pin!(fut);
-    let mut cx = std::task::Context::from_waker(futures::task::noop_waker_ref());
-    loop {
-        if let std::task::Poll::Ready(r) = fut.as_mut().poll(&mut cx) {
-            return r;
-        }
-    }
-}
-
 #[test]
 fn basic() {
-    block_on(async move {
+    futures::executor::block_on(async move {
         let (tx, mut rx) = channel::<String>(1);
         let tx = Sender::new(tx);
         tokio::pin!(tx);
