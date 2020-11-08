ffaa112ffc2f4a096455f3c6ca7dcfffe60ecee4
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-11-09 00:54:11 +0300

    Add one more test

--- tokio-util/src/sync/tests/mpsc.rs
+++ tokio-util/src/sync/tests/mpsc.rs
@@ -14,3 +14,24 @@ fn basic() {
         assert_eq!(rx.try_recv(), Ok("hello".to_string()))
     });
 }
+
+#[test]
+fn test_pending_and_disarm() {
+    futures::executor::block_on(async move {
+        let (tx, mut rx) = channel::<String>(1);
+        let tx2 = tx.clone();
+        let tx = Sender::new(tx);
+        tokio::pin!(tx);
+        poll_fn(|cx| tx.as_mut().poll_ready(cx)).await.unwrap();
+        assert!(tx2.try_send("hi".to_string()).is_err());
+        tx.as_mut().send("hello".to_string());
+        assert_eq!(rx.try_recv(), Ok("hello".to_string()));
+        assert_eq!(
+            rx.try_recv(),
+            Err(tokio::sync::mpsc::error::TryRecvError::Empty)
+        );
+        poll_fn(|cx| tx.as_mut().poll_ready(cx)).await.unwrap();
+        tx.disarm();
+        assert!(tx2.try_send("hi again".to_string()).is_ok());
+    });
+}
