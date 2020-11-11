b9577a499074e025293fa9ce84930bed1f6a7e13
Nylonicious <50183564+nylonicious@users.noreply.github.com>
2020-11-11 23:53:12 +0100

    fs: Shorten the line length.

--- tokio/src/fs/copy.rs
+++ tokio/src/fs/copy.rs
@@ -1,7 +1,8 @@
 use crate::fs::asyncify;
 use std::path::Path;
 
-/// Copies the contents of one file to another. This function will also copy the permission bits of the original file to the destination file.
+/// Copies the contents of one file to another. This function will also copy the permission bits
+/// of the original file to the destination file.
 /// This function will overwrite the contents of to.
 ///
 /// This is the async equivalent of [`std::fs::copy`][std].
