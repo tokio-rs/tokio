12574843e7bb0a7841037da01459ff3c0f552d5d
Carl Lerche <me@carllerche.com>
2020-11-30 11:24:45 -0800

    chore: prepare v0.3.5 release

--- tokio/src/lib.rs
+++ tokio/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tokio/0.3.4")]
+#![doc(html_root_url = "https://docs.rs/tokio/0.3.5")]
 #![allow(
     clippy::cognitive_complexity,
     clippy::large_enum_variant,
