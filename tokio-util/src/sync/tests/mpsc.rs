613024da138cc995cafbb1d0118b73338fb49b76
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-12-01 14:34:59 +0300

    One more test

--- tokio-util/src/sync/tests/mpsc.rs
+++ tokio-util/src/sync/tests/mpsc.rs
@@ -1,11 +1,13 @@
 // TODO: loom
+use std::task::Context;
+
 use crate::sync::Sender;
 use futures::future::poll_fn;
 use tokio::sync::mpsc::channel;
 
 #[test]
 fn basic() {
-    futures::executor::block_on(async move {
+    futures::executor::block_on(async {
         let (tx, mut rx) = channel::<String>(1);
         let tx = Sender::new(tx);
         tokio::pin!(tx);
@@ -17,7 +19,7 @@ fn basic() {
 
 #[test]
 fn test_pending_and_disarm() {
-    futures::executor::block_on(async move {
+    futures::executor::block_on(async {
         let (tx, mut rx) = channel::<String>(1);
         let tx2 = tx.clone();
         let tx = Sender::new(tx);
@@ -33,5 +35,30 @@ fn test_pending_and_disarm() {
         poll_fn(|cx| tx.as_mut().poll_ready(cx)).await.unwrap();
         tx.disarm();
         assert!(tx2.try_send("hi again".to_string()).is_ok());
+    });
+}
+
+#[test]
+fn disarm_during_pending() {
+    futures::executor::block_on(async {
+        let (tx, mut rx) = channel::<u32>(1);
+        let tx2 = tx.clone();
+        let tx = Sender::new(tx);
+        tokio::pin!(tx);
+        tx2.send(1).await.unwrap();
+        for _ in 0..100 {
+            assert!(tx
+                .as_mut()
+                .poll_ready(&mut Context::from_waker(futures::task::noop_waker_ref()))
+                .is_pending());
+        }
+        assert_eq!(rx.try_recv(), Ok(1));
+        assert_eq!(
+            rx.try_recv(),
+            Err(tokio::sync::mpsc::error::TryRecvError::Empty)
+        );
+        poll_fn(|cx| tx.as_mut().poll_ready(cx)).await.unwrap();
+        tx.disarm();
+        assert!(tx2.try_send(2).is_ok());
     });
 }
