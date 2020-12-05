4c1915ff923b0448ae4fe76ae4852d395518a4e6
Fuyang Liu <liufuyang@users.noreply.github.com>
2020-12-05 11:38:35 +0100

    Add doc test for into_std
    Signed-off-by: Fuyang Liu <liufuyang@users.noreply.github.com>

--- tokio/src/net/tcp/stream.rs
+++ tokio/src/net/tcp/stream.rs
@@ -184,7 +184,38 @@ impl TcpStream {
         Ok(TcpStream { io })
     }
 
-    /// Turn a `TcpStream` into a `std::net::TcpStream`.
+    /// Turn a [`tokio::net::TcpStream`] into a [`std::net::TcpStream`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::error::Error;
+    /// use std::io::Read;
+    /// use tokio::net::TcpListener;
+    /// # use tokio::net::TcpStream;
+    /// # use tokio::io::AsyncWriteExt;
+    /// # use tokio::time::Duration;
+    ///
+    /// #[tokio::main]
+    /// async fn main() -> Result<(), Box<dyn Error>> {
+    /// #   tokio::spawn(async {
+    /// #       tokio::time::sleep(Duration::from_millis(50)).await;
+    /// #       let mut stream: TcpStream = TcpStream::connect("127.0.0.1:8080").await.unwrap();
+    /// #       let _ = stream.write(b"Hello world!").await;
+    /// #   });
+    ///     let mut data = [0 as u8; 50];
+    ///     let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    ///     let (tokio_tcp_stream, _) = listener.accept().await?;
+    ///
+    ///     let mut std_tcp_stream = tokio_tcp_stream.into_std()?;
+    /// #   tokio::time::sleep(Duration::from_millis(100)).await;
+    ///     let size = std_tcp_stream.read(&mut data)?;
+    /// #   assert_eq!("Hello world!", std::str::from_utf8(&data[0..size])?);
+    /// #   Ok(())
+    /// }
+    /// ```
+    /// [`tokio::net::TcpStream`]: TcpStream
+    /// [`std::net::TcpStream`]: std::net::TcpStream
     pub fn into_std(self) -> io::Result<std::net::TcpStream> {
         #[cfg(unix)]
         {
