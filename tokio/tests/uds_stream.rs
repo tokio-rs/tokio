e1bcaf08c59cb0801591875296b8baf08060f0e7
cssivision <cssivision@gmail.com>
2020-12-11 10:54:46 +0800

    net: UnixStream add poll_write_ready/poll_read_ready

--- tokio/tests/uds_stream.rs
+++ tokio/tests/uds_stream.rs
@@ -3,13 +3,13 @@
 #![cfg(unix)]
 
 use std::io;
+use std::task::Poll;
 
 use tokio::io::{AsyncReadExt, AsyncWriteExt, Interest};
 use tokio::net::{UnixListener, UnixStream};
-use tokio_test::task;
-use tokio_test::{assert_pending, assert_ready_ok};
+use tokio_test::{assert_ok, assert_pending, assert_ready_ok, task};
 
-use futures::future::try_join;
+use futures::future::{poll_fn, try_join};
 
 #[tokio::test]
 async fn accept_read_write() -> std::io::Result<()> {
@@ -62,7 +62,7 @@ async fn shutdown() -> std::io::Result<()> {
 }
 
 #[tokio::test]
-async fn try_read_write_unix_stream() -> std::io::Result<()> {
+async fn try_read_write() -> std::io::Result<()> {
     let msg = b"hello world";
 
     let dir = tempfile::tempdir()?;
@@ -141,4 +141,114 @@ async fn try_read_write_unix_stream() -> std::io::Result<()> {
     }
 
     Ok(())
+}
+
+async fn create_pair() -> (UnixStream, UnixStream) {
+    let dir = assert_ok!(tempfile::tempdir());
+    let bind_path = dir.path().join("bind.sock");
+
+    let listener = assert_ok!(UnixListener::bind(&bind_path));
+
+    let accept = listener.accept();
+    let connect = UnixStream::connect(&bind_path);
+    let ((server, _), client) = assert_ok!(try_join(accept, connect).await);
+
+    (client, server)
+}
+
+macro_rules! assert_readable_by_polling {
+    ($stream:expr) => {
+        assert_ok!(poll_fn(|cx| $stream.poll_read_ready(cx)).await);
+    };
+}
+
+macro_rules! assert_not_readable_by_polling {
+    ($stream:expr) => {
+        poll_fn(|cx| {
+            assert_pending!($stream.poll_read_ready(cx));
+            Poll::Ready(())
+        })
+        .await;
+    };
+}
+
+macro_rules! assert_writable_by_polling {
+    ($stream:expr) => {
+        assert_ok!(poll_fn(|cx| $stream.poll_write_ready(cx)).await);
+    };
+}
+
+macro_rules! assert_not_writable_by_polling {
+    ($stream:expr) => {
+        poll_fn(|cx| {
+            assert_pending!($stream.poll_write_ready(cx));
+            Poll::Ready(())
+        })
+        .await;
+    };
+}
+
+#[tokio::test]
+async fn poll_read_ready() {
+    let (mut client, mut server) = create_pair().await;
+
+    // Initial state - not readable.
+    assert_not_readable_by_polling!(server);
+
+    // There is data in the buffer - readable.
+    assert_ok!(client.write_all(b"ping").await);
+    assert_readable_by_polling!(server);
+
+    // Readable until calls to `poll_read` return `Poll::Pending`.
+    let mut buf = [0u8; 4];
+    assert_ok!(server.read_exact(&mut buf).await);
+    assert_readable_by_polling!(server);
+    read_until_pending(&mut server);
+    assert_not_readable_by_polling!(server);
+
+    // Detect the client disconnect.
+    drop(client);
+    assert_readable_by_polling!(server);
+}
+
+#[tokio::test]
+async fn poll_write_ready() {
+    let (mut client, server) = create_pair().await;
+
+    // Initial state - writable.
+    assert_writable_by_polling!(client);
+
+    // No space to write - not writable.
+    write_until_pending(&mut client);
+    assert_not_writable_by_polling!(client);
+
+    // Detect the server disconnect.
+    drop(server);
+    assert_writable_by_polling!(client);
+}
+
+fn read_until_pending(stream: &mut UnixStream) {
+    let mut buf = vec![0u8; 1024 * 1024];
+    loop {
+        match stream.try_read(&mut buf) {
+            Ok(_) => (),
+            Err(err) => {
+                assert_eq!(err.kind(), io::ErrorKind::WouldBlock);
+                break;
+            }
+        }
+    }
+}
+
+fn write_until_pending(stream: &mut UnixStream) {
+    let buf = vec![0u8; 1024 * 1024];
+    loop {
+        match stream.try_write(&buf) {
+            Ok(_) => (),
+            Err(err) => {
+                assert_eq!(err.kind(), io::ErrorKind::WouldBlock);
+                break;
+            }
+        }
+    }
 }
