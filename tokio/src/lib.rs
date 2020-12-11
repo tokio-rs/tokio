b0626b2f438034c8fab958c4f56f75f5ff1a7611
fl9 <flo.huebsch@pm.me>
2020-12-11 20:26:54 +0100

    provide appropriate annotation for `tokio::main`

--- tokio/src/lib.rs
+++ tokio/src/lib.rs
@@ -407,25 +407,21 @@ cfg_macros! {
     #[doc(hidden)]
     pub use tokio_macros::select_priv_declare_output_enum;
 
-    cfg_rt! {
-        cfg_rt_multi_thread! {
-            // This is the docs.rs case (with all features) so make sure macros
-            // and rt are included in doc(cfg).
+    #[cfg(feature = "rt-multi-thread")]
+    #[cfg(not(test))] // Work around for rust-lang/rust#62127
+    #[cfg_attr(docsrs, doc(cfg(all(feature = "macros", any(feature = "rt-multi-thread", feature = "rt")))))]
+    pub use tokio_macros::main;
 
-            #[cfg(not(test))] // Work around for rust-lang/rust#62127
-            #[cfg_attr(docsrs, doc(cfg(all(feature = "macros", feature = "rt"))))]
-            pub use tokio_macros::main;
+    #[cfg(feature = "rt-multi-thread")]
+    #[cfg_attr(docsrs, doc(cfg(all(feature = "macros", any(feature = "rt-multi-thread", feature = "rt")))))]
+    pub use tokio_macros::test;
 
-            #[cfg_attr(docsrs, doc(cfg(all(feature = "macros", feature = "rt"))))]
-            pub use tokio_macros::test;
-        }
+    #[cfg(all(feature = "rt", not(feature = "rt-multi-thread")))]
+    #[cfg(not(test))] // Work around for rust-lang/rust#62127
+    pub use tokio_macros::main_rt as main;
 
-        cfg_not_rt_multi_thread! {
-            #[cfg(not(test))] // Work around for rust-lang/rust#62127
-            pub use tokio_macros::main_rt as main;
-            pub use tokio_macros::test_rt as test;
-        }
-    }
+    #[cfg(all(feature = "rt", not(feature = "rt-multi-thread")))]
+    pub use tokio_macros::test_rt as test;
 
     // Always fail if rt is not enabled.
     cfg_not_rt! {
