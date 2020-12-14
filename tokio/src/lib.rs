176c809e2052451c4d2c1c6d639ca56a900a779d
bdonlan <bdonlan@gmail.com>
2020-12-14 13:51:38 -0800

    chore: prepare v0.3.6 release (#3276)

--- tokio/src/lib.rs
+++ tokio/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tokio/0.3.5")]
+#![doc(html_root_url = "https://docs.rs/tokio/0.3.6")]
 #![allow(
     clippy::cognitive_complexity,
     clippy::large_enum_variant,
