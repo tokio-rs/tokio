22526a0d502f58b47ae6e305b298341d8366af05
github-action-benchmark <github@users.noreply.github.com>
2020-12-21 23:43:57 +0000

    add sync_semaphore (cargo) benchmark result for 564c943309f0a94474e2e71a115ec0bb45f3e7fd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1608594233340,
+  "lastUpdate": 1608594236935,
   "repoUrl": "https://github.com/tokio-rs/tokio",
   "entries": {
     "sync_rwlock": [
@@ -10611,6 +10611,60 @@ window.BENCHMARK_DATA = {
             "name": "uncontended_concurrent_single",
             "value": 920,
             "range": "± 5",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alice@ryhl.io",
+            "name": "Alice Ryhl",
+            "username": "Darksonn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "564c943309f0a94474e2e71a115ec0bb45f3e7fd",
+          "message": "io: rename `AsyncFd::with_io()` and rm `with_poll()` (#3306)",
+          "timestamp": "2020-12-21T15:42:38-08:00",
+          "tree_id": "6f29310dda04438222fb9581b3cd9581f1abe13e",
+          "url": "https://github.com/tokio-rs/tokio/commit/564c943309f0a94474e2e71a115ec0bb45f3e7fd"
+        },
+        "date": 1608594235809,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "contended_concurrent_multi",
+            "value": 13521,
+            "range": "± 3026",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contended_concurrent_single",
+            "value": 917,
+            "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontended",
+            "value": 544,
+            "range": "± 11",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontended_concurrent_multi",
+            "value": 13363,
+            "range": "± 2624",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontended_concurrent_single",
+            "value": 918,
+            "range": "± 6",
             "unit": "ns/iter"
           }
         ]
