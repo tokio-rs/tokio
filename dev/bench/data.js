be8c1ca054363bb1f3eb0488619d1e63b05982a4
github-action-benchmark <github@users.noreply.github.com>
2020-12-22 19:50:23 +0000

    add sync_rwlock (cargo) benchmark result for 2ee9520d10c1089230fc66eaa81f0574038faa82

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1608666612685,
+  "lastUpdate": 1608666623473,
   "repoUrl": "https://github.com/tokio-rs/tokio",
   "entries": {
     "sync_rwlock": [
@@ -3941,6 +3941,60 @@ window.BENCHMARK_DATA = {
             "name": "read_uncontended",
             "value": 591,
             "range": "± 38",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@carllerche.com",
+            "name": "Carl Lerche",
+            "username": "carllerche"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2ee9520d10c1089230fc66eaa81f0574038faa82",
+          "message": "fs: misc small API tweaks (#3315)",
+          "timestamp": "2020-12-22T11:13:41-08:00",
+          "tree_id": "0cfded0285a4943e6aac2fd843965f48e9ccc4ff",
+          "url": "https://github.com/tokio-rs/tokio/commit/2ee9520d10c1089230fc66eaa81f0574038faa82"
+        },
+        "date": 1608666622480,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "read_concurrent_contended",
+            "value": 896,
+            "range": "± 200",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_contended_multi",
+            "value": 14887,
+            "range": "± 3803",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_uncontended",
+            "value": 866,
+            "range": "± 93",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_uncontended_multi",
+            "value": 14347,
+            "range": "± 5260",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_uncontended",
+            "value": 520,
+            "range": "± 94",
             "unit": "ns/iter"
           }
         ]
