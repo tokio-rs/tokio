af2357f4e1dc95f6ed1af33b539413ca941e9bce
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-12-25 19:21:41 +0300

    enter rt

--- tokio/src/future/block_on.rs
+++ tokio/src/future/block_on.rs
@@ -5,13 +5,16 @@ cfg_rt! {
         // FIXME: Waiting for https://github.com/tokio-rs/tokio/pull/3097"
         // t10::runtime::Handle::current().block_on(f)
 
+        let h = t10::runtime::Handle::current();
+        let _enter = h.enter();
         futures::executor::block_on(f)
     }
 }
 
 cfg_not_rt! {
-    pub(crate) fn block_on<F: Future>(f: F) -> F::Output {
+    pub(crate) fn block_on<F: Future>(_f: F) -> F::Output {
         // FIXME performance degradation?
-       futures::executor::block_on(f)
+       //futures::executor::block_on(f)
+       todo!()
     }
 }
