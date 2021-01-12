5714000146a44e31bd46d2bfbd0f9bd2bbe4a6f3
Alice Ryhl <alice@ryhl.io>
2021-01-12 21:02:50 +0100

    chore: prepare tokio-stream 0.1.2

--- tokio-stream/src/lib.rs
+++ tokio-stream/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tokio-stream/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/tokio-stream/0.1.2")]
 #![allow(
     clippy::cognitive_complexity,
     clippy::large_enum_variant,
