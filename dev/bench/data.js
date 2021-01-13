1da9f8f0ef6747608f4f7e998824b4197c096cf4
github-action-benchmark <github@users.noreply.github.com>
2021-01-13 19:26:17 +0000

    add rt_multi_threaded (cargo) benchmark result for 672be92a03a489d028ed7ec2c6b1abdbf0874a1d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610565963390,
+  "lastUpdate": 1610565977079,
   "repoUrl": "https://github.com/tokio-rs/tokio",
   "entries": {
     "sync_rwlock": [
@@ -11323,6 +11323,54 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 19513314,
             "range": "± 2098262",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "luciofranco14@gmail.com",
+            "name": "Lucio Franco",
+            "username": "LucioFranco"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "672be92a03a489d028ed7ec2c6b1abdbf0874a1d",
+          "message": "mpsc: Add `Sender::try_reserve` function (#3418)\n\n* mpsc: Add `Sender::try_reserve` function\r\n\r\n* Update tokio/src/sync/mpsc/bounded.rs\r\n\r\nCo-authored-by: Alice Ryhl <alice@ryhl.io>\r\n\r\n* Fix doc links\r\n\r\nCo-authored-by: Alice Ryhl <alice@ryhl.io>",
+          "timestamp": "2021-01-13T14:24:23-05:00",
+          "tree_id": "dbefcc820c74146acd0f1164632acdc53a6a7448",
+          "url": "https://github.com/tokio-rs/tokio/commit/672be92a03a489d028ed7ec2c6b1abdbf0874a1d"
+        },
+        "date": 1610565975724,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 172996,
+            "range": "± 30645",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 692663,
+            "range": "± 218929",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 4674707,
+            "range": "± 538475",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 18893546,
+            "range": "± 2438913",
             "unit": "ns/iter"
           }
         ]
