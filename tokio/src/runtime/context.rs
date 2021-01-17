a7b29ee6148fd2ce6c1a4d64bf49ce271d1778aa
Arve Knudsen <arve.knudsen@gmail.com>
2021-01-17 12:43:30 +0100

    context::missing_error: Include Tokio version
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- tokio/src/runtime/context.rs
+++ tokio/src/runtime/context.rs
@@ -103,7 +103,7 @@ pub(crate) fn missing_error(features: &[&str]) -> String {
     } else {
         String::new()
     };
-    format!("there is no reactor running, must be called from the context of Tokio runtime{}", sfx)
+    format!("there is no reactor running, must be called from the context of Tokio {} runtime{}", env!("CARGO_PKG_VERSION"), sfx)
 }
 
 #[cfg(test)]
@@ -112,22 +112,33 @@ mod tests {
 
     #[test]
     fn test_missing_error_no_features() {
-        assert_eq!(&missing_error(&[]), "there is no reactor running, must be called from the context of Tokio runtime");
+        assert_eq!(missing_error(&[]), format!(
+                "there is no reactor running, must be called from the context of Tokio {} runtime",
+                env!("CARGO_PKG_VERSION")
+            ));
     }
 
     #[test]
     fn test_missing_error_one_feature() {
-        assert_eq!(&missing_error(&["rt"]), "there is no reactor running, must be called from the context of Tokio runtime with rt enabled");
+        assert_eq!(missing_error(&["rt"]), format!(
+                "there is no reactor running, must be called from the context of Tokio {} runtime with rt enabled",
+                env!("CARGO_PKG_VERSION")
+            ));
     }
 
     #[test]
     fn test_missing_error_two_features() {
-        assert_eq!(&missing_error(&["rt", "signal"]), "there is no reactor running, must be called from the context of Tokio runtime with either rt or signal enabled");
+        assert_eq!(missing_error(&["rt", "signal"]), format!(
+                "there is no reactor running, must be called from the context of Tokio {} runtime with either rt or signal enabled",
+                env!("CARGO_PKG_VERSION")
+            ));
     }
 
     #[test]
     fn test_missing_error_three_features() {
-        assert_eq!(&missing_error(&["rt", "signal", "sync"]), 
-                   "there is no reactor running, must be called from the context of Tokio runtime with either rt, signal or sync enabled");
+        assert_eq!(missing_error(&["rt", "signal", "sync"]), format!(
+                   "there is no reactor running, must be called from the context of Tokio {} runtime with either rt, signal or sync enabled",
+                    env!("CARGO_PKG_VERSION")
+                ));
     }
 }
