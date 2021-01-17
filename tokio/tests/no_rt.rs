a1bbf254efe8a8a48d3bfb6e5ceee1abbf8cecaa
Arve Knudsen <arve.knudsen@gmail.com>
2021-01-17 18:17:45 +0100

    Formatting
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- tokio/tests/no_rt.rs
+++ tokio/tests/no_rt.rs
@@ -7,7 +7,9 @@ use futures::executor::block_on;
 use std::net::TcpListener;
 
 #[test]
-#[should_panic(expected = "there is no reactor running, must be called from the context of a Tokio 1.x runtime")]
+#[should_panic(
+    expected = "there is no reactor running, must be called from the context of a Tokio 1.x runtime"
+)]
 fn timeout_panics_when_no_tokio_context() {
     block_on(timeout_value());
 }
