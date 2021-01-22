66f60ba98db1b0b7bbf655977a3837f131537280
github-action-benchmark <github@users.noreply.github.com>
2021-01-22 22:07:17 +0000

    add rt_multi_threaded (cargo) benchmark result for 35b1a4d0b3a411f52ffc97010b9b002e53bd0d84

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611353231873,
+  "lastUpdate": 1611353237283,
   "repoUrl": "https://github.com/tokio-rs/tokio",
   "entries": {
     "sync_rwlock": [
@@ -13771,6 +13771,54 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 20824423,
             "range": "± 3427491",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alice@ryhl.io",
+            "name": "Alice Ryhl",
+            "username": "Darksonn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "35b1a4d0b3a411f52ffc97010b9b002e53bd0d84",
+          "message": "chore: prepare Tokio 1.1.0 (#3462)",
+          "timestamp": "2021-01-22T23:05:37+01:00",
+          "tree_id": "54efedad7616780dc46f50b4a6248de978daa8dc",
+          "url": "https://github.com/tokio-rs/tokio/commit/35b1a4d0b3a411f52ffc97010b9b002e53bd0d84"
+        },
+        "date": 1611353232492,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 163075,
+            "range": "± 6906",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 640431,
+            "range": "± 22244",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 5099251,
+            "range": "± 1036352",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 18416014,
+            "range": "± 1858398",
             "unit": "ns/iter"
           }
         ]
