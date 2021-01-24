c611d5c6a20238777af22b74ff773a0d21924442
Fuyang Liu <liufuyang@users.noreply.github.com>
2021-01-24 08:03:58 +0100

    Update tokio-stream/src/wrappers/broadcast.rs
    Co-authored-by: Alice Ryhl <alice@ryhl.io>

--- tokio-stream/src/wrappers/broadcast.rs
+++ tokio-stream/src/wrappers/broadcast.rs
@@ -6,11 +6,10 @@ use tokio::sync::broadcast::Receiver;
 
 use std::task::{Context, Poll};
 
-/// A wrapper around [`Receiver`] that implements [`Stream`]. Achieved by using the [`async-stream`] crate.
+/// A wrapper around [`Receiver`] that implements [`Stream`].
 ///
 /// [`Receiver`]: struct@tokio::sync::broadcast::Receiver
 /// [`Stream`]: trait@crate::Stream
-/// [`async-stream`]: https://docs.rs/async-stream
 #[derive(Debug)]
 pub struct BroadcastStream<T: Clone> {
     inner: Pin<Box<dyn Stream<Item = Result<T, RecvError>> + Send + Sync >>,
