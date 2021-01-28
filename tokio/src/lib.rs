0635833d4f9740ddeb5ea28acd53edf8ca7372f4
Alice Ryhl <alice@ryhl.io>
2021-01-28 14:44:57 +0100

    chore: prepare Tokio 0.3.7

--- tokio/src/lib.rs
+++ tokio/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tokio/0.3.6")]
+#![doc(html_root_url = "https://docs.rs/tokio/0.3.7")]
 #![allow(
     clippy::cognitive_complexity,
     clippy::large_enum_variant,
