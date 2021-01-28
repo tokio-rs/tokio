9748ca867243d12ef780f7a2ac6b36b8d361e40c
github-action-benchmark <github@users.noreply.github.com>
2021-01-28 22:11:00 +0000

    add sync_mpsc (cargo) benchmark result for 1f9765fb5f3a9d461c50124ac3e3f52c4901fc06

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611871849563,
+  "lastUpdate": 1611871860617,
   "repoUrl": "https://github.com/tokio-rs/tokio",
   "entries": {
     "sync_rwlock": [
@@ -33779,6 +33779,90 @@ window.BENCHMARK_DATA = {
             "name": "uncontented_unbounded",
             "value": 823953,
             "range": "± 16631",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ivanppetkov@gmail.com",
+            "name": "Ivan Petkov",
+            "username": "ipetkov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1f9765fb5f3a9d461c50124ac3e3f52c4901fc06",
+          "message": "process: add missing drop child stdin to wait() (#3421)",
+          "timestamp": "2021-01-28T23:08:44+01:00",
+          "tree_id": "b76f705bf1718a1ed3f146dd3e27bdb1209e1e64",
+          "url": "https://github.com/tokio-rs/tokio/commit/1f9765fb5f3a9d461c50124ac3e3f52c4901fc06"
+        },
+        "date": 1611871859190,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "contention_bounded",
+            "value": 5960780,
+            "range": "± 2972898",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_bounded_full",
+            "value": 6211996,
+            "range": "± 1922846",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_unbounded",
+            "value": 5716859,
+            "range": "± 2851039",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_000_medium",
+            "value": 672,
+            "range": "± 168",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_medium",
+            "value": 677,
+            "range": "± 129",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_1_medium",
+            "value": 666,
+            "range": "± 115",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_large",
+            "value": 133739,
+            "range": "± 22667",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_medium",
+            "value": 1419,
+            "range": "± 456",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_bounded",
+            "value": 949941,
+            "range": "± 203114",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_unbounded",
+            "value": 673660,
+            "range": "± 125865",
             "unit": "ns/iter"
           }
         ]
