f3b8e9d86f9e68e010056c77d9905fe39b623ace
github-action-benchmark <github@users.noreply.github.com>
2021-02-05 18:45:39 +0000

    add rt_multi_threaded (cargo) benchmark result for 77ca8a934cf76ec74144ec11e78ae7c7efbe910a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612550734765,
+  "lastUpdate": 1612550739486,
   "repoUrl": "https://github.com/tokio-rs/tokio",
   "entries": {
     "sync_rwlock": [
@@ -15301,6 +15301,54 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 16385525,
             "range": "± 2835267",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "liufuyang@users.noreply.github.com",
+            "name": "Fuyang Liu",
+            "username": "liufuyang"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "77ca8a934cf76ec74144ec11e78ae7c7efbe910a",
+          "message": "signal: make Signal::poll_recv method public (#3383)\n\nSigned-off-by: Fuyang Liu <liufuyang@users.noreply.github.com>",
+          "timestamp": "2021-02-05T19:43:58+01:00",
+          "tree_id": "43cbc545e325e68e502f2eb294c1f8daa678f9b1",
+          "url": "https://github.com/tokio-rs/tokio/commit/77ca8a934cf76ec74144ec11e78ae7c7efbe910a"
+        },
+        "date": 1612550734607,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 163510,
+            "range": "± 79692",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 575067,
+            "range": "± 31927",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 4572899,
+            "range": "± 900314",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 15984155,
+            "range": "± 2078176",
             "unit": "ns/iter"
           }
         ]
