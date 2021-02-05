659576c526efee72eb7ec7e0922ff1564680883c
github-action-benchmark <github@users.noreply.github.com>
2021-02-05 21:20:34 +0000

    add rt_multi_threaded (cargo) benchmark result for 23fdc2b3c4b33300c3e8b44dcb126057d90f4935

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612560029042,
+  "lastUpdate": 1612560033877,
   "repoUrl": "https://github.com/tokio-rs/tokio",
   "entries": {
     "sync_rwlock": [
@@ -15709,6 +15709,54 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 20830878,
             "range": "± 2459489",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alice@ryhl.io",
+            "name": "Alice Ryhl",
+            "username": "Darksonn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "23fdc2b3c4b33300c3e8b44dcb126057d90f4935",
+          "message": "chore: prepare tokio-stream 0.1.3 (#3507)",
+          "timestamp": "2021-02-05T22:18:56+01:00",
+          "tree_id": "71de6463d2318580ab7896a185d7fcd8d02b165d",
+          "url": "https://github.com/tokio-rs/tokio/commit/23fdc2b3c4b33300c3e8b44dcb126057d90f4935"
+        },
+        "date": 1612560032017,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 174799,
+            "range": "± 18528",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 672039,
+            "range": "± 82535",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 4433220,
+            "range": "± 1038679",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 18879651,
+            "range": "± 2124834",
             "unit": "ns/iter"
           }
         ]
