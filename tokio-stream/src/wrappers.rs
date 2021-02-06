0506a58f217fca90007d019bc32929f74ed0c2fd
Alice Ryhl <alice@ryhl.io>
2021-02-06 22:11:48 +0100

    rustfmt

--- tokio-stream/src/wrappers.rs
+++ tokio-stream/src/wrappers.rs
@@ -1,7 +1,13 @@
 //! Wrappers for Tokio types that implement `Stream`.
 //!
-#![cfg_attr(unix, doc = "You are viewing documentation built under unix. To view windows-specific wrappers, change to the `x86_64-pc-windows-msvc` platform.")]
-#![cfg_attr(windows, doc = "You are viewing documentation built under windows. To view unix-specific wrappers, change to the `x86_64-unknown-linux-gnu` platform.")]
+#![cfg_attr(
+    unix,
+    doc = "You are viewing documentation built under unix. To view windows-specific wrappers, change to the `x86_64-pc-windows-msvc` platform."
+)]
+#![cfg_attr(
+    windows,
+    doc = "You are viewing documentation built under windows. To view unix-specific wrappers, change to the `x86_64-unknown-linux-gnu` platform."
+)]
 
 /// Error types for the wrappers.
 pub mod errors {
