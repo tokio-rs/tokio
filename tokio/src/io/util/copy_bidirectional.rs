eaf461616c65e2dec6f54218afc4c4afccc0a6a7
conblem <mail@conblem.me>
2021-03-19 12:46:09 +0100

    run fmt

--- tokio/src/io/util/copy_bidirectional.rs
+++ tokio/src/io/util/copy_bidirectional.rs
@@ -2,31 +2,33 @@ use super::copy::CopyBuffer;
 
 use crate::io::{AsyncRead, AsyncWrite};
 
-use std::task::{Context, Poll};
+use std::future::Future;
 use std::io;
 use std::pin::Pin;
-use std::future::Future;
+use std::task::{Context, Poll};
 
 enum TransferState {
     Running(CopyBuffer),
     ShuttingDown(u64),
-    Done(u64)
+    Done(u64),
 }
 
 struct CopyBidirectional<'a, A: ?Sized, B: ?Sized> {
     a: &'a mut A,
     b: &'a mut B,
     a_to_b: TransferState,
-    b_to_a: TransferState
+    b_to_a: TransferState,
 }
 
 fn transfer_one_direction<A, B>(
     cx: &mut Context<'_>,
     state: &mut TransferState,
     r: &mut A,
-    w: &mut B
+    w: &mut B,
 ) -> Poll<io::Result<u64>>
-    where A:  AsyncRead + AsyncWrite + Unpin + ?Sized, B: AsyncRead + AsyncWrite + Unpin + ?Sized
+where
+    A: AsyncRead + AsyncWrite + Unpin + ?Sized,
+    B: AsyncRead + AsyncWrite + Unpin + ?Sized,
 {
     let mut r = Pin::new(r);
     let mut w = Pin::new(w);
@@ -48,13 +50,20 @@ fn transfer_one_direction<A, B>(
 }
 
 impl<'a, A, B> Future for CopyBidirectional<'a, A, B>
-    where A: AsyncRead + AsyncWrite + Unpin + ?Sized, B: AsyncRead + AsyncWrite + Unpin + ?Sized
+where
+    A: AsyncRead + AsyncWrite + Unpin + ?Sized,
+    B: AsyncRead + AsyncWrite + Unpin + ?Sized,
 {
     type Output = io::Result<(u64, u64)>;
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         // Unpack self into mut refs to each field to avoid borrow check issues.
-        let CopyBidirectional { a, b, a_to_b, b_to_a } = &mut *self;
+        let CopyBidirectional {
+            a,
+            b,
+            a_to_b,
+            b_to_a,
+        } = &mut *self;
 
         let a_to_b = transfer_one_direction(cx, a_to_b, &mut *a, &mut *b)?;
         let b_to_a = transfer_one_direction(cx, b_to_a, &mut *b, &mut *a)?;
@@ -95,14 +104,16 @@ impl<'a, A, B> Future for CopyBidirectional<'a, A, B>
 /// # Return value
 ///
 /// Returns a tuple of bytes copied `a` to `b` and bytes copied `b` to `a`.
-pub async fn copy_bidirectional<A,B>(a: &mut A, b: &mut B)
-    -> Result<(u64, u64), std::io::Error>
-where A: AsyncRead + AsyncWrite + Unpin + ?Sized, B: AsyncRead + AsyncWrite + Unpin + ?Sized
+pub async fn copy_bidirectional<A, B>(a: &mut A, b: &mut B) -> Result<(u64, u64), std::io::Error>
+where
+    A: AsyncRead + AsyncWrite + Unpin + ?Sized,
+    B: AsyncRead + AsyncWrite + Unpin + ?Sized,
 {
     CopyBidirectional {
         a,
         b,
         a_to_b: TransferState::Running(CopyBuffer::new()),
         b_to_a: TransferState::Running(CopyBuffer::new()),
-    }.await
+    }
+    .await
 }
