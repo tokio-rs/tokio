2118986c1dbe64c639c41d335f7add341c72e095
github-action-benchmark <github@users.noreply.github.com>
2021-03-20 13:55:04 +0000

    add sync_mpsc (cargo) benchmark result for 0bfcbc8be5368bf2a203c2c2856e5d3e7f5154b3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1616248461803,
+  "lastUpdate": 1616248504184,
   "repoUrl": "https://github.com/tokio-rs/tokio",
   "entries": {
     "sync_rwlock": [
@@ -50579,6 +50579,90 @@ window.BENCHMARK_DATA = {
             "name": "uncontented_unbounded",
             "value": 729525,
             "range": "± 91387",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "david.pdrsn@gmail.com",
+            "name": "David Pedersen",
+            "username": "davidpdrsn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0bfcbc8be5368bf2a203c2c2856e5d3e7f5154b3",
+          "message": "runtime: fix unclear docs for `{Handle,Runtime}::block_on` (#3628)",
+          "timestamp": "2021-03-20T14:52:25+01:00",
+          "tree_id": "b5bc658aff9693ae146773d8c1ca1c775b404f45",
+          "url": "https://github.com/tokio-rs/tokio/commit/0bfcbc8be5368bf2a203c2c2856e5d3e7f5154b3"
+        },
+        "date": 1616248501566,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "contention_bounded",
+            "value": 7143035,
+            "range": "± 3066706",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_bounded_full",
+            "value": 6407240,
+            "range": "± 2179975",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_unbounded",
+            "value": 6415435,
+            "range": "± 1777016",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_000_medium",
+            "value": 713,
+            "range": "± 115",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_medium",
+            "value": 690,
+            "range": "± 114",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_1_medium",
+            "value": 695,
+            "range": "± 87",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_large",
+            "value": 140750,
+            "range": "± 32941",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_medium",
+            "value": 1853,
+            "range": "± 338",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_bounded",
+            "value": 1128686,
+            "range": "± 277554",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_unbounded",
+            "value": 767356,
+            "range": "± 140565",
             "unit": "ns/iter"
           }
         ]
