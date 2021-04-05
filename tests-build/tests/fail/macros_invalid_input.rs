618d2bfc717050faa0368115e6c341eb211c2ccd
David Pedersen <david.pdrsn@gmail.com>
2021-04-05 22:40:42 +0200

    macros: various error message improvements (#3677)
    Improves a few of the error messages for `#[tokio::main]` and `#[tokio::test]`.
    
    Also adds a note to the docs about `start_paused` requiring the `test-util` feature which wasn't mentioned previously.

--- tests-build/tests/fail/macros_invalid_input.rs
+++ tests-build/tests/fail/macros_invalid_input.rs
@@ -18,6 +18,24 @@ async fn test_fn_has_args(_x: u8) {}
 #[tokio::test(foo)]
 async fn test_attr_has_args() {}
 
+#[tokio::test(foo = 123)]
+async fn test_unexpected_attr() {}
+
+#[tokio::test(flavor = 123)]
+async fn test_flavor_not_string() {}
+
+#[tokio::test(flavor = "foo")]
+async fn test_unknown_flavor() {}
+
+#[tokio::test(flavor = "multi_thread", start_paused = false)]
+async fn test_multi_thread_with_start_paused() {}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = "foo")]
+async fn test_worker_threads_not_int() {}
+
+#[tokio::test(flavor = "current_thread", worker_threads = 4)]
+async fn test_worker_threads_and_current_thread() {}
+
 #[tokio::test]
 #[test]
 async fn test_has_second_test_attr() {}
