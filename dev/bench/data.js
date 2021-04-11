bfa0732ce9e8eca7cc0608df4f6dffc49aa092d6
github-action-benchmark <github@users.noreply.github.com>
2021-04-11 18:41:08 +0000

    add sync_rwlock (cargo) benchmark result for 28d68798973fbfaab889d73c4da7cbc2d5a0e2f8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1617915135295,
+  "lastUpdate": 1618166468528,
   "repoUrl": "https://github.com/tokio-rs/tokio",
   "entries": {
     "sync_rwlock": [
@@ -13013,6 +13013,60 @@ window.BENCHMARK_DATA = {
             "name": "read_uncontended",
             "value": 591,
             "range": "± 12",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "david.pdrsn@gmail.com",
+            "name": "David Pedersen",
+            "username": "davidpdrsn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "28d68798973fbfaab889d73c4da7cbc2d5a0e2f8",
+          "message": "macros: forward input arguments in `#[tokio::test]` (#3691)\n\nFixes #2388\r\n\r\nPreviously `#[tokio::test]` would error on functions that took\r\narguments. That meant other attribute macros couldn't do further\r\ntransformations on them. This changes that so arguments are forwarded as\r\nis.\r\n\r\nWhatever else might be included on the function is forwarded as well.\r\nFor example return type, generics, etc.\r\n\r\nWorth noting that this is only for compatibility with other macros.\r\n`#[test]`s that take arguments will still fail to compile.\r\n\r\nA bit odd that [trybuild] tests don't fail `#[test]` functions with\r\narguments which is why the new tests are run with `t.pass(...)`. They do\r\nactually fail if part of a real crate.\r\n\r\n[trybuild]: https://crates.io/crates/trybuild",
+          "timestamp": "2021-04-11T20:39:05+02:00",
+          "tree_id": "c50965c227bfc40bef8ff305d46f85318e04397d",
+          "url": "https://github.com/tokio-rs/tokio/commit/28d68798973fbfaab889d73c4da7cbc2d5a0e2f8"
+        },
+        "date": 1618166465766,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "read_concurrent_contended",
+            "value": 1046,
+            "range": "± 164",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_contended_multi",
+            "value": 17814,
+            "range": "± 11247",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_uncontended",
+            "value": 1049,
+            "range": "± 198",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_uncontended_multi",
+            "value": 16543,
+            "range": "± 9210",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_uncontended",
+            "value": 596,
+            "range": "± 103",
             "unit": "ns/iter"
           }
         ]
