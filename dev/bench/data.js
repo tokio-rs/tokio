6344961d75f8d9e5fee760b32c43bebbfe825ecf
github-action-benchmark <github@users.noreply.github.com>
2021-04-21 22:25:38 +0000

    add rt_multi_threaded (cargo) benchmark result for 2b9b55810847b4c7855e3de82f432ca997600f30

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1619043922736,
+  "lastUpdate": 1619043938483,
   "repoUrl": "https://github.com/tokio-rs/tokio",
   "entries": {
     "sync_rwlock": [
@@ -25909,6 +25909,54 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 22186596,
             "range": "± 3340623",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@carllerche.com",
+            "name": "Carl Lerche",
+            "username": "carllerche"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2b9b55810847b4c7855e3de82f432ca997600f30",
+          "message": "time: prevent `time::advance` from going too far (#3712)\n\nPreviously, `time::advance` would set the mocked clock forward the\r\nrequested amount, then yield. However, if there was no work ready to\r\nperform immediately, this would result in advancing to the next expiring\r\nsleep.\r\n\r\nNow, `time::advance(...)` will unblock at the requested time. The\r\ndifference between `time::advance(...)` and `time::sleep(...)` is a bit\r\nfuzzy. The main difference is `time::sleep(...)` operates on the current\r\ntask and `time::advance(...)` operates at the runtime level.\r\n\r\nFixes #3710",
+          "timestamp": "2021-04-21T15:23:35-07:00",
+          "tree_id": "6e5d0c6d86afdeaee3700f2851df5e78bfe7f8a4",
+          "url": "https://github.com/tokio-rs/tokio/commit/2b9b55810847b4c7855e3de82f432ca997600f30"
+        },
+        "date": 1619043935526,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 196447,
+            "range": "± 15673",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 740716,
+            "range": "± 81081",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 5217970,
+            "range": "± 979656",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 20228907,
+            "range": "± 3922397",
             "unit": "ns/iter"
           }
         ]
