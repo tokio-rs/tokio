11572617851090a26af26d98f7cf7ab110fa525f
Hy Che <giahy.che@shodan.io>
2021-05-04 01:14:21 +0700

    stream: Add Error trait impl for BroadcastStreamRecvError

--- tokio-stream/src/wrappers/broadcast.rs
+++ tokio-stream/src/wrappers/broadcast.rs
@@ -17,6 +17,26 @@ pub struct BroadcastStream<T> {
     inner: ReusableBoxFuture<(Result<T, RecvError>, Receiver<T>)>,
 }
 
+/// An error returned from the inner stream of a [`BroadcastStream`].
+#[derive(Debug, PartialEq)]
+pub enum BroadcastStreamRecvError {
+    /// The receiver lagged too far behind. Attempting to receive again will
+    /// return the oldest message still retained by the channel.
+    ///
+    /// Includes the number of skipped messages.
+    Lagged(u64),
+}
+
+impl fmt::Display for BroadcastStreamRecvError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            BroadcastStreamRecvError::Lagged(amt) => write!(f, "channel lagged by {}", amt),
+        }
+    }
+}
+
+impl std::error::Error for BroadcastStreamRecvError {}
+
 async fn make_future<T: Clone>(mut rx: Receiver<T>) -> (Result<T, RecvError>, Receiver<T>) {
     let result = rx.recv().await;
     (result, rx)
@@ -32,14 +52,16 @@ impl<T: 'static + Clone + Send> BroadcastStream<T> {
 }
 
 impl<T: 'static + Clone + Send> Stream for BroadcastStream<T> {
-    type Item = Result<T, RecvError>;
+    type Item = Result<T, BroadcastStreamRecvError>;
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let (result, rx) = ready!(self.inner.poll(cx));
         self.inner.set(make_future(rx));
         match result {
             Ok(item) => Poll::Ready(Some(Ok(item))),
             Err(RecvError::Closed) => Poll::Ready(None),
-            Err(err) => Poll::Ready(Some(Err(err))),
+            Err(RecvError::Lagged(n)) => {
+                Poll::Ready(Some(Err(BroadcastStreamRecvError::Lagged(n))))
+            }
         }
     }
 }
