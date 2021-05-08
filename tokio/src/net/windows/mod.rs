ff4c6b30d0e431b528354df3770743960a186506
John-John Tedro <udoprog@tedro.se>
2021-05-08 19:53:41 +0200

    rustfmt

--- tokio/src/net/windows/mod.rs
+++ tokio/src/net/windows/mod.rs
@@ -1,4 +1,6 @@
 //! Windows specific network types.
 
 mod named_pipe;
-pub use self::named_pipe::{NamedPipe, NamedPipeBuilder, NamedPipeClientBuilder, PipeMode, PipeEnd};
+pub use self::named_pipe::{
+    NamedPipe, NamedPipeBuilder, NamedPipeClientBuilder, PipeEnd, PipeMode,
+};
