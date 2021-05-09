b46f4808334fada4da1881f9419fec6563669dd2
Taiki Endo <te316e89@gmail.com>
2021-05-10 03:41:17 +0900

    net: hide net::unix::datagram module from docs

--- tokio/src/net/unix/mod.rs
+++ tokio/src/net/unix/mod.rs
@@ -1,5 +1,9 @@
 //! Unix domain socket utility types
 
+// This module does not currently provide any public API, but it was
+// unintentionally defined as a public module. Hide it from the documentation
+// instead of changing it to a private module to avoid breakage.
+#[doc(hidden)]
 pub mod datagram;
 
 pub(crate) mod listener;
