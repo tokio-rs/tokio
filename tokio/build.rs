bf8960c0e65fa9f5bb8666a8086694b2f7c79198
John-John Tedro <udoprog@tedro.se>
2021-05-17 12:43:46 +0200

    assert platform-minimum requirements at build time

--- tokio/build.rs
+++ tokio/build.rs
@@ -19,4 +19,29 @@ fn main() {
             );
         }
     }
+
+    assert_platform_minimums();
+}
+
+/// Assert platform-minimum requirements for Tokio to work correctly. This might
+/// be feasible to do as a constant fn, once MSRC is bumped a bit more.
+fn assert_platform_minimums() {
+    use std::mem;
+    use std::sync::atomic::{AtomicIsize, AtomicUsize};
+
+    if mem::size_of::<usize>() < 4 {
+        panic!("Tokio only works correctly if usize is at least 4 bytes.");
+    }
+
+    if mem::size_of::<isize>() < 4 {
+        panic!("Tokio only works correctly if isize is at least 4 bytes.");
+    }
+
+    if mem::size_of::<AtomicUsize>() < 4 {
+        panic!("Tokio only works correctly if AtomicUsize is at least 4 bytes.");
+    }
+
+    if mem::size_of::<AtomicIsize>() < 4 {
+        panic!("Tokio only works correctly if AtomicIsize is at least 4 bytes.");
+    }
 }
