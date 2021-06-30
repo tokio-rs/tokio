22f96b35a6a28ca93db8ddaa8a57fe75bdb7ac5f
Alice Ryhl <alice@ryhl.io>
2021-06-30 17:59:16 +0200

    test: test with tracing

--- tokio/src/runtime/tests/loom_queue.rs
+++ tokio/src/runtime/tests/loom_queue.rs
@@ -30,7 +30,7 @@ fn basic() {
 
         for _ in 0..2 {
             for _ in 0..2 {
-                let (task, _) = task::joinable::<_, Runtime>(async {});
+                let (task, _) = super::joinable::<_, Runtime>(async {});
                 local.push_back(task, &inject);
             }
 
@@ -39,7 +39,7 @@ fn basic() {
             }
 
             // Push another task
-            let (task, _) = task::joinable::<_, Runtime>(async {});
+            let (task, _) = super::joinable::<_, Runtime>(async {});
             local.push_back(task, &inject);
 
             while local.pop().is_some() {
@@ -81,7 +81,7 @@ fn steal_overflow() {
         let mut n = 0;
 
         // push a task, pop a task
-        let (task, _) = task::joinable::<_, Runtime>(async {});
+        let (task, _) = super::joinable::<_, Runtime>(async {});
         local.push_back(task, &inject);
 
         if local.pop().is_some() {
@@ -89,7 +89,7 @@ fn steal_overflow() {
         }
 
         for _ in 0..6 {
-            let (task, _) = task::joinable::<_, Runtime>(async {});
+            let (task, _) = super::joinable::<_, Runtime>(async {});
             local.push_back(task, &inject);
         }
 
@@ -133,7 +133,7 @@ fn multi_stealer() {
 
         // Push work
         for _ in 0..NUM_TASKS {
-            let (task, _) = task::joinable::<_, Runtime>(async {});
+            let (task, _) = super::joinable::<_, Runtime>(async {});
             local.push_back(task, &inject);
         }
 
@@ -170,10 +170,10 @@ fn chained_steal() {
 
         // Load up some tasks
         for _ in 0..4 {
-            let (task, _) = task::joinable::<_, Runtime>(async {});
+            let (task, _) = super::joinable::<_, Runtime>(async {});
             l1.push_back(task, &inject);
 
-            let (task, _) = task::joinable::<_, Runtime>(async {});
+            let (task, _) = super::joinable::<_, Runtime>(async {});
             l2.push_back(task, &inject);
         }
 
--- tokio/src/runtime/tests/task.rs
+++ tokio/src/runtime/tests/task.rs
@@ -7,13 +7,13 @@ use std::sync::Arc;
 
 #[test]
 fn create_drop() {
-    let _ = task::joinable::<_, Runtime>(async { unreachable!() });
+    let _ = super::joinable::<_, Runtime>(async { unreachable!() });
 }
 
 #[test]
 fn schedule() {
     with(|rt| {
-        let (task, _) = task::joinable(async {
+        let (task, _) = super::joinable(async {
             crate::task::yield_now().await;
         });
 
@@ -26,7 +26,7 @@ fn schedule() {
 #[test]
 fn shutdown() {
     with(|rt| {
-        let (task, _) = task::joinable(async {
+        let (task, _) = super::joinable(async {
             loop {
                 crate::task::yield_now().await;
             }
