8085daa311e97a19bf05612954e204f18be91f2f
Alice Ryhl <alice@ryhl.io>
2021-07-07 16:31:43 +0200

    Move inject to cfg_rt_multi_thread

--- tokio/src/runtime/task/mod.rs
+++ tokio/src/runtime/task/mod.rs
@@ -9,8 +9,10 @@ pub use self::error::JoinError;
 mod harness;
 use self::harness::Harness;
 
-mod inject;
-pub(super) use self::inject::Inject;
+cfg_rt_multi_thread! {
+    mod inject;
+    pub(super) use self::inject::Inject;
+}
 
 mod join;
 #[allow(unreachable_pub)] // https://github.com/rust-lang/rust/issues/57411
