17699f6c74911d65f97125ac33b36169b48eca65
Alice Ryhl <alice@ryhl.io>
2021-07-13 13:10:55 +0200

    Fix imports

--- tokio/src/runtime/tests/loom_queue.rs
+++ tokio/src/runtime/tests/loom_queue.rs
@@ -1,6 +1,6 @@
 use crate::runtime::blocking::NoopSchedule;
 use crate::runtime::queue;
-use crate::runtime::task::{self, Inject, Schedule, Task};
+use crate::runtime::task::Inject;
 
 use loom::thread;
 
