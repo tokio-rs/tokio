32000c9a7f6c4643ab97f16b9d960b5487ffbbca
Alan Somers <asomers@gmail.com>
2021-07-25 16:48:49 -0600

    Fix the build on Windows

--- tokio/src/fs/open_options/mock_open_options.rs
+++ tokio/src/fs/open_options/mock_open_options.rs
@@ -4,6 +4,8 @@ use mockall::mock;
 use crate::fs::mocks::MockFile;
 #[cfg(unix)]
 use std::os::unix::fs::OpenOptionsExt;
+#[cfg(windows)]
+use std::os::windows::fs::OpenOptionsExt;
 use std::{io, path::Path};
 
 mock! {
@@ -20,8 +22,17 @@ mock! {
     impl Clone for OpenOptions {
         fn clone(&self) -> Self;
     }
+    #[cfg(unix)]
     impl OpenOptionsExt for OpenOptions {
         fn custom_flags(&mut self, flags: i32) -> &mut Self;
         fn mode(&mut self, mode: u32) -> &mut Self;
+    }
+    #[cfg(windows)]
+    impl OpenOptionsExt for OpenOptions {
+        fn access_mode(&mut self, access: u32) -> &mut Self;
+        fn share_mode(&mut self, val: u32) -> &mut Self;
+        fn custom_flags(&mut self, flags: u32) -> &mut Self;
+        fn attributes(&mut self, val: u32) -> &mut Self;
+        fn security_qos_flags(&mut self, flags: u32) -> &mut Self;
     }
 }
