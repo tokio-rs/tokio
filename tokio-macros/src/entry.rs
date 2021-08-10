2ee2a0d346f6c63345af737344e75ccf81227963
Kateřina Churanová <katerina@churanova.eu>
2021-08-10 22:40:21 +0200

    fix: making the expect valid only to the builder, and not the closure itself

--- tokio-macros/src/entry.rs
+++ tokio-macros/src/entry.rs
@@ -325,12 +325,13 @@ fn parse_knobs(
     let brace_token = input.block.brace_token;
     input.block = syn::parse2(quote_spanned! {last_stmt_end_span=>
         {
+            let body_closure = || async #body;
             #[allow(clippy::expect_used)]
             #rt
                 .enable_all()
                 .build()
                 .expect("Failed building the Runtime")
-                .block_on(async #body)
+                .block_on(body_closure())
         }
     })
     .expect("Parsing failure");
