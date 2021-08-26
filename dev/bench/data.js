4540ca722bdf5ebed730ea4b73c02ae71533f0e3
github-action-benchmark <github@users.noreply.github.com>
2021-08-26 18:35:12 +0000

    add sync_rwlock (cargo) benchmark result for 1e2e38b7cdf4c9f51a0034469c8553d995af1383

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1629928149902,
+  "lastUpdate": 1630002911948,
   "repoUrl": "https://github.com/tokio-rs/tokio",
   "entries": {
     "sync_rwlock": [
@@ -23705,6 +23705,60 @@ window.BENCHMARK_DATA = {
             "name": "read_uncontended",
             "value": 630,
             "range": "± 133",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "eliza@buoyant.io",
+            "name": "Eliza Weisman",
+            "username": "hawkw"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1e2e38b7cdf4c9f51a0034469c8553d995af1383",
+          "message": "sync: use `WakeList` in `Notify` and `batch_semaphore` (#4071)\n\n## Motivation\r\n\r\nPR #4055 added a new `WakeList` type, to manage a potentially\r\nuninitialized array when waking batches of wakers. This has the\r\nadvantage of not initializing a bunch of empty `Option`s when only a\r\nsmall number of tasks are being woken, potentially improving performance\r\nin these cases.\r\n\r\nCurrently, `WakeList` is used only in the IO driver. However,\r\n`tokio::sync` contains some code that's almost identical to the code in\r\nthe IO driver that was replaced with `WakeList`, so we can apply the\r\nsame optimizations there.\r\n\r\n## Solution\r\n\r\nThis branch changes `tokio::sync::Notify` and\r\n`tokio::sync::batch_semaphore::Semaphore` to use `WakeList` when waking\r\nbatches of wakers. This was a pretty straightforward drop-in\r\nreplacement.\r\n\r\nSigned-off-by: Eliza Weisman <eliza@buoyant.io>",
+          "timestamp": "2021-08-26T11:33:22-07:00",
+          "tree_id": "d74ef96d6251eefbbe6d2eff7f92b3dc494e2900",
+          "url": "https://github.com/tokio-rs/tokio/commit/1e2e38b7cdf4c9f51a0034469c8553d995af1383"
+        },
+        "date": 1630002904038,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "read_concurrent_contended",
+            "value": 852,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_contended_multi",
+            "value": 18082,
+            "range": "± 6911",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_uncontended",
+            "value": 868,
+            "range": "± 6",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_uncontended_multi",
+            "value": 17974,
+            "range": "± 6919",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_uncontended",
+            "value": 543,
+            "range": "± 72",
             "unit": "ns/iter"
           }
         ]
