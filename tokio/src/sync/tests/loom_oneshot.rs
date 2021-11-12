844dc9be2f95e2403dc50562333090af7d2d20a5
Eliza Weisman <eliza@buoyant.io>
2021-11-12 09:42:00 -0800

    loom test for #4225
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio/src/sync/tests/loom_oneshot.rs
+++ tokio/src/sync/tests/loom_oneshot.rs
@@ -55,6 +55,21 @@ fn changing_rx_task() {
     });
 }
 
+#[test]
+fn recv_close() {
+    // reproduces https://github.com/tokio-rs/tokio/issues/4225
+    loom::model(|| {
+        let (tx, mut rx) = oneshot::channel();
+        thread::spawn(move || {
+            let _ = tx.send(());
+        });
+        thread::spawn(move || {
+            rx.close();
+            let _ = rx.try_recv();
+        });
+    })
+}
+
 // TODO: Move this into `oneshot` proper.
 
 use std::future::Future;
