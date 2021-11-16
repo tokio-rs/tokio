095b5dcf93655ae48c789feb81fb3998b8cae0fb
Alice Ryhl <alice@ryhl.io>
2021-11-16 22:22:47 +0100

    net: add connect_std -> TcpSocket doc alias (#4219)

--- tokio/src/net/tcp/socket.rs
+++ tokio/src/net/tcp/socket.rs
@@ -81,6 +81,7 @@ cfg_net! {
     /// [`AsRawFd`]: https://doc.rust-lang.org/std/os/unix/io/trait.AsRawFd.html
     /// [`AsRawSocket`]: https://doc.rust-lang.org/std/os/windows/io/trait.AsRawSocket.html
     /// [`socket2`]: https://docs.rs/socket2/
+    #[cfg_attr(docsrs, doc(alias = "connect_std"))]
     pub struct TcpSocket {
         inner: mio::net::TcpSocket,
     }
