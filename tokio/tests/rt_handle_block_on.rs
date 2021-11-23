1a423b3322bd32afc761a2a39fc986909aab633d
Taiki Endo <te316e89@gmail.com>
2021-11-23 19:53:32 +0900

    chore: remove doc URL from Cargo.toml (#4251)
    https://doc.rust-lang.org/cargo/reference/manifest.html#the-documentation-field
    
    > If no URL is specified in the manifest file, crates.io will
    > automatically link your crate to the corresponding docs.rs page.

--- tokio/tests/rt_handle_block_on.rs
+++ tokio/tests/rt_handle_block_on.rs
@@ -135,7 +135,7 @@ rt_test! {
         let contents = Handle::current()
             .block_on(fs::read_to_string("Cargo.toml"))
             .unwrap();
-        assert!(contents.contains("Cargo.toml"));
+        assert!(contents.contains("https://tokio.rs"));
     }
 
     #[test]
