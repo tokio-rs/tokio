e2c6c686f86c18cf48cf8d0864bb0960e220ae7d
Kevin (Kun) Kassimo Qian <kqian@google.com>
2021-12-04 13:10:16 -0800

    Opt out FreeBSD test on permission since root user can always stat

--- tokio/tests/fs_try_exists.rs
+++ tokio/tests/fs_try_exists.rs
@@ -17,25 +17,29 @@ async fn try_exists() {
     assert_eq!(fs::try_exists(existing_path).await.unwrap(), true);
     assert_eq!(fs::try_exists(nonexisting_path).await.unwrap(), false);
 
-    let permission_denied_directory_path = dir.path().join("baz");
-    fs::create_dir(&permission_denied_directory_path)
-        .await
-        .unwrap();
-    let permission_denied_file_path = permission_denied_directory_path.join("baz.txt");
-    fs::write(&permission_denied_file_path, b"Hello File!")
-        .await
-        .unwrap();
-    let mut perms = tokio::fs::metadata(&permission_denied_directory_path)
-        .await
-        .unwrap()
-        .permissions();
-    perms.set_mode(0o244);
-    fs::set_permissions(&permission_denied_directory_path, perms)
-        .await
-        .unwrap();
-    let permission_denied_result = fs::try_exists(permission_denied_file_path).await;
-    assert_eq!(
-        permission_denied_result.err().unwrap().kind(),
-        std::io::ErrorKind::PermissionDenied
-    );
+    // FreeBSD root user always has permission to stat.
+    #[cfg(not(freebsd))]
+    {
+        let permission_denied_directory_path = dir.path().join("baz");
+        fs::create_dir(&permission_denied_directory_path)
+            .await
+            .unwrap();
+        let permission_denied_file_path = permission_denied_directory_path.join("baz.txt");
+        fs::write(&permission_denied_file_path, b"Hello File!")
+            .await
+            .unwrap();
+        let mut perms = tokio::fs::metadata(&permission_denied_directory_path)
+            .await
+            .unwrap()
+            .permissions();
+        perms.set_mode(0o244);
+        fs::set_permissions(&permission_denied_directory_path, perms)
+            .await
+            .unwrap();
+        let permission_denied_result = fs::try_exists(permission_denied_file_path).await;
+        assert_eq!(
+            permission_denied_result.err().unwrap().kind(),
+            std::io::ErrorKind::PermissionDenied
+        );
+    }
 }
