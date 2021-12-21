78e0f0b42a4f7a50f3986f576703e5a3cb473b79
Eliza Weisman <eliza@buoyant.io>
2021-12-21 11:11:48 -0800

    docs: improve RustDoc for unstable features (#4331)
    Currently, the docs.rs documentation for tokio is built without
    --cfg tokio_unstable set. This means that unstable features are not shown in
    the API docs, making them difficutl to discover. Clearly, we do want to
    document the existence of unstable APIs, given that there's a section in
    the lib.rs documentation listing them, so it would be better if it was
    also possible to determine what APIs an unstable feature enables when
    reading the RustDoc documentation.
    
    This branch changes the docs.rs metadata to also pass --cfg tokio_unstable
    when building the documentation. It turns out that it's
    necessary to separately pass the cfg flag to both RustDoc and rustc,
    or else the tracing dependency, which is only enabled in
    target.cfg(tokio_unstable).dependencies, will be missing and the build
    will fail.
    
    In addition, I made some minor improvements to the docs for unstable
    features. Some links in the task::Builder docs were broken, and the
    required tokio_unstable cfg was missing from the doc(cfg(...))
    attributes. Furthermore, I added a note in the top-level docs for
    unstable APIs, stating that they are unstable and linking back to the
    section in the crate-level docs that explains how to enable unstable
    features.
    
    Fixes #4328

--- tokio/src/macros/cfg.rs
+++ tokio/src/macros/cfg.rs
@@ -178,7 +178,7 @@ macro_rules! cfg_stats {
     ($($item:item)*) => {
         $(
             #[cfg(all(tokio_unstable, feature = "stats"))]
-            #[cfg_attr(docsrs, doc(cfg(feature = "stats")))]
+            #[cfg_attr(docsrs, doc(cfg(all(tokio_unstable, feature = "stats"))))]
             $item
         )*
     }
@@ -365,7 +365,7 @@ macro_rules! cfg_trace {
     ($($item:item)*) => {
         $(
             #[cfg(all(tokio_unstable, feature = "tracing"))]
-            #[cfg_attr(docsrs, doc(cfg(feature = "tracing")))]
+            #[cfg_attr(docsrs, doc(cfg(all(tokio_unstable, feature = "tracing"))))]
             $item
         )*
     };
--- tokio/src/runtime/stats/mod.rs
+++ tokio/src/runtime/stats/mod.rs
@@ -1,5 +1,11 @@
 //! This module contains information need to view information about how the
 //! runtime is performing.
+//!
+//! **Note**: This is an [unstable API][unstable]. The public API of types in
+//! this module may break in 1.x releases. See [the documentation on unstable
+//! features][unstable] for details.
+//!
+//! [unstable]: crate#unstable-features
 #![allow(clippy::module_inception)]
 
 cfg_stats! {
