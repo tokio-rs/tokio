6122d6d55ef1362de6091de0415470ae2936380b
John-John Tedro <udoprog@tedro.se>
2022-02-18 19:07:19 +0100

    fix trybuild output

--- tests-build/tests/fail/macros_main_generics.rs
+++ tests-build/tests/fail/macros_main_generics.rs
@@ -2,6 +2,9 @@ use tests_build::tokio;
 
 // This should parse but fail since default values for const generic parameters
 // are experimental.
+//
+// TODO(udoprog): might want to version constrain this check with `rustversion`
+// since this won't always be experimental moving forward (!).
 #[tokio::main]
 async fn where_clause_const_generics<const T: usize = { 1 + 1 }>() {}
 
