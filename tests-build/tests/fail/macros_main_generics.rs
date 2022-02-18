53690436836e4552027691b774b89defdc1c2bcd
John-John Tedro <udoprog@tedro.se>
2022-02-18 17:50:00 +0100

    rustfmt

--- tests-build/tests/fail/macros_main_generics.rs
+++ tests-build/tests/fail/macros_main_generics.rs
@@ -3,8 +3,6 @@ use tests_build::tokio;
 // This should parse but fail since default values for const generic parameters
 // are experimental.
 #[tokio::main]
-async fn where_clause_const_generics<const T: usize = {1 + 1}>() {
-}
+async fn where_clause_const_generics<const T: usize = { 1 + 1 }>() {}
 
-fn main() {
-}
+fn main() {}
--- tests-build/tests/pass/macros_main_generics.rs
+++ tests-build/tests/pass/macros_main_generics.rs
@@ -3,14 +3,16 @@ use tests_build::tokio;
 use std::fmt::Debug;
 
 #[tokio::main]
-async fn single_parameter<T>() {
-}
+async fn single_parameter<T>() {}
 
 // This should parse since we treat angle brackets as groups during parsing and
 // simply skip over them.
 #[tokio::main]
-async fn where_clause<T>() where T: Iterator, <T as Iterator>::Item: Debug {
+async fn where_clause<T>()
+where
+    T: Iterator,
+    <T as Iterator>::Item: Debug,
+{
 }
 
-fn main() {
-}
+fn main() {}
