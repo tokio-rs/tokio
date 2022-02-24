03d1bf923969a14fb68cc494c6f7fe6ce9bf5c3c
Eliza Weisman <eliza@buoyant.io>
2022-02-24 12:08:18 -0800

    add boilerplate hashmap capacity management
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-util/src/task/join_map.rs
+++ tokio-util/src/task/join_map.rs
@@ -10,15 +10,68 @@ use tokio::{
     task::{AbortHandle, JoinError, JoinSet, LocalSet},
 };
 
+/// A collection of tasks spawned on a Tokio runtime, associated with hash map
+/// keys.
+///
+/// This type is very similar to the [`JoinSet`] type in `tokio::task`, with the
+/// addition of a set of keys associated with each task. These keys allow
+/// [cancelling a task](JoinMap::abort) in the `JoinMap` by key, or [test whether
+/// a task corresponding to a given key exists](JoinMap::contains_active_task)
+/// in the `JoinMap`.
+///
+/// In addition, when tasks in the `JoinMap` complete, they will return the
+/// associated key along with the value returned by the task, if any.
+///
+/// A `JoinMap` can be used to await the completion of some or all of the tasks
+/// in the map. The map is not ordered, and the tasks will be returned in the
+/// order they complete.
+///
+/// All of the tasks must have the same return type `V`.
+///
+/// When the `JoinMap` is dropped, all tasks in the `JoinMap` are immediately aborted.
+///
+/// **Note**: This is an [unstable API][unstable]. The public API of this type
+/// may break in 1.x releases. See [the documentation on unstable
+/// features][unstable] for details.
+///
+/// [unstable]: tokio#unstable-features
+#[cfg_attr(docsrs, doc(cfg(all(feature = "rt", tokio_unstable))))]
 pub struct JoinMap<K, V, S = RandomState> {
     aborts: HashMap<K, AbortHandle, S>,
     joins: JoinSet<(K, V)>,
 }
 
 impl<K, V> JoinMap<K, V> {
-    /// Create a new `JoinMap`.
+    /// Create a new empty `JoinMap`.
+    ///
+    /// The `JoinMap` is initially created with a capacity of 0, so it will not
+    /// allocate until a task is first spawned on it.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tokio_util::task::JoinMap;
+    /// let mut map: JoinMap<&str, i32> = JoinMap::new();
+    /// ```
     pub fn new() -> Self {
         Self::with_hasher(RandomState::new())
+    }
+
+    /// Creates an empty `JoinMap` with the specified capacity.
+    ///
+    /// The `JoinMap` will be able to hold at least `capacity` tasks without
+    /// reallocating. If `capacity` is 0, the `JoinMap` will not allocate.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tokio_util::task::JoinMap;
+    /// let mut map: JoinMap<&str, i32> = JoinMap::with_capacity(10);
+    /// ```
+    #[inline]
+    #[must_use]
+    pub fn with_capacity(capacity: usize) -> Self {
+        JoinMap::with_capacity_and_hasher(capacity, Default::default())
     }
 }
 
@@ -29,27 +82,47 @@ impl<K, V, S> JoinMap<K, V, S> {
     /// The created map has the default initial capacity.
     ///
     /// Warning: `hash_builder` is normally randomly generated, and
+    /// is designed to allow `JoinMap` to be resistant to attacks that
+    /// cause many collisions and very poor performance. Setting it
+    /// manually using this function can expose a DoS attack vector.
+    ///
+    /// The `hash_builder` passed should implement the [`BuildHasher`] trait for
+    /// the `JoinMap` to be useful, see its documentation for details.
+    #[inline]
+    #[must_use]
+    pub fn with_hasher(hash_builder: S) -> Self {
+        Self::with_capacity_and_hasher(0, hash_builder)
+    }
+
+    /// Creates an empty `JoinMap` with the specified capacity, using `hash_builder`
+    /// to hash the keys.
+    ///
+    /// The `JoinMap` will be able to hold at least `capacity` elements without
+    /// reallocating. If `capacity` is 0, the `JoinMap` will not allocate.
+    ///
+    /// Warning: `hash_builder` is normally randomly generated, and
     /// is designed to allow HashMaps to be resistant to attacks that
     /// cause many collisions and very poor performance. Setting it
     /// manually using this function can expose a DoS attack vector.
     ///
     /// The `hash_builder` passed should implement the [`BuildHasher`] trait for
-    /// the HashMap to be useful, see its documentation for details.
+    /// the `JoinMap`to be useful, see its documentation for details.
     ///
     /// # Examples
     ///
     /// ```
-    /// use std::collections::HashMap;
+    /// use tokio_util::task::JoinMap;
     /// use std::collections::hash_map::RandomState;
     ///
     /// let s = RandomState::new();
-    /// let mut map = HashMap::with_hasher(s);
-    /// map.insert(1, 2);
+    /// let mut map = JoinMap::with_capacity_and_hasher(10, s);
+    /// map.spawn(1, async move { "hello world!" });
     /// ```
     #[inline]
-    pub fn with_hasher(hash_builder: S) -> Self {
+    #[must_use]
+    pub fn with_capacity_and_hasher(capacity: usize, hash_builder: S) -> Self {
         Self {
-            aborts: HashMap::with_hasher(hash_builder),
+            aborts: HashMap::with_capacity_and_hasher(capacity, hash_builder),
             joins: JoinSet::new(),
         }
     }
@@ -62,6 +135,24 @@ impl<K, V, S> JoinMap<K, V, S> {
     /// Returns whether the `JoinMap` is empty.
     pub fn is_empty(&self) -> bool {
         self.joins.is_empty()
+    }
+
+    /// Returns the number of tasks the map can hold without reallocating.
+    ///
+    /// This number is a lower bound; the `JoinMap` might be able to hold
+    /// more, but is guaranteed to be able to hold at least this many.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tokio::task::JoinMap;
+    ///
+    /// let map: JoinMap<i32, i32> = JoinMap::with_capacity(100);
+    /// assert!(map.capacity() >= 100);
+    /// ```
+    #[inline]
+    pub fn capacity(&self) -> usize {
+        self.aborts.capacity()
     }
 }
 
@@ -91,10 +182,10 @@ where
     }
 
     /// Spawn the provided task on the provided runtime and store it in this
-    /// `JoinMap` returning an [`AbortHandle`] that can be used to remotely
-    /// cancel the task.
+    /// `JoinMap` with the provided key.
     ///
-    /// [`AbortHandle`]: crate::task::AbortHandle
+    /// If a task previously existed in the `JoinMap` for this key, that task
+    /// will be cancelled and replaced with the new one.
     pub fn spawn_on<F>(&mut self, key: K, task: F, handle: &Handle)
     where
         F: Future<Output = V>,
@@ -107,15 +198,16 @@ where
     }
 
     /// Spawn the provided task on the current [`LocalSet`] and store it in this
-    /// `JoinMap`, returning an [`AbortHandle`]  that can be used to remotely
-    /// cancel the task.
+    /// `JoinMap` with the provided key.
+    ///
+    /// If a task previously existed in the `JoinMap` for this key, that task
+    /// will be cancelled and replaced with the new one.
     ///
     /// # Panics
     ///
     /// This method panics if it is called outside of a `LocalSet`.
     ///
-    /// [`LocalSet`]: crate::task::LocalSet
-    /// [`AbortHandle`]: crate::task::AbortHandle
+    /// [`LocalSet`]: tokio::task::LocalSet
     pub fn spawn_local<F>(&mut self, key: K, task: F)
     where
         F: Future<Output = V>,
@@ -126,11 +218,12 @@ where
     }
 
     /// Spawn the provided task on the provided [`LocalSet`] and store it in
-    /// this `JoinMap`, returning an [`AbortHandle`] that can be used to
-    /// remotely cancel the task.
+    /// this `JoinMap` with the provided key.
     ///
-    /// [`LocalSet`]: crate::task::LocalSet
-    /// [`AbortHandle`]: crate::task::AbortHandle
+    /// If a task previously existed in the `JoinMap` for this key, that task
+    /// will be cancelled and replaced with the new one.
+    ///
+    /// [`LocalSet`]: tokio::task::LocalSet
     pub fn spawn_local_on<F>(&mut self, key: K, task: F, local_set: &LocalSet)
     where
         F: Future<Output = V>,
@@ -241,6 +334,98 @@ where
     {
         self.aborts.get(key).map_or(false, AbortHandle::is_active)
     }
+
+    /// Reserves capacity for at least `additional` more tasks to be spawned
+    /// on this `JoinMap` without reallocating. The collection may reserve more space to avoid
+    /// frequent reallocations.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the new allocation size overflows [`usize`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tokio_util::task::JoinMap;
+    ///
+    /// let mut map: JoinMap<&str, i32> = JoinMap::new();
+    /// map.reserve(10);
+    /// ```
+    #[inline]
+    pub fn reserve(&mut self, additional: usize) {
+        self.aborts.reserve(additional)
+    }
+
+    /// Tries to reserve capacity for at least `additional` more tasks to be spawned
+    /// on this `JoinMap` without reallocating. The collection may reserve more space to avoid
+    /// frequent reallocations.
+    ///
+    /// # Errors
+    ///
+    /// If the capacity overflows, or the allocator reports a failure, then an error
+    /// is returned.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tokio_util::task::JoinMap;
+    ///
+    /// let mut map: JoinMap<&str, i32> = JoinMap::new();
+    /// map.try_reserve(10).expect("why is the test harness OOMing on 10 bytes?");
+    /// ```
+    #[inline]
+    pub fn try_reserve(
+        &mut self,
+        additional: usize,
+    ) -> Result<(), std::collections::TryReserveError> {
+        self.aborts.try_reserve(additional)
+    }
+
+    /// Shrinks the capacity of the `JoinMap` as much as possible. It will drop
+    /// down as much as possible while maintaining the internal rules
+    /// and possibly leaving some space in accordance with the resize policy.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tokio_util::task::JoinMap;
+    ///
+    /// let mut map: JoinMap<i32, i32> = JoinMap::with_capacity(100);
+    /// map.spawn(1, async move { 2 });
+    /// map.spawn(3, async move { 4 });
+    /// assert!(map.capacity() >= 100);
+    /// map.shrink_to_fit();
+    /// assert!(map.capacity() >= 2);
+    /// ```
+    #[inline]
+    pub fn shrink_to_fit(&mut self) {
+        self.aborts.shrink_to_fit();
+    }
+
+    /// Shrinks the capacity of the map with a lower limit. It will drop
+    /// down no lower than the supplied limit while maintaining the internal rules
+    /// and possibly leaving some space in accordance with the resize policy.
+    ///
+    /// If the current capacity is less than the lower limit, this is a no-op.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tokio_util::task::JoinMap;
+    ///
+    /// let mut map: JoinMap<i32, i32> = JoinMap::with_capacity(100);
+    /// map.spawn(1, async move { 2 });
+    /// map.spawn(3, async move { 4 });
+    /// assert!(map.capacity() >= 100);
+    /// map.shrink_to(10);
+    /// assert!(map.capacity() >= 10);
+    /// map.shrink_to(0);
+    /// assert!(map.capacity() >= 2);
+    /// ```
+    #[inline]
+    pub fn shrink_to(&mut self, min_capacity: usize) {
+        self.aborts.shrink_to(min_capacity)
+    }
 }
 
 impl<K, V, S> JoinMap<K, V, S>
@@ -262,18 +447,21 @@ where
     /// The tasks removed by this call will continue to run in the background even if the `JoinMap`
     /// is dropped.
     pub fn detach_all(&mut self) {
-        self.joins.detach_all()
+        self.joins.detach_all();
+        self.aborts.clear();
     }
 }
 
 impl<K: fmt::Debug, V, S> fmt::Debug for JoinMap<K, V, S> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // debug print the keys in this `JoinMap`.
         struct KeySet<'a, K, V, S>(&'a JoinMap<K, V, S>);
         impl<K: fmt::Debug, V, S> fmt::Debug for KeySet<'_, K, V, S> {
             fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
                 f.debug_set().entries(self.0.aborts.keys()).finish()
             }
         }
+
         f.debug_struct("JoinMap")
             .field("keys", &KeySet(self))
             .finish()
