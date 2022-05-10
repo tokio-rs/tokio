ae7574bb2078abd377905b393906e691c4ec9284
Finomnis <finomnis@gmail.com>
2022-05-10 13:17:28 +0200

    Simplify the leaked_buffers struct

--- tokio/tests/support/leaked_buffers.rs
+++ tokio/tests/support/leaked_buffers.rs
@@ -2,13 +2,13 @@
 /// Frees created buffers when dropped.
 ///
 /// This struct is of course unsafe and the fact that
-/// it outlives the created buffers has to be managed by
+/// it must outlive the created slices has to be ensured by
 /// the programmer.
 ///
 /// Used at certain test scenarios as a safer version of
 /// Vec::leak, to satisfy the address sanitizer.
 pub struct LeakedBuffers {
-    leaked_vecs: Vec<(*const u8, usize)>,
+    leaked_vecs: Vec<Box<[u8]>>,
 }
 
 impl LeakedBuffers {
@@ -18,18 +18,9 @@ impl LeakedBuffers {
         }
     }
     pub fn create<'a>(&mut self, size: usize) -> &'a mut [u8] {
-        let slice = Box::leak(vec![0; size].into_boxed_slice());
-        self.leaked_vecs.push((slice.as_ptr(), slice.len()));
+        let mut new_mem = vec![0u8; size].into_boxed_slice();
+        let slice = unsafe { std::slice::from_raw_parts_mut(new_mem.as_mut_ptr(), new_mem.len()) };
+        self.leaked_vecs.push(new_mem);
         slice
-    }
-}
-
-impl Drop for LeakedBuffers {
-    fn drop(&mut self) {
-        for (ptr, len) in self.leaked_vecs.drain(..) {
-            unsafe {
-                Box::from_raw(std::slice::from_raw_parts_mut(ptr as *mut u8, len));
-            }
-        }
     }
 }
