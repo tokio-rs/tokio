0b9e7458f1cc4930715dc9dd4c925d9f12f2a3da
Alice Ryhl <aliceryhl@google.com>
2022-05-31 11:03:36 +0200

    Update tokio/src/runtime/task/join.rs

--- tokio/src/runtime/task/join.rs
+++ tokio/src/runtime/task/join.rs
@@ -214,21 +214,21 @@ impl<T> JoinHandle<T> {
     /// use tokio::time;
     ///
     /// # #[tokio::main(flavor = "current_thread")]
-    /// async fn main() {
-    /// #  time::pause();
-    ///    let handle1 = tokio::spawn(async {
-    ///        // do some stuff here
-    ///    });
-    ///    let handle2 = tokio::spawn(async {
-    ///        // do some other stuff here
-    ///        time::sleep(time::Duration::from_secs(10)).await;
-    ///    });
-    ///    // Wait for the task to finish
-    ///    handle2.abort();
-    ///    time::sleep(time::Duration::from_secs(1)).await;
-    ///    assert!(handle1.is_finished());
-    ///    assert!(handle2.is_finished());
-    /// }
+    /// # async fn main() {
+    /// # time::pause();
+    /// let handle1 = tokio::spawn(async {
+    ///     // do some stuff here
+    /// });
+    /// let handle2 = tokio::spawn(async {
+    ///     // do some other stuff here
+    ///     time::sleep(time::Duration::from_secs(10)).await;
+    /// });
+    /// // Wait for the task to finish
+    /// handle2.abort();
+    /// time::sleep(time::Duration::from_secs(1)).await;
+    /// assert!(handle1.is_finished());
+    /// assert!(handle2.is_finished());
+    /// # }
     /// ```
     /// [`abort`]: method@JoinHandle::abort
     pub fn is_finished(&self) -> bool {
