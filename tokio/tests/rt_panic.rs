2ab60c20a3b5609b2868670c8c5081aeb58322ce
Stainsby, Hayden <hayden.stainsby@here.com>
2022-06-27 17:37:31 +0200

    io: add track_caller to public APIs
    Functions that may panic can be annotated with #[track_caller] so that
    in the event of a panic, the function where the user called the
    panicking function is shown instead of the file and line within Tokio
    source.
    
    This change adds #[track_caller] to all the non-unstable public io APIs
    in the main tokio crate where the documentation describes how the
    function may panic due to incorrect context or inputs.
    
    Additionally, the documentation for `AsyncFd` was updated to indicate
    that the functions `new` and `with_intent` can panic.
    
    Tests are included to cover each potentially panicking function. The
    logic to test the location of a panic (which is a little complex), has
    been moved to a test support module.
    
    Refs: #4413

--- tokio/tests/rt_panic.rs
+++ tokio/tests/rt_panic.rs
@@ -2,42 +2,13 @@
 #![cfg(feature = "full")]
 
 use futures::future;
-use parking_lot::{const_mutex, Mutex};
 use std::error::Error;
-use std::panic;
-use std::sync::Arc;
 use tokio::runtime::{Builder, Handle, Runtime};
 
-fn test_panic<Func: FnOnce() + panic::UnwindSafe>(func: Func) -> Option<String> {
-    static PANIC_MUTEX: Mutex<()> = const_mutex(());
-
-    {
-        let _guard = PANIC_MUTEX.lock();
-        let panic_file: Arc<Mutex<Option<String>>> = Arc::new(Mutex::new(None));
-
-        let prev_hook = panic::take_hook();
-        {
-            let panic_file = panic_file.clone();
-            panic::set_hook(Box::new(move |panic_info| {
-                let panic_location = panic_info.location().unwrap();
-                panic_file
-                    .lock()
-                    .clone_from(&Some(panic_location.file().to_string()));
-            }));
-        }
-
-        let result = panic::catch_unwind(func);
-        // Return to the previously set panic hook (maybe default) so that we get nice error
-        // messages in the tests.
-        panic::set_hook(prev_hook);
-
-        if result.is_err() {
-            panic_file.lock().clone()
-        } else {
-            None
-        }
-    }
+mod support {
+    pub mod panic;
 }
+use support::panic::test_panic;
 
 #[test]
 fn current_handle_panic_caller() -> Result<(), Box<dyn Error>> {
