5e271d7347caa5d4171f3d38195e0ece81214fef
Noah Kennedy <nkennedy@cloudflare.com>
2022-07-01 13:30:22 -0500

    tests: alter integration tests for stdio to not use a handrolled cat implementation
    This fixes #4801, where, as a result of https://github.com/rust-lang/rust/pull/95469, our implementation of cat used for this test no longer works, as stdio functions on windows now can abort the process if the pipe is set to nonblocking mode.
    
    Unfortunately in windows, setting one end of the pipe to be nonblocking makes the whole thing nonblocking, so when, in tokio::process we set the child pipes to nonblocking mode, it causes serious problems for any rust program at the other end.
    
    Fixing this issue is for another day, but fixing the tests is for today.

--- tests-integration/src/bin/test-cat.rs
+++ tests-integration/src/bin/test-cat.rs
@@ -1,20 +1,14 @@
 //! A cat-like utility that can be used as a subprocess to test I/O
 //! stream communication.
 
-use std::io;
-use std::io::Write;
+use tokio::io::AsyncWriteExt;
 
-fn main() {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
-    let mut line = String::new();
-    loop {
-        line.clear();
-        stdin.read_line(&mut line).unwrap();
-        if line.is_empty() {
-            break;
-        }
-        stdout.write_all(line.as_bytes()).unwrap();
-    }
-    stdout.flush().unwrap();
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let mut stdin = tokio::io::stdin();
+    let mut stdout = tokio::io::stdout();
+
+    tokio::io::copy(&mut stdin, &mut stdout).await.unwrap();
+
+    stdout.flush().await.unwrap();
 }
