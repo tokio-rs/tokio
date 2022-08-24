87b58043e525b0356bb3f535d46b231db61aa841
Carl Lerche <me@carllerche.com>
2022-08-24 10:41:37 -0700

    and again

--- tokio/src/runtime/builder.rs
+++ tokio/src/runtime/builder.rs
@@ -821,7 +821,7 @@ impl Builder {
         /// use tokio::runtime;
         ///
         /// let rt = runtime::Builder::new_multi_thread()
-        ///     .disable_lifo_slot
+        ///     .disable_lifo_slot()
         ///     .build()
         ///     .unwrap();
         /// ```
