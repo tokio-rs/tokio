a66884a2fb80d1180451706f3c3e006a3fdcb036
Isaac Sikkema <isaacsikkema@gmail.com>
2022-08-25 05:33:25 -0400

    net: add documentation to `try_read()` for zero-length buffers (#4937)

--- tokio/src/net/tcp/stream.rs
+++ tokio/src/net/tcp/stream.rs
@@ -547,8 +547,12 @@ impl TcpStream {
     /// # Return
     ///
     /// If data is successfully read, `Ok(n)` is returned, where `n` is the
-    /// number of bytes read. `Ok(0)` indicates the stream's read half is closed
-    /// and will no longer yield data. If the stream is not ready to read data
+    /// number of bytes read. If `n` is `0`, then it can indicate one of two scenarios:
+    ///
+    /// 1. The stream's read half is closed and will no longer yield data.
+    /// 2. The specified buffer was 0 bytes in length.
+    ///
+    /// If the stream is not ready to read data,
     /// `Err(io::ErrorKind::WouldBlock)` is returned.
     ///
     /// # Examples
