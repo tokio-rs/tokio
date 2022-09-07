0a99627dc2fb01b32299bfb06530652d1782b66e
Carl Lerche <me@carllerche.com>
2022-09-07 10:37:08 -0700

    rt: minor time driver refactors
    This patch makes some minor refactors. It renames `ClockTime` to
    `TimeSource` since that is how all variables refer to it. It also moves
    the type into a new file.
    
    Finally, it moves the `unpark` handle out of the mutex as it does not
    need to be there. Note, the call to `unpark` is still called while the
    mutex is held, so there is no functional change. Moving it out of the
    mutex is in preparation of moving the unpark handle completely out of
    the time driver.

--- tokio/src/time/sleep.rs
+++ tokio/src/time/sleep.rs
@@ -1,5 +1,5 @@
 #[cfg(all(tokio_unstable, feature = "tracing"))]
-use crate::runtime::time::ClockTime;
+use crate::runtime::time::TimeSource;
 use crate::runtime::time::{Handle, TimerEntry};
 use crate::time::{error::Error, Duration, Instant};
 use crate::util::trace;
