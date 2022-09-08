40a53b6deb428bd7a1fe39edc554f96728c13b3b
Carl Lerche <me@carllerche.com>
2022-09-08 10:58:40 -0700

    rt: move spawn_blocking methods to blocking mod
    A few spawn_blocking methods were implemented on a Handle struct that is
    intended to hold all runtime driver handles. These methods make more
    sense in the blocking module.

--- tokio/src/runtime/blocking/mod.rs
+++ tokio/src/runtime/blocking/mod.rs
@@ -4,7 +4,7 @@
 //! compilation.
 
 mod pool;
-pub(crate) use pool::{spawn_blocking, BlockingPool, Mandatory, SpawnError, Spawner, Task};
+pub(crate) use pool::{spawn_blocking, BlockingPool, Spawner};
 
 cfg_fs! {
     pub(crate) use pool::spawn_mandatory_blocking;
@@ -13,7 +13,6 @@ cfg_fs! {
 mod schedule;
 mod shutdown;
 mod task;
-pub(crate) use schedule::NoopSchedule;
 pub(crate) use task::BlockingTask;
 
 use crate::runtime::Builder;
