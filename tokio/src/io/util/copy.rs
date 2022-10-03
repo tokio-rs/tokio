dadb7c22acc37981e5f4b6f172dfa90f49acc2b9
Simon Farnsworth <simon@farnz.org.uk>
2022-10-03 10:30:21 +0100

    oops - if the read doesn't claim success, the buffer shouldn't change

--- tokio/src/io/util/copy.rs
+++ tokio/src/io/util/copy.rs
@@ -40,9 +40,11 @@ impl CopyBuffer {
         buf.set_filled(me.cap);
 
         let res = reader.poll_read(cx, &mut buf);
-        let filled_len = buf.filled().len();
-        me.read_done = me.cap == filled_len;
-        me.cap = filled_len;
+        if let Poll::Ready(_) = res {
+            let filled_len = buf.filled().len();
+            me.read_done = me.cap == filled_len;
+            me.cap = filled_len;
+        }
         res
     }
 
