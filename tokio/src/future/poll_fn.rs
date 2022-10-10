624dff4529f89a2aea59adbd7bef45c6fe5b036a
Alice Ryhl <aliceryhl@google.com>
2022-10-10 12:54:11 +0200

    Comment on why pin-project is not used

--- tokio/src/future/poll_fn.rs
+++ tokio/src/future/poll_fn.rs
@@ -45,8 +45,15 @@ where
     type Output = T;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<T> {
-        // SAFETY: We never construct a `Pin<&mut F>` anywhere, so accessing `f`
+        // Safety: We never construct a `Pin<&mut F>` anywhere, so accessing `f`
         // mutably in an unpinned way is sound.
+        //
+        // This use of unsafe cannot be replaced with the pin-project macro
+        // because:
+        //  * If we put `#[pin]` on the field, then it gives us a `Pin<&mut F>`,
+        //    which we can't use to call the closure.
+        //  * If we don't put `#[pin]` on the field, then it makes `PollFn` be
+        //    unconditionally `Unpin`, which we also don't want.
         let me = unsafe { Pin::into_inner_unchecked(self) };
         (me.f)(cx)
     }
