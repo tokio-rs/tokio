3b5ef4eb984df78f5bfc4f9ff22181908177886f
Ryan Thomas <rthomas@users.noreply.github.com>
2022-10-30 01:59:29 +1100

    time: document that timeouts check only before poll (#5126)

--- tokio/src/time/timeout.rs
+++ tokio/src/time/timeout.rs
@@ -21,6 +21,10 @@ use std::task::{self, Poll};
 /// value is returned. Otherwise, an error is returned and the future is
 /// canceled.
 ///
+/// Note that the timeout is checked before polling the future, so if the future
+/// does not yield during execution then it is possible for the future to complete
+/// and exceed the timeout _without_ returning an error.
+///
 /// This function returns a future whose return type is [`Result`]`<T,`[`Elapsed`]`>`, where `T` is the
 /// return type of the provided future.
 ///
