620880f4ca1bb3ea96e8018e220f5e5fd1317875
Daniel Bischof <daniel.bischof@protonmail.com>
2022-10-30 12:04:22 +0100

    io: add `tokio_util::io::{CopyToBytes, SinkWriter}` (#5070)
    Co-authored-by: Alice Ryhl <aliceryhl@google.com>
    Co-authored-by: Simon Farnsworth <simon@farnz.org.uk>

--- tokio-util/src/io/mod.rs
+++ tokio-util/src/io/mod.rs
@@ -10,9 +10,11 @@
 //! [`Body`]: https://docs.rs/hyper/0.13/hyper/struct.Body.html
 //! [`AsyncRead`]: tokio::io::AsyncRead
 
+mod copy_to_bytes;
 mod inspect;
 mod read_buf;
 mod reader_stream;
+mod sink_writer;
 mod stream_reader;
 
 cfg_io_util! {
@@ -20,8 +22,10 @@ cfg_io_util! {
     pub use self::sync_bridge::SyncIoBridge;
 }
 
+pub use self::copy_to_bytes::CopyToBytes;
 pub use self::inspect::{InspectReader, InspectWriter};
 pub use self::read_buf::read_buf;
 pub use self::reader_stream::ReaderStream;
+pub use self::sink_writer::SinkWriter;
 pub use self::stream_reader::StreamReader;
 pub use crate::util::{poll_read_buf, poll_write_buf};
