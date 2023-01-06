a6e13ff76942eb9bf003110a4b108941f2fca6b5
David Tolnay <dtolnay@gmail.com>
2023-01-05 21:35:10 -0800

    Add load and compare_exchange_weak to loom StaticAtomicU64

--- tokio/src/loom/std/atomic_u64_static_once_cell.rs
+++ tokio/src/loom/std/atomic_u64_static_once_cell.rs
@@ -23,11 +23,32 @@ impl StaticAtomicU64 {
         }
     }
 
+    pub(crate) fn load(&self, order: Ordering) -> u64 {
+        *self.inner().lock()
+    }
+
     pub(crate) fn fetch_add(&self, val: u64, order: Ordering) -> u64 {
         let mut lock = self.inner().lock();
         let prev = *lock;
         *lock = prev + val;
         prev
+    }
+
+    pub(crate) fn compare_exchange_weak(
+        &self,
+        current: u64,
+        new: u64,
+        _success: Ordering,
+        _failure: Ordering,
+    ) -> Result<u64, u64> {
+        let mut lock = self.inner().lock();
+
+        if *lock == current {
+            *lock = new;
+            Ok(current)
+        } else {
+            Err(*lock)
+        }
     }
 
     fn inner(&self) -> &Mutex<u64> {
