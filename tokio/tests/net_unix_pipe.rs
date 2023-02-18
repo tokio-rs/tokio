19ee40c38341c75de6ccdf2ceeae7ad502019bb6
Tymoteusz <tymo@tymo.xyz>
2023-02-18 22:26:44 +0100

    fmt

--- tokio/tests/net_unix_pipe.rs
+++ tokio/tests/net_unix_pipe.rs
@@ -1,8 +1,8 @@
 #![cfg(feature = "full")]
 #![cfg(unix)]
 
-use tokio::net::unix::pipe;
 use tokio::io::{AsyncReadExt, AsyncWriteExt, Interest};
+use tokio::net::unix::pipe;
 use tokio_test::task;
 use tokio_test::{assert_err, assert_ok, assert_pending, assert_ready_ok};
 
