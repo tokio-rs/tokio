3735aaac85628525a423a510cf55a66c1c5269e6
Alice Ryhl <aliceryhl@google.com>
2023-03-19 15:48:35 +0100

    use PoisonError directly

--- tokio-util/src/sync/cancellation_token/tree_node.rs
+++ tokio-util/src/sync/cancellation_token/tree_node.rs
@@ -178,7 +178,7 @@ where
                 locked_node = node.inner.lock().unwrap();
                 locked_parent
             }
-            Err(err @ TryLockError::Poisoned(_)) => Err(err).unwrap(),
+            Err(TryLockError::Poisoned(err)) => Err(err).unwrap(),
         };
 
         // If we unlocked the child, then the parent may have changed. Check
