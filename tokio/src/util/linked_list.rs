f536db58668cd54147bbab5b698db49a369b1ef1
Jack Wrenn <jack@wrenn.fyi>
2023-04-18 15:08:56 +0000

    tokio: optimize LinkedList::for_each
    ref: https://github.com/tokio-rs/tokio/pull/5608#discussion_r1169643857

--- tokio/src/util/linked_list.rs
+++ tokio/src/util/linked_list.rs
@@ -300,20 +300,15 @@ cfg_taskdump! {
         where
             F: FnMut(&T::Handle),
         {
-            let mut addr = 0;
-            if let Some(first) = self.pop_back() {
-                addr = T::as_raw(&first).as_ptr() as usize;
-                f(&first);
-                self.push_front(first);
-            }
+            use std::mem::ManuallyDrop;
 
-            while let Some(handle) = self.pop_back() {
-                if addr == T::as_raw(&handle).as_ptr() as usize {
-                    self.push_front(handle);
-                    break;
-                } else {
+            let mut next = self.head;
+
+            while let Some(curr) = next {
+                unsafe {
+                    let handle = ManuallyDrop::new(T::from_raw(curr));
                     f(&handle);
-                    self.push_front(handle);
+                    next = T::pointers(curr).as_ref().get_next();
                 }
             }
         }
