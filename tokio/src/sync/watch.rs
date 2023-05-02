be71e7e90d60a471094e835156f432d88fdeb8f1
Gil Shoshan <shoshan.gil@gmail.com>
2023-05-02 13:40:39 +0300

    fmt fix - removed whitespaces

--- tokio/src/sync/watch.rs
+++ tokio/src/sync/watch.rs
@@ -212,13 +212,13 @@ pub mod error {
     pub struct SendError<T>(pub T);
 
     // ===== impl SendError =====
-    
+
     impl<T> fmt::Debug for SendError<T> {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
             f.debug_struct("SendError").finish_non_exhaustive()
         }
     }
-    
+
     impl<T> fmt::Display for SendError<T> {
         fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
             write!(fmt, "channel closed")
