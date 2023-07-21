fad2ce23120e88a03236426d71f88547dd5a9eda
Carl Lerche <me@carllerche.com>
2023-07-21 08:54:40 -0700

    try again

--- tokio/tests/rt_threaded_alt.rs
+++ tokio/tests/rt_threaded_alt.rs
@@ -715,27 +715,3 @@ fn rt() -> runtime::Runtime {
         .build()
         .unwrap()
 }
-
-#[cfg(tokio_unstable)]
-mod unstable {
-    use super::*;
-
-    #[test]
-    fn test_disable_lifo_slot() {
-        let rt = runtime::Builder::new_multi_thread_alt()
-            .disable_lifo_slot()
-            .worker_threads(2)
-            .build()
-            .unwrap();
-
-        rt.block_on(async {
-            tokio::spawn(async {
-                // Spawn another task and block the thread until completion. If the LIFO slot
-                // is used then the test doesn't complete.
-                futures::executor::block_on(tokio::spawn(async {})).unwrap();
-            })
-            .await
-            .unwrap();
-        })
-    }
-}
