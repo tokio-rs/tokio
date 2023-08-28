280575599137db4facd8ebf21fb8dc429bbd9dc5
Alexander Kirilin <github@alexanderkirilin.com>
2023-08-28 15:24:06 -0500

    Fix example

--- tokio/src/sync/semaphore.rs
+++ tokio/src/sync/semaphore.rs
@@ -71,21 +71,21 @@ use std::sync::Arc;
 ///
 /// #[tokio::main]
 /// async fn main() {
-///    let semaphore = Arc::new(Semaphore::new(3));
-///    let mut join_handles = Vec::new();
+///     let semaphore = Arc::new(Semaphore::new(3));
+///     let mut join_handles = Vec::new();
 ///
-///    for _ in 0..5 {
-///        let permit = semaphore.clone().acquire_owned().await.unwrap();
-///        join_handles.push(tokio::spawn(async move {
-///            // perform task...
-///            // explicitly own `permit` in the task
-///            drop(permit);
-///        }));
-///    }
+///     for _ in 0..5 {
+///         let permit = semaphore.clone().acquire_owned().await.unwrap();
+///         join_handles.push(tokio::spawn(async move {
+///             // perform task...
+///             // explicitly own `permit` in the task
+///             drop(permit);
+///         }));
+///     }
 ///
-///    for handle in join_handles {
-///        handle.await.unwrap();
-///    }
+///     for handle in join_handles {
+///         handle.await.unwrap();
+///     }
 /// }
 /// ```
 ///
