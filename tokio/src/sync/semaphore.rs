b17bddc1bcd2327fcf3e59adc78b807a9d8ed7c0
Alexander Kirilin <github@alexanderkirilin.com>
2023-08-29 12:49:08 -0500

    Add comment

--- tokio/src/sync/semaphore.rs
+++ tokio/src/sync/semaphore.rs
@@ -76,33 +76,34 @@ use std::sync::Arc;
 ///     let listener = TcpListener::bind("127.0.0.1:8080").await?;
 ///
 ///     loop {
+///         // Acquire permit before accepting, else the connection will wait without purpose
 ///         let permit = semaphore.clone().acquire_owned().await.unwrap();
 ///         let (mut socket, _) = listener.accept().await?;
 ///
 ///         tokio::spawn(async move {
-///                 let mut buf = [0; 1024];
+///              let mut buf = [0; 1024];
 ///
-///                 // In a loop, read data from the socket and write the data back.
-///                 loop {
-///                     let n = match socket.read(&mut buf).await {
-///                         // socket closed
-///                         Ok(n) if n == 0 => break,
-///                         Ok(n) => n,
-///                         Err(e) => {
-///                             eprintln!("failed to read from socket; err = {:?}", e);
-///                             break;
-///                         }
-///                     };
+///              // In a loop, read data from the socket and write the data back.
+///              loop {
+///                  let n = match socket.read(&mut buf).await {
+///                      // socket closed
+///                      Ok(n) if n == 0 => break,
+///                      Ok(n) => n,
+///                      Err(e) => {
+///                          eprintln!("failed to read from socket; err = {:?}", e);
+///                          break;
+///                      }
+///                  };
 ///
-///                     // Write the data back
-///                     if let Err(e) = socket.write_all(&buf[0..n]).await {
-///                         eprintln!("failed to write to socket; err = {:?}", e);
-///                         break;
-///                     }
-///                 }
+///                  // Write the data back
+///                  if let Err(e) = socket.write_all(&buf[0..n]).await {
+///                      eprintln!("failed to write to socket; err = {:?}", e);
+///                      break;
+///                  }
+///              }
 ///
-///                 // Drop the permit, so more tasks can be created
-///                 drop(permit);
+///              // Drop the permit, so more tasks can be created
+///              drop(permit);
 ///         });
 ///     }
 /// }
