e2d527f8dd9bba69e8608cc5e77d6cf15d65e53c
Goldstein <root@goldstein.rs>
2023-09-01 13:18:13 +0300

    util: generalize interface for `UdpFramed`
    Previously `UdpFramed` only accepted smart pointers to `UdpSocket`.
    This commit changes it to accept anything that has `.poll_recv_from()`
    and `.poll_send_to()`. It allows for easier testing and more
    composability.

--- tokio-util/src/udp/mod.rs
+++ tokio-util/src/udp/mod.rs
@@ -1,4 +1,62 @@
 //! UDP framing
 
+use std::{
+    borrow::Borrow,
+    io,
+    net::SocketAddr,
+    task::{Context, Poll},
+};
+use tokio::{io::ReadBuf, net::UdpSocket};
+
 mod frame;
 pub use frame::UdpFramed;
+
+/// Types that support receiving datagrams.
+///
+/// This trait is implemented for any types that implement [`Borrow`]<[`UdpSocket`]>.
+pub trait PollRecvFrom {
+    /// Attempts to receive a single datagram on the socket.
+    ///
+    /// See [`UdpSocket::poll_recv_from()`] for more information.
+    fn poll_recv_from(
+        &mut self,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<io::Result<SocketAddr>>;
+}
+
+impl<T: Borrow<UdpSocket>> PollRecvFrom for T {
+    fn poll_recv_from(
+        &mut self,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<io::Result<SocketAddr>> {
+        (*self).borrow().poll_recv_from(cx, buf)
+    }
+}
+
+/// Types that support sending datagrams to [`SocketAddr`]s.
+///
+/// This trait is implemented for any types that implement [`Borrow`]<[`UdpSocket`]>.
+pub trait PollSendTo {
+    /// Attempts to send data on the socket to a given address.
+    ///
+    /// See [`UdpSocket::poll_send_to()`] for more information.
+    fn poll_send_to(
+        &mut self,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+        target: SocketAddr,
+    ) -> Poll<io::Result<usize>>;
+}
+
+impl<T: Borrow<UdpSocket>> PollSendTo for T {
+    fn poll_send_to(
+        &mut self,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+        target: SocketAddr,
+    ) -> Poll<io::Result<usize>> {
+        (*self).borrow().poll_send_to(cx, buf, target)
+    }
+}
