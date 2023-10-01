08d7d0c4fe5f6eee4df802de82543ee4c730e026
Weijia Jiang <wjjiang19@163.com>
2023-10-01 20:01:04 +0800

    feat: make spawn_concurrency_level to be a unstable api

--- tokio/src/runtime/builder.rs
+++ tokio/src/runtime/builder.rs
@@ -445,6 +445,7 @@ impl Builder {
     ///
     /// This will panic if `val` is not larger than `0`.
     #[track_caller]
+    #[cfg(tokio_unstable)]
     pub fn spawn_concurrency_level(&mut self, mut val: usize) -> &mut Self {
         assert!(val > 0, "spawn concurrency level cannot be set to 0");
         if val > 1 << 16 {
