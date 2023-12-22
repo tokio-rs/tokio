3ceab8920bc20bc6652359df9b3a5eb32d37b7d5
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-12-22 19:52:46 +0300

    Updated either.rs

--- tokio-util/src/either.rs
+++ tokio-util/src/either.rs
@@ -169,21 +169,21 @@ where
     L: futures_sink::Sink<Item, Error = Error>,
     R: futures_sink::Sink<Item, Error = Error>,
 {
-    type Error = L::Error;
+    type Error = Error;
 
-    fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+    fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::result::Result<(), Self::Error>> {
         delegate_call!(self.poll_ready(cx))
     }
 
-    fn start_send(self: Pin<&mut Self>, item: Item) -> Result<(), Error> {
+    fn start_send(self: Pin<&mut Self>, item: Item) -> std::result::Result<(), Self::Error> {
         delegate_call!(self.start_send(item))
     }
 
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::result::Result<(), Self::Error>> {
         delegate_call!(self.poll_flush(cx))
     }
 
-    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::result::Result<(), Self::Error>> {
         delegate_call!(self.poll_close(cx))
     }
 }
