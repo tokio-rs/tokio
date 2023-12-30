02b779e315c5c5f0dbbc8b56fc711cc8e665ee1e
Paul Olteanu <p.a.olteanu@gmail.com>
2023-12-30 15:05:37 -0500

    sync: add `watch::Receiver::mark_unchanged` (#6252)

--- tokio/src/sync/watch.rs
+++ tokio/src/sync/watch.rs
@@ -669,6 +669,17 @@ impl<T> Receiver<T> {
         self.version.decrement();
     }
 
+    /// Marks the state as unchanged.
+    ///
+    /// The current value will be considered seen by the receiver.
+    ///
+    /// This is useful if you are not interested in the current value
+    /// visible in the receiver.
+    pub fn mark_unchanged(&mut self) {
+        let current_version = self.shared.state.load().version();
+        self.version = current_version;
+    }
+
     /// Waits for a change notification, then marks the newest value as seen.
     ///
     /// If the newest value in the channel has not yet been marked seen when
