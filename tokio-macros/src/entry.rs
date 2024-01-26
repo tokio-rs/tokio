4cf57f638309cd6c3e4e44082a7d797b985a3c67
ik1ne <ik1ne@naver.com>
2024-01-26 09:32:32 +0900

    macros: wrap main body with function definition instead of async block

--- tokio-macros/src/entry.rs
+++ tokio-macros/src/entry.rs
@@ -1,6 +1,7 @@
 use proc_macro2::{Span, TokenStream, TokenTree};
 use quote::{quote, quote_spanned, ToTokens};
 use syn::parse::{Parse, ParseStream, Parser};
+use syn::spanned::Spanned;
 use syn::{braced, Attribute, Ident, Path, Signature, Visibility};
 
 // syn::AttributeArgs does not implement syn::Parse
@@ -381,6 +382,14 @@ fn parse_knobs(mut input: ItemFn, is_test: bool, config: FinalConfig) -> TokenSt
     };
 
     let body = input.body();
+    let unique_fn_name = Ident::new("__tokio_internal_async_body", Span::call_site());
+    let generics = &input.sig.generics;
+    let fn_output = match &input.sig.output {
+        syn::ReturnType::Default => None,
+        syn::ReturnType::Type(_, ret_type) => Some(quote_spanned! {input.output_span()=>
+            -> #ret_type
+        }),
+    };
 
     // For test functions pin the body to the stack and use `Pin<&mut dyn
     // Future>` to reduce the amount of `Runtime::block_on` (and related
@@ -392,21 +401,15 @@ fn parse_knobs(mut input: ItemFn, is_test: bool, config: FinalConfig) -> TokenSt
     // We don't do this for the main function as it should only be used once so
     // there will be no benefit.
     let body = if is_test {
-        let output_type = match &input.sig.output {
-            // For functions with no return value syn doesn't print anything,
-            // but that doesn't work as `Output` for our boxed `Future`, so
-            // default to `()` (the same type as the function output).
-            syn::ReturnType::Default => quote! { () },
-            syn::ReturnType::Type(_, ret_type) => quote! { #ret_type },
-        };
         quote! {
-            let body = async #body;
+            async fn #unique_fn_name #generics() #fn_output #body
+            let body = #unique_fn_name();
             #crate_path::pin!(body);
-            let body: ::core::pin::Pin<&mut dyn ::core::future::Future<Output = #output_type>> = body;
         }
     } else {
         quote! {
-            let body = async #body;
+            async fn #unique_fn_name #generics() #fn_output #body
+            let body = #unique_fn_name();
         }
     };
 
@@ -488,6 +491,11 @@ impl ItemFn {
             brace_token: self.brace_token,
             stmts: &self.stmts,
         }
+    }
+
+    /// Get the span of the output type of the function item.
+    fn output_span(&self) -> Span {
+        self.sig.output.span()
     }
 
     /// Convert our local function item into a token stream.
