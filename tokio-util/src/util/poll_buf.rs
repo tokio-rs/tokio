f231a5138b797997e65973938834ed39bb97aa1b
Timo Glane <timo.glane@gmail.com>
2024-03-30 12:59:46 +0100

    Allow dynamically sized types in tokio_util::io::poll_write_buf

--- tokio-util/src/util/poll_buf.rs
+++ tokio-util/src/util/poll_buf.rs
@@ -120,7 +120,7 @@ pub fn poll_read_buf<T: AsyncRead + ?Sized, B: BufMut>(
 /// [`File`]: tokio::fs::File
 /// [vectored writes]: tokio::io::AsyncWrite::poll_write_vectored
 #[cfg_attr(not(feature = "io"), allow(unreachable_pub))]
-pub fn poll_write_buf<T: AsyncWrite, B: Buf>(
+pub fn poll_write_buf<T: AsyncWrite + ?Sized, B: Buf>(
     io: Pin<&mut T>,
     cx: &mut Context<'_>,
     buf: &mut B,
