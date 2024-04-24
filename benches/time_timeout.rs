1ae3de2839061348abb9021d93c15c0b2f819a6f
wathenjiang <wathenjiang@tencent.com>
2024-04-24 15:36:29 +0800

    feat: add single_thread_sleep and multi_thread_sleep-8

--- benches/time_timeout.rs
+++ benches/time_timeout.rs
@@ -5,7 +5,7 @@
 use std::time::{Duration, Instant};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use tokio::time::timeout;
+use tokio::time::{sleep, timeout};
 
 // a vevry quick async task, but might timeout
 async fn quick_job() -> usize {
@@ -13,14 +13,14 @@ async fn quick_job() -> usize {
 }
 
 fn single_thread_scheduler_timeout(c: &mut Criterion) {
-    do_test(c, 1, "single_thread_timeout");
+    do_timeout_test(c, 1, "single_thread_timeout");
 }
 
 fn multi_thread_scheduler_timeout(c: &mut Criterion) {
-    do_test(c, 8, "multi_thread_timeout-8");
+    do_timeout_test(c, 8, "multi_thread_timeout-8");
 }
 
-fn do_test(c: &mut Criterion, workers: usize, name: &str) {
+fn do_timeout_test(c: &mut Criterion, workers: usize, name: &str) {
     let runtime = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .worker_threads(workers)
@@ -31,14 +31,14 @@ fn do_test(c: &mut Criterion, workers: usize, name: &str) {
         b.iter_custom(|iters| {
             let start = Instant::now();
             runtime.block_on(async {
-                black_box(spawn_job(iters as usize, workers).await);
+                black_box(spawn_timeout_job(iters as usize, workers).await);
             });
             start.elapsed()
         })
     });
 }
 
-async fn spawn_job(iters: usize, procs: usize) {
+async fn spawn_timeout_job(iters: usize, procs: usize) {
     let mut handles = Vec::with_capacity(procs);
     for _ in 0..procs {
         handles.push(tokio::spawn(async move {
@@ -53,10 +53,52 @@ async fn spawn_job(iters: usize, procs: usize) {
     }
 }
 
+fn single_thread_scheduler_sleep(c: &mut Criterion) {
+    do_sleep_test(c, 1, "single_thread_sleep");
+}
+
+fn multi_thread_scheduler_sleep(c: &mut Criterion) {
+    do_sleep_test(c, 8, "multi_thread_sleep-8");
+}
+
+fn do_sleep_test(c: &mut Criterion, workers: usize, name: &str) {
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .worker_threads(workers)
+        .build()
+        .unwrap();
+
+    c.bench_function(name, |b| {
+        b.iter_custom(|iters| {
+            let start = Instant::now();
+            runtime.block_on(async {
+                black_box(spawn_sleep_job(iters as usize, workers).await);
+            });
+            start.elapsed()
+        })
+    });
+}
+
+async fn spawn_sleep_job(iters: usize, procs: usize) {
+    let mut handles = Vec::with_capacity(procs);
+    for _ in 0..procs {
+        handles.push(tokio::spawn(async move {
+            for _ in 0..iters / procs {
+                let _h = black_box(sleep(Duration::from_secs(1)));
+            }
+        }));
+    }
+    for handle in handles {
+        handle.await.unwrap();
+    }
+}
+
 criterion_group!(
     timeout_benchmark,
     single_thread_scheduler_timeout,
     multi_thread_scheduler_timeout,
+    single_thread_scheduler_sleep,
+    multi_thread_scheduler_sleep
 );
 
 criterion_main!(timeout_benchmark);
