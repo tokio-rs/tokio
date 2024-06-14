1ca6b4bcc6fc70d7096d601e5479c3b2adf9f471
Russell Cohen <rcoh@amazon.com>
2024-06-14 15:51:41 +0000

    metrics: Stabilize `num_active_tasks`
    1. stabilize `num_active_tasks`
    2. Rename internal counters to match `num_active_tasks`
    3. Split `rt_metrics` into `rt_metrics` and `rt_unstable_metrics`.

--- tokio/src/runtime/metrics/runtime.rs
+++ tokio/src/runtime/metrics/runtime.rs
@@ -47,6 +47,29 @@ impl RuntimeMetrics {
         self.handle.inner.num_workers()
     }
 
+    /// Returns the current number of active tasks in the runtime.
+    ///
+    /// This value increases and decreases over time as tasks are spawned and as they are completed or cancelled.
+    ///
+    /// To see the total number of spawned tasks, see `spawned_tasks_count`. Note that this API currently requires using `--cfg tokio_unstable`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tokio::runtime::Handle;
+    ///
+    /// #[tokio::main]
+    /// async fn main() {
+    ///    let metrics = Handle::current().metrics();
+    ///
+    ///     let n = metrics.num_active_tasks();
+    ///     println!("Runtime has {} active tasks", n);
+    /// }
+    /// ```
+    pub fn num_active_tasks(&self) -> usize {
+        self.handle.inner.num_active_tasks()
+    }
+
     cfg_unstable_metrics! {
 
         /// Returns the number of additional threads spawned by the runtime.
@@ -81,26 +104,6 @@ impl RuntimeMetrics {
             self.num_active_tasks()
         }
 
-        /// Returns the current number of active tasks in the runtime.
-        ///
-        /// This value increases and decreases over time as tasks are spawned and as they are completed or cancelled.
-        ///
-        /// # Examples
-        ///
-        /// ```
-        /// use tokio::runtime::Handle;
-        ///
-        /// #[tokio::main]
-        /// async fn main() {
-        ///    let metrics = Handle::current().metrics();
-        ///
-        ///     let n = metrics.num_active_tasks();
-        ///     println!("Runtime has {} active tasks", n);
-        /// }
-        /// ```
-        pub fn num_active_tasks(&self) -> usize {
-            self.handle.inner.active_tasks_count()
-        }
 
         /// Returns the number of idle threads, which have spawned by the runtime
         /// for `spawn_blocking` calls.
