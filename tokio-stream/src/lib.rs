d827273e3d8a77ee1e0bd8d87e8ec6aab5f349d0
sharpened-nacho <166434451+sharpened-nacho@users.noreply.github.com>
2024-06-25 14:07:02 +0200

    stream: fix documentation element
    Removed redundant target in label.
    
    Fixes: #6656

--- tokio-stream/src/lib.rs
+++ tokio-stream/src/lib.rs
@@ -82,7 +82,7 @@ pub mod wrappers;
 mod stream_ext;
 pub use stream_ext::{collect::FromStream, StreamExt};
 /// Adapters for types that implement the `Stream` trait.
-/// Created through the tokio [`StreamExt`](crate::stream_ext::StreamExt) methods.
+/// Created through the tokio [`StreamExt`] methods.
 pub mod adapters {
     pub use crate::stream_ext::{
         AllFuture, AnyFuture, Chain, Filter, FilterMap, FoldFuture, Fuse, Map, MapWhile, Merge,
