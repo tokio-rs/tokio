6aba5a4fbc6639a619bb5373041396c5e5016a1a
Matthew Leach <dev@mattleach.net>
2024-07-10 11:12:51 +0100

    tests: pdfd-reaper: fix tests when '-' is absent from kernel version
    On my machine, any test that calls `is_pidfd_available` fails as my
    kernel string does not contain a '-'; the code expects there to be one.
    
    Fix the test so that is works regardless on whether the kernel string
    contains a '-'.

--- tokio/src/process/unix/pidfd_reaper.rs
+++ tokio/src/process/unix/pidfd_reaper.rs
@@ -245,7 +245,12 @@ mod test {
         assert!(status.success());
         let stdout = String::from_utf8_lossy(&stdout);
 
-        let mut kernel_version_iter = stdout.split_once('-').unwrap().0.split('.');
+        let mut kernel_version_iter = match stdout.split_once('-') {
+            Some((version, _)) => version,
+            _ => &stdout,
+        }
+        .split('.');
+
         let major: u32 = kernel_version_iter.next().unwrap().parse().unwrap();
         let minor: u32 = kernel_version_iter.next().unwrap().parse().unwrap();
 
