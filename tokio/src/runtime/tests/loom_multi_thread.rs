798792d0012fc62165b7a228f522b8be34becf0f
Havish <Havish747@gmail.com>
2024-08-28 14:56:32 -0400

    runtime: use std::task::ready

--- tokio/src/runtime/tests/loom_multi_thread.rs
+++ tokio/src/runtime/tests/loom_multi_thread.rs
@@ -11,7 +11,7 @@ mod yield_now;
 use crate::future::poll_fn;
 use crate::runtime::tests::loom_oneshot as oneshot;
 use crate::runtime::{self, Runtime};
-use crate::{spawn, task};
+use crate::{ready, spawn, task};
 use tokio_test::assert_ok;
 
 use loom::sync::atomic::{AtomicBool, AtomicUsize};
