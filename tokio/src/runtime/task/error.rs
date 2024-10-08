7680b24eac69deadb7a0dee8d30f6370dd633368
mox692 <moymoymox@gmail.com>
2024-10-08 20:34:17 +0900

    task: stabilize `task::Id` related apis

--- tokio/src/runtime/task/error.rs
+++ tokio/src/runtime/task/error.rs
@@ -123,14 +123,7 @@ impl JoinError {
     /// Returns a [task ID] that identifies the task which errored relative to
     /// other currently spawned tasks.
     ///
-    /// **Note**: This is an [unstable API][unstable]. The public API of this type
-    /// may break in 1.x releases. See [the documentation on unstable
-    /// features][unstable] for details.
-    ///
     /// [task ID]: crate::task::Id
-    /// [unstable]: crate#unstable-features
-    #[cfg(tokio_unstable)]
-    #[cfg_attr(docsrs, doc(cfg(tokio_unstable)))]
     pub fn id(&self) -> Id {
         self.id
     }
