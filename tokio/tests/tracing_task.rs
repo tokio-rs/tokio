4bbdfcfc5597414336333e8021b8cd740372fc26
Taiki Endo <te316e89@gmail.com>
2025-01-25 21:55:15 +0900

    chore: use [lints] to address unexpected_cfgs lint

--- tokio/tests/tracing_task.rs
+++ tokio/tests/tracing_task.rs
@@ -2,7 +2,6 @@
 //!
 //! These tests ensure that the instrumentation for task spawning and task
 //! lifecycles is correct.
-#![allow(unknown_lints, unexpected_cfgs)]
 #![warn(rust_2018_idioms)]
 #![cfg(all(tokio_unstable, feature = "tracing", target_has_atomic = "64"))]
 
