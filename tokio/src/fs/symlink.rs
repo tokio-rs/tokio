41bb31515840f780993ca9b162bb643f41b5b409
Florian Gäbler <14858479+florian-g2@users.noreply.github.com>
2025-02-06 20:46:25 +0100

    fs: align symlink parameter names with std

--- tokio/src/fs/symlink.rs
+++ tokio/src/fs/symlink.rs
@@ -5,12 +5,12 @@ use std::path::Path;
 
 /// Creates a new symbolic link on the filesystem.
 ///
-/// The `dst` path will be a symbolic link pointing to the `src` path.
+/// The `link` path will be a symbolic link pointing to the `original` path.
 ///
 /// This is an async version of [`std::os::unix::fs::symlink`].
-pub async fn symlink(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
-    let src = src.as_ref().to_owned();
-    let dst = dst.as_ref().to_owned();
+pub async fn symlink(original: impl AsRef<Path>, link: impl AsRef<Path>) -> io::Result<()> {
+    let original = original.as_ref().to_owned();
+    let link = link.as_ref().to_owned();
 
-    asyncify(move || std::os::unix::fs::symlink(src, dst)).await
+    asyncify(move || std::os::unix::fs::symlink(original, link)).await
 }
