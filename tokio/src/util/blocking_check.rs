680f16cf5a8dbb7856b2f942293f2e61b27843d6
noah <nomaxx117@gmail.com>
2025-02-19 22:47:51 -0600

    docs.rs doesnt have windows so im guessing

--- tokio/src/util/blocking_check.rs
+++ tokio/src/util/blocking_check.rs
@@ -1,7 +1,7 @@
 #[cfg(unix)]
 use std::os::fd::AsFd;
 #[cfg(windows)]
-use std::os::windows::io::AsHandle;
+use std::os::windows::io::AsSocket;
 
 #[cfg(unix)]
 #[allow(unused_variables)]
@@ -20,7 +20,7 @@ pub(crate) fn check_socket_for_blocking<S: AsFd>(s: &S) -> crate::io::Result<()>
 
 #[cfg(windows)]
 #[allow(unused_variables)]
-pub(crate) fn check_socket_for_blocking<S: AsHandle>(s: &S) -> crate::io::Result<()> {
+pub(crate) fn check_socket_for_blocking<S: AsSocket>(s: &S) -> crate::io::Result<()> {
     #[cfg(debug_assertions)]
     {
         let sock = socket2::SockRef::from(s);
