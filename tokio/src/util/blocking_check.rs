782d37f1d5753b87ee72ae7e141b67de47e913f1
noah <nomaxx117@gmail.com>
2025-02-19 22:05:34 -0600

    that operating system is a decades-old mistake

--- tokio/src/util/blocking_check.rs
+++ tokio/src/util/blocking_check.rs
@@ -1,7 +1,26 @@
+#[cfg(unix)]
 use std::os::fd::AsFd;
+#[cfg(windows)]
+use std::os::windows::io::AsHandle;
 
+#[cfg(unix)]
 #[allow(unused_variables)]
 pub(crate) fn check_socket_for_blocking<S: AsFd>(s: &S) -> crate::io::Result<()> {
+    #[cfg(debug_assertions)]
+    {
+        let sock = socket2::SockRef::from(s);
+
+        if !sock.nonblocking()? {
+            eprintln!("Warning: binding a nonblocking socket, this may be a bug!");
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(windows)]
+#[allow(unused_variables)]
+pub(crate) fn check_socket_for_blocking<S: AsHandle>(s: &S) -> crate::io::Result<()> {
     #[cfg(debug_assertions)]
     {
         let sock = socket2::SockRef::from(s);
