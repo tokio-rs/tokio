b6c51dd9973811d53a073b9332892e0f79627642
brecht stamper <stamper.brecht@gmail.com>
2025-04-30 11:42:38 +0200

    shutdown after tcp reset test

--- tokio/tests/tcp_shutdown.rs
+++ tokio/tests/tcp_shutdown.rs
@@ -2,8 +2,10 @@
 #![cfg(all(feature = "full", not(target_os = "wasi"), not(miri)))] // Wasi doesn't support bind
                                                                    // No `socket` on miri.
 
+use std::time::Duration;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::time::sleep;
 use tokio_test::assert_ok;
 
 #[tokio::test]
@@ -28,4 +30,25 @@ async fn shutdown() {
     assert_eq!(n, 0);
     assert_ok!(AsyncWriteExt::shutdown(&mut stream).await);
     handle.await.unwrap()
+}
+
+#[tokio::test]
+async fn shutdown_after_tcp_reset() {
+    let srv = assert_ok!(TcpListener::bind("127.0.0.1:0").await);
+    let addr = assert_ok!(srv.local_addr());
+
+    let handle = tokio::spawn(async move {
+        let mut stream = assert_ok!(TcpStream::connect(&addr).await);
+        sleep(Duration::from_millis(2)).await;
+        assert_ok!(AsyncWriteExt::shutdown(&mut stream).await);
+    });
+
+    let (stream, _) = assert_ok!(srv.accept().await);
+    // By setting linger to 0 we will trigger a TCP reset
+    stream.set_linger(Some(Duration::new(0, 0))).unwrap();
+
+    sleep(Duration::from_millis(1)).await;
+    drop(stream);
+
+    handle.await.unwrap();
 }
