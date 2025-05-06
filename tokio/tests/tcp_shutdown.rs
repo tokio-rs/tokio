24532604fcd0cacd5d1034abc8fcf8bff65d1dff
brecht stamper <stamper.brecht@gmail.com>
2025-05-06 09:48:46 +0200

    use channels for tests

--- tokio/tests/tcp_shutdown.rs
+++ tokio/tests/tcp_shutdown.rs
@@ -5,7 +5,7 @@
 use std::time::Duration;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::time::sleep;
+use tokio::sync::oneshot::channel;
 use tokio_test::assert_ok;
 
 #[tokio::test]
@@ -37,18 +37,24 @@ async fn shutdown_after_tcp_reset() {
     let srv = assert_ok!(TcpListener::bind("127.0.0.1:0").await);
     let addr = assert_ok!(srv.local_addr());
 
+    let (connected_tx, connected_rx) = channel();
+    let (dropped_tx, dropped_rx) = channel();
+
     let handle = tokio::spawn(async move {
         let mut stream = assert_ok!(TcpStream::connect(&addr).await);
-        sleep(Duration::from_millis(2)).await;
+        connected_tx.send(()).unwrap();
+
+        dropped_rx.await.unwrap();
         assert_ok!(AsyncWriteExt::shutdown(&mut stream).await);
     });
 
     let (stream, _) = assert_ok!(srv.accept().await);
     // By setting linger to 0 we will trigger a TCP reset
     stream.set_linger(Some(Duration::new(0, 0))).unwrap();
+    connected_rx.await.unwrap();
 
-    sleep(Duration::from_millis(1)).await;
     drop(stream);
+    dropped_tx.send(()).unwrap();
 
     handle.await.unwrap();
 }
@@ -58,16 +64,19 @@ async fn shutdown_multiple_calls() {
     let srv = assert_ok!(TcpListener::bind("127.0.0.1:0").await);
     let addr = assert_ok!(srv.local_addr());
 
+    let (connected_tx, connected_rx) = channel();
+
     let handle = tokio::spawn(async move {
         let mut stream = assert_ok!(TcpStream::connect(&addr).await);
+        connected_tx.send(()).unwrap();
         assert_ok!(AsyncWriteExt::shutdown(&mut stream).await);
         assert_ok!(AsyncWriteExt::shutdown(&mut stream).await);
         assert_ok!(AsyncWriteExt::shutdown(&mut stream).await);
     });
 
     let (mut stream, _) = assert_ok!(srv.accept().await);
-    sleep(Duration::from_millis(1)).await;
-    assert_ok!(AsyncWriteExt::shutdown(&mut stream).await);
+    connected_rx.await.unwrap();
 
+    assert_ok!(AsyncWriteExt::shutdown(&mut stream).await);
     handle.await.unwrap();
 }
