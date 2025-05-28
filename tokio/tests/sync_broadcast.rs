193c1574a119605e4d9081d481a6bb03e7bf26cc
Jeff Vander Stoep <jvanderstoep@gmail.com>
2025-05-28 13:32:24 +0200

    examples: update rand crate to 0.9.1 (#7371)

--- tokio/tests/sync_broadcast.rs
+++ tokio/tests/sync_broadcast.rs
@@ -565,13 +565,11 @@ fn sender_len() {
 #[test]
 #[cfg(not(all(target_family = "wasm", not(target_os = "wasi"))))]
 fn sender_len_random() {
-    use rand::Rng;
-
     let (tx, mut rx1) = broadcast::channel(16);
     let mut rx2 = tx.subscribe();
 
     for _ in 0..1000 {
-        match rand::thread_rng().gen_range(0..4) {
+        match rand::random_range(0..4) {
             0 => {
                 let _ = rx1.try_recv();
             }
