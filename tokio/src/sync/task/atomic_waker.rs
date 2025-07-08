395cb69fb12a499dd6a261151918cab7fa492583
Orson Peters <orsonpeters@gmail.com>
2025-07-08 13:22:04 +0200

    sync: improve AtomicWaker::wake performance

--- tokio/src/sync/task/atomic_waker.rs
+++ tokio/src/sync/task/atomic_waker.rs
@@ -316,11 +316,11 @@ impl AtomicWaker {
         // memory the `AtomicWaker` is associated with.
         match self.state.fetch_or(WAKING, AcqRel) {
             WAITING => {
-                // The waking lock has been acquired.
+                // SAFETY: the waking lock has been acquired.
                 let waker = unsafe { self.waker.with_mut(|t| (*t).take()) };
 
-                // Release the lock
-                self.state.fetch_and(!WAKING, Release);
+                // Release the lock.
+                self.state.store(WAITING, Release);
 
                 waker
             }
