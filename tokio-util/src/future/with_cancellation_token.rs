dace3d22f6126812ccc4d1880c1d5123fdcacd3b
yanyuxing <yuxing.bitcapybara@gmail.com>
2025-07-26 10:43:16 +0800

    future: Include `RunUntilCancelledFuture` inside `WithCancellationToken` to eliminate duplicate code

--- tokio-util/src/future/with_cancellation_token.rs
+++ tokio-util/src/future/with_cancellation_token.rs
@@ -6,7 +6,7 @@ use std::{
 
 use pin_project_lite::pin_project;
 
-use crate::sync::{CancellationToken, WaitForCancellationFuture, WaitForCancellationFutureOwned};
+use crate::sync::{CancellationToken, RunUntilCancelledFuture, RunUntilCancelledFutureOwned};
 
 pin_project! {
     /// A Future that is resolved once the corresponding [`CancellationToken`]
@@ -15,17 +15,15 @@ pin_project! {
     #[must_use = "futures do nothing unless polled"]
     pub struct WithCancellationTokenFuture<'a, F: Future> {
         #[pin]
-        cancellation: WaitForCancellationFuture<'a>,
-        #[pin]
-        future: F,
+        run_until_cancelled: Option<RunUntilCancelledFuture<'a, F>>
     }
 }
 
 impl<'a, F: Future> WithCancellationTokenFuture<'a, F> {
     pub(crate) fn new(cancellation_token: &'a CancellationToken, future: F) -> Self {
         Self {
-            cancellation: cancellation_token.cancelled(),
-            future,
+            run_until_cancelled: (!cancellation_token.is_cancelled())
+                .then(|| RunUntilCancelledFuture::new(cancellation_token, future)),
         }
     }
 }
@@ -35,12 +33,9 @@ impl<'a, F: Future> Future for WithCancellationTokenFuture<'a, F> {
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let this = self.project();
-        if this.cancellation.poll(cx).is_ready() {
-            Poll::Ready(None)
-        } else if let Poll::Ready(res) = this.future.poll(cx) {
-            Poll::Ready(Some(res))
-        } else {
-            Poll::Pending
+        match this.run_until_cancelled.as_pin_mut() {
+            Some(fut) => fut.poll(cx),
+            None => Poll::Ready(None),
         }
     }
 }
@@ -52,17 +47,15 @@ pin_project! {
     #[must_use = "futures do nothing unless polled"]
     pub struct WithCancellationTokenFutureOwned<F: Future> {
         #[pin]
-        cancellation: WaitForCancellationFutureOwned,
-        #[pin]
-        future: F,
+        run_until_cancelled: Option<RunUntilCancelledFutureOwned<F>>
     }
 }
 
 impl<F: Future> WithCancellationTokenFutureOwned<F> {
     pub(crate) fn new(cancellation_token: CancellationToken, future: F) -> Self {
         Self {
-            cancellation: cancellation_token.cancelled_owned(),
-            future,
+            run_until_cancelled: (!cancellation_token.is_cancelled())
+                .then(|| RunUntilCancelledFutureOwned::new(cancellation_token, future)),
         }
     }
 }
@@ -72,12 +65,9 @@ impl<F: Future> Future for WithCancellationTokenFutureOwned<F> {
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let this = self.project();
-        if this.cancellation.poll(cx).is_ready() {
-            Poll::Ready(None)
-        } else if let Poll::Ready(res) = this.future.poll(cx) {
-            Poll::Ready(Some(res))
-        } else {
-            Poll::Pending
+        match this.run_until_cancelled.as_pin_mut() {
+            Some(fut) => fut.poll(cx),
+            None => Poll::Ready(None),
         }
     }
 }
