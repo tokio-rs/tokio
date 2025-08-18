de1eaeaf6f2270928e407ad77b807f695da9832b
Varga Marcell <128537619+marci1175@users.noreply.github.com>
2025-08-18 14:30:44 +0200

    Added function implementation to Shared

--- tokio/src/runtime/scheduler/multi_thread/worker/metrics.rs
+++ tokio/src/runtime/scheduler/multi_thread/worker/metrics.rs
@@ -11,5 +11,11 @@ cfg_unstable_metrics! {
         pub(crate) fn worker_local_queue_depth(&self, worker: usize) -> usize {
             self.remotes[worker].steal.len()
         }
+
+        pub(crate) fn worker_local_queue_depth_checked(&self, worker: usize) -> Option<usize> {
+            self.remotes.get(worker).map(|worker| {
+                worker.steal.len()
+            })
+        }
     }
 }
