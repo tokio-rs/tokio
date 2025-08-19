356d13a56ffb5a5ae79ca064bb5fbab7ab3d15ec
Qi <add_sp@outlook.com>
2025-08-19 22:52:08 +0800

    make the code better
    Signed-off-by: ADD-SP <qiqi.zhang@konghq.com>

--- tokio/src/sync/mpsc/bounded.rs
+++ tokio/src/sync/mpsc/bounded.rs
@@ -255,32 +255,43 @@ impl<T> Receiver<T> {
     /// In other words, it is a more efficient version of the
     /// following implementation.
     /// ```
-    /// # async fn foo(limit: usize) -> usize {
-    /// # let (tx, mut rx) = tokio::sync::mpsc::channel(64);
-    /// # let mut buffer = Vec::<()>::new();
-    /// if limit == 0 {
-    ///     return 0;
-    /// }
-    ///
-    /// // Wait for at least one message (or channel close).
-    /// let Some(first_message) = rx.recv().await else {
-    ///     return 0;
-    /// };
-    ///
-    /// buffer.push(first_message);
-    /// let mut num_pushed = 1;
-    ///
-    /// // Try to get more messages, but don't sleep.
-    /// while num_pushed < limit {
-    ///     if let Ok(msg) = rx.try_recv() {
-    ///         buffer.push(msg);
-    ///         num_pushed += 1;
-    ///     } else {
-    ///         break;
+    /// # struct Mock<T> {
+    /// #     _p: std::marker::PhantomData<T>,
+    /// # }
+    /// # impl<T> Mock<T> {
+    /// #
+    /// # async fn recv(&mut self) -> Option<T> {
+    /// #     todo!()
+    /// # }
+    /// #
+    /// # fn try_recv(&mut self) -> Result<T, ()> {
+    /// #     todo!()
+    /// # }
+    /// async fn recv_many(&mut self, buffer: &mut Vec<T>, limit: usize) -> usize {
+    ///     if limit == 0 {
+    ///         return 0;
     ///     }
-    /// }
     ///
-    /// return num_pushed;
+    ///     // Wait for at least one message (or channel close).
+    ///     let Some(first_message) = self.recv().await else {
+    ///         return 0;
+    ///     };
+    ///
+    ///     buffer.push(first_message);
+    ///     let mut num_pushed = 1;
+    ///
+    ///     // Try to get more messages, but don't sleep.
+    ///     while num_pushed < limit {
+    ///         if let Ok(msg) = self.try_recv() {
+    ///             buffer.push(msg);
+    ///             num_pushed += 1;
+    ///         } else {
+    ///             break;
+    ///         }
+    ///     }
+    ///
+    ///     num_pushed
+    /// }
     /// # }
     /// ```
     ///
