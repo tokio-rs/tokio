1f307f2dbf811073633fb83e125069c9eacdf5a9
Aatif Syed <aatif@lo.tech>
2025-09-04 11:13:30 +0100

    review: move next to Debug

--- tokio/src/net/tcp/stream.rs
+++ tokio/src/net/tcp/stream.rs
@@ -70,12 +70,6 @@ cfg_net! {
     pub struct TcpStream {
         io: PollEvented<mio::net::TcpStream>,
     }
-
-    impl AsRef<TcpStream> for TcpStream {
-        fn as_ref(&self) -> &Self {
-            self
-        }
-    }
 }
 
 impl TcpStream {
@@ -1465,6 +1459,12 @@ impl AsyncWrite for TcpStream {
 impl fmt::Debug for TcpStream {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.io.fmt(f)
+    }
+}
+
+impl AsRef<Self> for TcpStream {
+    fn as_ref(&self) -> &Self {
+        self
     }
 }
 
