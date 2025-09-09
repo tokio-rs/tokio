fbd6e06734798de9948d720d8db6669d902e3f3f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-09-09 10:06:48 +0300

    fs: Preserve the max_buf_size when cloning a File

--- tokio/src/fs/file.rs
+++ tokio/src/fs/file.rs
@@ -465,7 +465,9 @@ impl File {
         self.inner.lock().await.complete_inflight().await;
         let std = self.std.clone();
         let std_file = asyncify(move || std.try_clone()).await?;
-        Ok(File::from_std(std_file))
+        let mut file = File::from_std(std_file);
+        file.set_max_buf_size(self.max_buf_size);
+        Ok(file)
     }
 
     /// Destructures `File` into a [`std::fs::File`]. This function is
