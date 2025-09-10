8976c61aa9b7fc70fbf392e1321926d43cbcd73f
Martin Grigorov <martin-g@users.noreply.github.com>
2025-09-10 11:52:43 +0300

    Remove unneeded indefinite article
    Co-authored-by: Alice Ryhl <aliceryhl@google.com>

--- tokio/src/sync/mod.rs
+++ tokio/src/sync/mod.rs
@@ -278,7 +278,7 @@
 //!
 //! ## `watch` channel
 //!
-//! The [`watch` channel] supports sending **many** values from a **many**
+//! The [`watch` channel] supports sending **many** values from **many**
 //! producers to **many** consumers. However, only the **most recent** value is
 //! stored in the channel. Consumers are notified when a new value is sent, but
 //! there is no guarantee that consumers will see **all** values.
