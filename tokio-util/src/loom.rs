f2fda8a85ff5670d4b3dcc39a931ffe843a0a822
Benjamin Ran <benjaminran@Benjamins-MacBook-Pro-3.local>
2025-09-23 22:29:09 +0300

    tokio-util: enable loom testing for cancellation token
    Fixes: #7271

--- tokio-util/src/loom.rs
+++ tokio-util/src/loom.rs
@@ -1,1 +1,9 @@
-pub(crate) use std::sync;
+//! This module abstracts over `loom` and `std::sync` types depending on whether we
+//! are running loom tests or not.
+
+pub(crate) mod sync {
+    #[cfg(loom)]
+    pub(crate) use loom::sync::{Arc, Mutex, MutexGuard};
+    #[cfg(not(loom))]
+    pub(crate) use std::sync::{Arc, Mutex, MutexGuard};
+}
