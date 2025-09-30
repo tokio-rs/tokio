a0f7f5c94a3958093dbda6934e498e90c3fd1f17
Qi <add_sp@outlook.com>
2025-09-30 19:53:19 +0800

    fs: emit compilation error without `tokio_unstable` for `io-uring` (#7634)
    Signed-off-by: ADD-SP <qiqi.zhang@konghq.com>

--- tokio/src/lib.rs
+++ tokio/src/lib.rs
@@ -480,6 +480,9 @@ compile_error! {
 ))]
 compile_error!("Only features sync,macros,io-util,rt,time are supported on wasm.");
 
+#[cfg(all(not(tokio_unstable), feature = "io-uring"))]
+compile_error!("The `io-uring` feature requires `--cfg tokio_unstable`.");
+
 #[cfg(all(not(tokio_unstable), tokio_taskdump))]
 compile_error!("The `tokio_taskdump` feature requires `--cfg tokio_unstable`.");
 
