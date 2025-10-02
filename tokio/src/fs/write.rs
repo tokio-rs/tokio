3698a6f153c3b3484a5231901aae441e39327768
Motoyuki Kimura <moymoymox@gmail.com>
2025-10-02 20:01:18 +0900

    fs: support io_uring in `fs::write` (#7567)

--- tokio/src/fs/write.rs
+++ tokio/src/fs/write.rs
@@ -1,4 +1,4 @@
-use crate::fs::asyncify;
+use crate::{fs::asyncify, util::as_ref::OwnedBuf};
 
 use std::{io, path::Path};
 
@@ -24,8 +24,69 @@ use std::{io, path::Path};
 /// # }
 /// ```
 pub async fn write(path: impl AsRef<Path>, contents: impl AsRef<[u8]>) -> io::Result<()> {
-    let path = path.as_ref().to_owned();
+    let path = path.as_ref();
     let contents = crate::util::as_ref::upgrade(contents);
 
+    #[cfg(all(
+        tokio_unstable,
+        feature = "io-uring",
+        feature = "rt",
+        feature = "fs",
+        target_os = "linux"
+    ))]
+    {
+        let handle = crate::runtime::Handle::current();
+        let driver_handle = handle.inner.driver().io();
+        if driver_handle.check_and_init()? {
+            return write_uring(path, contents).await;
+        }
+    }
+
+    write_spawn_blocking(path, contents).await
+}
+
+#[cfg(all(
+    tokio_unstable,
+    feature = "io-uring",
+    feature = "rt",
+    feature = "fs",
+    target_os = "linux"
+))]
+async fn write_uring(path: &Path, mut buf: OwnedBuf) -> io::Result<()> {
+    use crate::{fs::OpenOptions, runtime::driver::op::Op};
+    use std::os::fd::OwnedFd;
+
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(path)
+        .await?;
+
+    let mut fd: OwnedFd = file
+        .try_into_std()
+        .expect("unexpected in-flight operation detected")
+        .into();
+
+    let total: usize = buf.as_ref().len();
+    let mut buf_offset: usize = 0;
+    let mut file_offset: u64 = 0;
+    while buf_offset < total {
+        let (n, _buf, _fd) = Op::write_at(fd, buf, buf_offset, file_offset)?.await?;
+        if n == 0 {
+            return Err(io::ErrorKind::WriteZero.into());
+        }
+
+        buf = _buf;
+        fd = _fd;
+        buf_offset += n as usize;
+        file_offset += n as u64;
+    }
+
+    Ok(())
+}
+
+async fn write_spawn_blocking(path: &Path, contents: OwnedBuf) -> io::Result<()> {
+    let path = path.to_owned();
     asyncify(move || std::fs::write(path, contents)).await
 }
