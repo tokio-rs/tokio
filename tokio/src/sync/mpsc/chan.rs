0762d01a29d15d22cb18157b3c0cba4bc9b4c255
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-10-15 23:20:06 +0900

    Update tokio/src/sync/mpsc/chan.rs
    Co-authored-by: Alice Ryhl <aliceryhl@google.com>

--- tokio/src/sync/mpsc/chan.rs
+++ tokio/src/sync/mpsc/chan.rs
@@ -439,14 +439,9 @@ impl<T, S: Semaphore> Rx<T, S> {
                             return Ok(value);
                         }
                         TryPopResult::Closed => return Err(TryRecvError::Disconnected),
-                        TryPopResult::Empty => {
-                            // If close() was called, an empty queue should report Disconnected.
-                            return if rx_fields.rx_closed {
-                                Err(TryRecvError::Disconnected)
-                            } else {
-                                Err(TryRecvError::Empty)
-                            };
-                        }
+                        // If close() was called, an empty queue should report Disconnected.
+                        TryPopResult::Empty if rx_fields.rx_closed => Err(TryRecvError::Disconnected),
+                        TryPopResult::Empty => Err(TryRecvError::Empty),
                         TryPopResult::Busy => {} // fall through
                     }
                 };
