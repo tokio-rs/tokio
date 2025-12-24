85be9f9062329f9862fe646b5ea58d5aab98e8e4
Vrtgs <vrtgs@vrtgs.xyz>
2025-12-24 22:46:00 +0300

    fix statx on unavailable platforms

--- tokio/src/io/uring/statx.rs
+++ tokio/src/io/uring/statx.rs
@@ -1,9 +1,9 @@
-use crate::io::uring::utils::{box_assume_init, box_new_uninit, cstr};
+use crate::io::uring::utils::{box_new_uninit, cstr};
 use crate::runtime::driver::op::{CancelData, Cancellable, Completable, CqeResult, Op};
 use io_uring::{opcode, types};
-use std::ffi::CString;
+use linux_raw_sys::general::statx;
+use std::fmt::{Debug, Formatter};
 use std::io;
-use std::io::Error;
 use std::mem::MaybeUninit;
 use std::path::Path;
 
@@ -12,18 +12,29 @@ pub(crate) struct Statx {
     /// This field will be read by the kernel during the operation, so we
     /// need to ensure it is valid for the entire duration of the operation.
     #[allow(dead_code)]
-    path: CString,
-    buffer: Box<MaybeUninit<libc::statx>>,
+    path: std::ffi::CString,
+    buffer: Box<MaybeUninit<statx>>,
+}
+
+pub(crate) struct Metadata(#[allow(dead_code)] statx);
+
+impl Debug for Metadata {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Metadata").finish_non_exhaustive()
+    }
 }
 
 impl Completable for Statx {
-    type Output = io::Result<libc::statx>;
+    type Output = io::Result<Metadata>;
 
     fn complete(self, cqe: CqeResult) -> Self::Output {
-        cqe.result.map(|_| *unsafe { box_assume_init(self.buffer) })
+        use crate::io::uring::utils::box_assume_init;
+
+        cqe.result
+            .map(|_| Metadata(*unsafe { box_assume_init(self.buffer) }))
     }
 
-    fn complete_with_error(self, error: Error) -> Self::Output {
+    fn complete_with_error(self, error: io::Error) -> Self::Output {
         Err(error)
     }
 }
@@ -38,18 +49,18 @@ impl Op<Statx> {
     /// Submit a request to open a file.
     fn statx(path: &Path, follow_symlinks: bool) -> io::Result<Op<Statx>> {
         let path = cstr(path)?;
-        let mut buffer = box_new_uninit::<libc::statx>();
+        let mut buffer = box_new_uninit::<statx>();
 
-        let flags = libc::AT_STATX_SYNC_AS_STAT
-            | (libc::AT_SYMLINK_NOFOLLOW * libc::c_int::from(!follow_symlinks));
+        let flags: u32 = linux_raw_sys::general::AT_STATX_SYNC_AS_STAT
+            | (linux_raw_sys::general::AT_SYMLINK_NOFOLLOW * u32::from(!follow_symlinks));
 
         let open_op = opcode::Statx::new(
             types::Fd(libc::AT_FDCWD),
             path.as_ptr(),
             buffer.as_mut_ptr().cast(),
         )
-        .flags(flags)
-        .mask(libc::STATX_BASIC_STATS | libc::STATX_BTIME)
+        .flags(flags as i32)
+        .mask(linux_raw_sys::general::STATX_BASIC_STATS | linux_raw_sys::general::STATX_BTIME)
         .build();
 
         // SAFETY: Parameters are valid for the entire duration of the operation
--- tokio/src/io/uring/utils.rs
+++ tokio/src/io/uring/utils.rs
@@ -6,15 +6,14 @@ pub(crate) fn cstr(p: &Path) -> io::Result<CString> {
     Ok(CString::new(p.as_os_str().as_bytes())?)
 }
 
-// TODO: Remove this once we bump the MSRV to 1.82.
+// TODO(MSRV 1.82): When bumping MSRV, switch to `Box::<T>::new_uninit()`.
 pub(crate) fn box_new_uninit<T>() -> Box<MaybeUninit<T>> {
     // Box::<T>::new_uninit()
     Box::new(MaybeUninit::uninit())
 }
 
-// TODO: Remove this once we bump the MSRV to 1.82.
+// TODO(MSRV 1.82): When bumping MSRV, switch to `Box::<MaybeUninit<T>>::assume_init()`.
 pub(crate) unsafe fn box_assume_init<T>(boxed: Box<MaybeUninit<T>>) -> Box<T> {
-    // Box::<MaybeUninit<T>>::assume_init()
     let raw = Box::into_raw(boxed);
     unsafe { Box::from_raw(raw as *mut T) }
 }
