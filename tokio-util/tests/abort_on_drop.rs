6be1ab65a407659b88dbb6684f4c3341abb91e3c
ultrabear <bearodark@gmail.com>
2026-01-13 11:30:02 -0800

    Add tests

--- tokio-util/tests/abort_on_drop.rs
+++ tokio-util/tests/abort_on_drop.rs
@@ -1,5 +1,5 @@
 use tokio::{sync::oneshot, task::yield_now};
-use tokio_util::task::AbortOnDropHandle;
+use tokio_util::task::{AbortOnDrop, AbortOnDropHandle};
 
 #[tokio::test]
 async fn aborts_task_on_drop() {
@@ -33,6 +33,43 @@ async fn does_not_abort_after_detach() {
         let _ = rx.await;
     });
     let handle = AbortOnDropHandle::new(handle);
+    handle.detach(); // returns and drops the original join handle
+    yield_now().await;
+    assert!(!tx.is_closed()); // task is still live
+}
+
+#[tokio::test]
+async fn handle_aborts_task_on_drop() {
+    let (mut tx, rx) = oneshot::channel::<bool>();
+    let handle = tokio::spawn(async move {
+        let _ = rx.await;
+    });
+    let handle = AbortOnDrop::new(handle.abort_handle());
+    drop(handle);
+    tx.closed().await;
+    assert!(tx.is_closed());
+}
+
+#[tokio::test]
+async fn handle_aborts_task_directly() {
+    let (mut tx, rx) = oneshot::channel::<bool>();
+    let handle = tokio::spawn(async move {
+        let _ = rx.await;
+    });
+    let handle = AbortOnDrop::new(handle.abort_handle());
+    handle.abort();
+    tx.closed().await;
+    assert!(tx.is_closed());
+    assert!(handle.is_finished());
+}
+
+#[tokio::test]
+async fn handle_does_not_abort_after_detach() {
+    let (tx, rx) = oneshot::channel::<bool>();
+    let handle = tokio::spawn(async move {
+        let _ = rx.await;
+    });
+    let handle = AbortOnDrop::new(handle.abort_handle());
     handle.detach(); // returns and drops the original join handle
     yield_now().await;
     assert!(!tx.is_closed()); // task is still live
