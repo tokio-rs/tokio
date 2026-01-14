cb8f45958943f0a4397c92971af1c208d4d4cb23
daschinmoy21 <daschinmoyy21@gmail.com>
2026-01-14 22:32:23 +0530

    skipped check for ! return types

--- tokio-macros/src/entry.rs
+++ tokio-macros/src/entry.rs
@@ -510,13 +510,20 @@ fn parse_knobs(mut input: ItemFn, is_test: bool, config: FinalConfig) -> TokenSt
         }
     } else {
         // force typecheck without runtime overhead
-        quote! {
-            let body = async #body;
-            // identity function that constrains the future's output type to ensure better error messages
-            let body = {
+        let check_block = match &input.sig.output {
+            syn::ReturnType::Type(_, t) if matches!(**t, syn::Type::Never(_)) => quote! {},
+            _ => quote! {
                 if false {
                     let _: &dyn ::core::future::Future<Output = #output_type> = &body;
                 }
+            },
+        };
+
+        quote! {
+            let body = async #body;
+            // Compile-time assertion that the future's output matches the return type.
+            let body = {
+                #check_block
                 body
             };
         }
