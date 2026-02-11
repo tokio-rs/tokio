b1616db79930e8bfcc44e229081b13b0e95d34b2
mattiapitossi <mattia.pitossi@helloprima.com>
2026-02-11 17:45:34 +0100

    gate task module only on docs

--- tokio/src/lib.rs
+++ tokio/src/lib.rs
@@ -549,13 +549,15 @@ cfg_not_sync! {
     mod sync;
 }
 
-cfg_rt! {
-    pub mod task;
-    pub use task::spawn;
-}
+// Currently, task module does not expose any public API outside `rt`
+// feature, so we mark it in the docs. This happens only to docs to
+// avoid introducing breaking changes by restricting the visibility
+// of the task module.
+#[cfg_attr(docsrs, doc(cfg(feature = "rt")))]
+pub mod task;
 
-cfg_not_rt! {
-    pub(crate) mod task;
+cfg_rt! {
+    pub use task::spawn;
 }
 
 cfg_time! {
