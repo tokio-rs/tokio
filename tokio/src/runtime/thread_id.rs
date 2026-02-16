8a4f82869f7e3968f7f717d6572e06ad3aed98a7
winlogon <walker84837@gmail.com>
2026-02-16 14:14:23 +0100

    runtime: remove unused StaticAtomicU64 import in tests
    StaticAtomicU64 is no longer used under cfg(all(test, loom)). This
    commit moves its import to the non-loom path to satisfy `-D warnings`.

--- tokio/src/runtime/thread_id.rs
+++ tokio/src/runtime/thread_id.rs
@@ -5,7 +5,8 @@ pub(crate) struct ThreadId(NonZeroU64);
 
 impl ThreadId {
     pub(crate) fn next() -> Self {
-        use crate::loom::sync::atomic::{Ordering::Relaxed, StaticAtomicU64};
+        use crate::loom::sync::atomic::Ordering::Relaxed;
+
         #[cfg(all(test, loom))]
         use loom::sync::atomic::AtomicU64;
 
@@ -13,6 +14,9 @@ impl ThreadId {
         crate::loom::lazy_static! {
             static ref NEXT_ID: AtomicU64 = AtomicU64::new(0);
         }
+
+        #[cfg(not(all(test, loom)))]
+        use crate::loom::sync::atomic::StaticAtomicU64;
 
         #[cfg(not(all(test, loom)))]
         static NEXT_ID: StaticAtomicU64 = StaticAtomicU64::new(0);
