b24e2c0faec3ee2b606f16129ab9e5d7078122b6
mattiapitossi <mattiapitossi@gmail.com>
2026-02-20 18:03:54 +0100

    fix unwrap docs

--- tokio/src/runtime/handle.rs
+++ tokio/src/runtime/handle.rs
@@ -483,7 +483,7 @@ impl Handle {
     ///
     /// #[tokio::main(flavor = "current_thread", name = "my-runtime")]
     /// async fn main() {
-    ///   println!("Current runtime name: {}", Handle::current().name().unwrap_or("unnamed");
+    ///   println!("Current runtime name: {}", Handle::current().name().unwrap();
     /// }
     /// ```
     ///
