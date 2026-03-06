e5755266a88fa0ed9c2c55219aeb6848efde617e
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2026-03-06 08:21:22 +0200

    Add freeze_last_access_time/freeze_last_write_time for Windows
    The Windows build started failing for Rust 1.94.0:
    ```
    error[E0046]: not all trait items implemented, missing: `freeze_last_access_time`, `freeze_last_write_time`
      --> tokio\src\fs\open_options\mock_open_options.rs:12:1
       |
    12 | / mock! {
    13 | |     #[derive(Debug)]
    14 | |     pub OpenOptions {
    15 | |         pub fn append(&mut self, append: bool) -> &mut Self;
    ...  |
    31 | |     #[cfg(windows)]
    32 | |     impl OpenOptionsExt for OpenOptions {
       | |_______________________________________^ missing `freeze_last_access_time`, `freeze_last_write_time` in implementation
       |
       = note: this error originates in the macro `mock` (in Nightly builds, run with -Z macro-backtrace for more info)
       = help: implement the missing item: `fn freeze_last_access_time(&mut self, _: bool) -> &mut Self { todo!() }`
       = help: implement the missing item: `fn freeze_last_write_time(&mut self, _: bool) -> &mut Self { todo!() }`
    ```

--- tokio/src/fs/open_options/mock_open_options.rs
+++ tokio/src/fs/open_options/mock_open_options.rs
@@ -35,5 +35,7 @@ mock! {
         fn custom_flags(&mut self, flags: u32) -> &mut Self;
         fn attributes(&mut self, val: u32) -> &mut Self;
         fn security_qos_flags(&mut self, flags: u32) -> &mut Self;
+        fn freeze_last_access_time(&mut self, freeze: bool) -> &mut Self;
+        fn freeze_last_write_time(&mut self, freeze: bool) -> &mut Self;
     }
 }
