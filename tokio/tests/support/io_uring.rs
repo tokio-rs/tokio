fd568868cbc9049519033268b5ea3c603cedbbd1
Mattia Pitossi <mattiapitossi@gmail.com>
2026-05-09 14:55:32 +0200

    improve io-uring tests

---
+++ tokio/tests/support/io_uring.rs
@@ -0,0 +1,26 @@
+#![cfg(all(
+    tokio_unstable,
+    feature = "io-uring",
+    feature = "rt",
+    feature = "fs",
+    target_os = "linux"
+))]
+
+use io_uring::IoUring;
+
+// Currently, we are running some of the tests on Kernels where io_uring is not supported
+// to check if the fallback mechanism works, this comes with the limitation that we are not
+// able to run some checks (e.g., asserting a poll returns pending). So this utiliy function
+// is useful when we want to run a test only in Linux targets where io_uring is supported.
+pub fn io_uring_supported() -> bool {
+    let mut probe = io_uring::Probe::new();
+
+    let uring = IoUring::new(256).unwrap();
+
+    match uring.submitter().register_probe(&mut probe) {
+        Ok(_) => true,
+        // The Kernel does not support io-uring
+        Err(e) if e.raw_os_error() == Some(libc::ENOSYS) => false,
+        Err(_) => unreachable!("The target should either support or not support io-uring"),
+    }
+}
