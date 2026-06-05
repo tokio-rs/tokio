2680812992d6817fd7c9c92d951edcc3c6cc19e5
Nanasi <this.saif.email@gmail.com>
2026-06-05 03:26:27 -0400

    sync: fix compilation of SetOnce under loom

--- tokio/src/sync/set_once.rs
+++ tokio/src/sync/set_once.rs
@@ -124,13 +124,11 @@ impl<T: Eq> Eq for SetOnce<T> {}
 
 impl<T> Drop for SetOnce<T> {
     fn drop(&mut self) {
-        if *self.value_set.get_mut() {
+        // TODO: Use get_mut()
+        if self.value_set.load(Ordering::Relaxed) {
             // SAFETY: If the value_set is true, then the value is initialized
             // then there is a value to be dropped and this is safe
-            unsafe {
-                self.value
-                    .with_mut(|ptr| ptr::drop_in_place((*ptr).as_mut_ptr()));
-            }
+            unsafe { self.value.with_mut(|ptr| ptr::drop_in_place(ptr as *mut T)) }
         }
     }
 }
@@ -310,18 +308,21 @@ impl<T> SetOnce<T> {
 
     /// Takes the value from the cell, destroying the cell in the process.
     /// Returns `None` if the cell is empty.
-    pub fn into_inner(mut self) -> Option<T> {
-        if *self.value_set.get_mut() {
+    pub fn into_inner(self) -> Option<T> {
+        // TODO: Use get_mut()
+        let value_set = self.value_set.load(Ordering::Relaxed);
+
+        if value_set {
             // Since we have taken ownership of self, its drop implementation
             // will be called by the end of this function, to prevent a double
             // free we will set the value_set to false so that the drop
             // implementation does not try to drop the value again.
-            *self.value_set.get_mut() = false;
+            self.value_set.store(false, Ordering::Relaxed);
 
             // SAFETY: The SetOnce is currently initialized, we can assume the
             // value is initialized and return that, when we return the value
             // we give the drop handler to the return scope.
-            Some(unsafe { self.value.with(|ptr| ptr::read(ptr).assume_init()) })
+            Some(unsafe { self.value.with_mut(|ptr| ptr::read(ptr).assume_init()) })
         } else {
             None
         }
