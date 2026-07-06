71d2b4d4222fa97babdfb7a0fabaf36508fe0a35
Joseph Perez <joperez@hotmail.fr>
2026-07-06 18:15:39 +0200

    Remove AtomicWaker reexport in loom

--- tokio/src/loom/std/mod.rs
+++ tokio/src/loom/std/mod.rs
@@ -15,10 +15,6 @@ pub(crate) mod cell {
     pub(crate) use super::unsafe_cell::UnsafeCell;
 }
 
-#[cfg(any(feature = "net", feature = "process", feature = "signal",))]
-pub(crate) mod future {
-    pub(crate) use crate::sync::AtomicWaker;
-}
 
 pub(crate) mod hint {
     pub(crate) use std::hint::spin_loop;
