3fb01f91553e3b4c94196b51858880eb31723e7d
Joseph Perez <joperez@hotmail.fr>
2026-07-06 18:54:11 +0200

    Gate AtomicWaker behind rt or time feature

--- tokio/src/sync/task/mod.rs
+++ tokio/src/sync/task/mod.rs
@@ -1,4 +1,6 @@
 //! Thread-safe task notification primitives.
 
+#[cfg(any(feature = "rt", feature = "time"))]
 mod atomic_waker;
+#[cfg(any(feature = "rt", feature = "time"))]
 pub(crate) use self::atomic_waker::AtomicWaker;
