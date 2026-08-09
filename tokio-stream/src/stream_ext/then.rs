d4569bb550ef8adc73d5612b2daeb7f3f2f7dbbe
Rachit2323 <76684704+Rachit2323@users.noreply.github.com>
2026-08-09 21:41:51 +0530

    stream: add inner stream accessors to adaptors (#8272)

--- tokio-stream/src/stream_ext/then.rs
+++ tokio-stream/src/stream_ext/then.rs
@@ -38,6 +38,32 @@ impl<St, Fut, F> Then<St, Fut, F> {
             f,
         }
     }
+
+    /// Returns a reference to the inner stream.
+    pub fn get_ref(&self) -> &St {
+        &self.stream
+    }
+
+    /// Returns a mutable reference to the inner stream.
+    ///
+    /// Mutating the inner stream may confuse this combinator.
+    pub fn get_mut(&mut self) -> &mut St {
+        &mut self.stream
+    }
+
+    /// Returns a pinned mutable reference to the inner stream.
+    ///
+    /// Mutating the inner stream may confuse this combinator.
+    pub fn get_pin_mut(self: Pin<&mut Self>) -> Pin<&mut St> {
+        self.project().stream
+    }
+
+    /// Consumes this combinator and returns the inner stream.
+    ///
+    /// This may discard intermediate combinator state.
+    pub fn into_inner(self) -> St {
+        self.stream
+    }
 }
 
 impl<St, F, Fut> Stream for Then<St, Fut, F>
