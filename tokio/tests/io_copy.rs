022c004b4a7c42c0c534e9737e9c66ff916517b1
rifuki <rifuki.dev@gmail.com>
2026-09-09 23:31:35 +0700

    io: flush the writer when the reader stalls in `copy_buf` (#8424)

--- tokio/tests/io_copy.rs
+++ tokio/tests/io_copy.rs
@@ -44,39 +44,39 @@ async fn copy() {
     assert_eq!(wr, b"hello world");
 }
 
+struct BufferedWd {
+    buf: BytesMut,
+    writer: io::DuplexStream,
+}
+
+impl AsyncWrite for BufferedWd {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        self.get_mut().buf.extend_from_slice(buf);
+        Poll::Ready(Ok(buf.len()))
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        let this = self.get_mut();
+
+        while !this.buf.is_empty() {
+            let n = ready!(Pin::new(&mut this.writer).poll_write(cx, &this.buf))?;
+            let _ = this.buf.split_to(n);
+        }
+
+        Pin::new(&mut this.writer).poll_flush(cx)
+    }
+
+    fn poll_shutdown(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        Pin::new(&mut self.writer).poll_shutdown(cx)
+    }
+}
+
 #[tokio::test]
 async fn proxy() {
-    struct BufferedWd {
-        buf: BytesMut,
-        writer: io::DuplexStream,
-    }
-
-    impl AsyncWrite for BufferedWd {
-        fn poll_write(
-            self: Pin<&mut Self>,
-            _cx: &mut Context<'_>,
-            buf: &[u8],
-        ) -> Poll<io::Result<usize>> {
-            self.get_mut().buf.extend_from_slice(buf);
-            Poll::Ready(Ok(buf.len()))
-        }
-
-        fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-            let this = self.get_mut();
-
-            while !this.buf.is_empty() {
-                let n = ready!(Pin::new(&mut this.writer).poll_write(cx, &this.buf))?;
-                let _ = this.buf.split_to(n);
-            }
-
-            Pin::new(&mut this.writer).poll_flush(cx)
-        }
-
-        fn poll_shutdown(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-            Pin::new(&mut self.writer).poll_shutdown(cx)
-        }
-    }
-
     let (rd, wd) = io::duplex(1024);
     let mut rd = rd.take(1024);
     let mut wd = BufferedWd {
@@ -89,6 +89,24 @@ async fn proxy() {
     assert_ok!(wd.flush().await);
 
     let n = assert_ok!(io::copy(&mut rd, &mut wd).await);
+
+    assert_eq!(n, 1024);
+}
+
+#[tokio::test]
+async fn proxy_buf() {
+    let (rd, wd) = io::duplex(1024);
+    let mut rd = io::BufReader::new(rd).take(1024);
+    let mut wd = BufferedWd {
+        buf: BytesMut::new(),
+        writer: wd,
+    };
+
+    // write start bytes
+    assert_ok!(wd.write_all(&[0x42; 512]).await);
+    assert_ok!(wd.flush().await);
+
+    let n = assert_ok!(io::copy_buf(&mut rd, &mut wd).await);
 
     assert_eq!(n, 1024);
 }
