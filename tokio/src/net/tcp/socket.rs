083180513c2c9ed57d981adb8d964f64270a1c1c
Minh Vu <vuhoangminh97@gmail.com>
2026-09-25 14:18:01 +0200

    net: saturate TcpSocket listen backlog

--- tokio/src/net/tcp/socket.rs
+++ tokio/src/net/tcp/socket.rs
@@ -93,6 +93,24 @@ cfg_net! {
     }
 }
 
+fn backlog_to_c_int(backlog: u32) -> i32 {
+    backlog.min(i32::MAX as u32) as i32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::backlog_to_c_int;
+
+    #[test]
+    fn backlog_to_c_int_saturates() {
+        assert_eq!(backlog_to_c_int(0), 0);
+        assert_eq!(backlog_to_c_int(1), 1);
+        assert_eq!(backlog_to_c_int(i32::MAX as u32), i32::MAX);
+        assert_eq!(backlog_to_c_int(i32::MAX as u32 + 1), i32::MAX);
+        assert_eq!(backlog_to_c_int(u32::MAX), i32::MAX);
+    }
+}
+
 impl TcpSocket {
     /// Creates a new socket configured for IPv4.
     ///
@@ -904,7 +922,7 @@ impl TcpSocket {
     /// }
     /// ```
     pub fn listen(self, backlog: u32) -> io::Result<TcpListener> {
-        self.inner.listen(backlog as i32)?;
+        self.inner.listen(backlog_to_c_int(backlog))?;
         #[cfg(not(windows))]
         let mio = {
             use std::os::fd::{FromRawFd, IntoRawFd};
